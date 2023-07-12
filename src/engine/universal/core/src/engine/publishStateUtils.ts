993affc77bece9c8855477f6097ae81fd75e5faa
jjoderis <janis.joderi@web.de>
2023-07-12 11:48:26 +0200

    Publishing deployment and instance information to a server defined in bpmn will use a topic that is closer to the one used for publishing to the default engine

--- src/engine/universal/core/src/engine/publishStateUtils.ts
+++ src/engine/universal/core/src/engine/publishStateUtils.ts
@@ -12,14 +12,14 @@ export async function publishCurrentInstanceState(engine, instance) {
   try {
     const instanceInformation = engine.getInstanceInformation(instance.id);
 
+    const defaultTopic = `process/${instanceInformation.processId}/instance/${instance.id}`;
+
     // send instance information to the default messaging server
     try {
-      await messaging.publish(
-        `process/${instanceInformation.processId}/instance/${instance.id}`,
-        instanceInformation,
-        undefined,
-        { retain: true, prependDefaultTopic: true }
-      );
+      await messaging.publish(defaultTopic, instanceInformation, undefined, {
+        retain: true,
+        prependDefaultTopic: true,
+      });
     } catch (err) {
       if (engine._log) {
         engine._log.warn(
@@ -35,13 +35,19 @@ export async function publishCurrentInstanceState(engine, instance) {
     const { mqttServer } = getMetaDataFromElement(processObj);
 
     if (mqttServer) {
-      const { url, user, password, topic } = mqttServer;
+      let { url, user, password, topic } = mqttServer;
+
+      // We want to handle 3 situations
+      // 1. empty topic => set topic to default process/[definition-id]/versions/[version-id] (without preceding slash)
+      // 2. non-empty topic without trailing slash => append the default topic [user-defined-topic]/process/[definition-id]/versions/[version-id]
+      // 3. non-empty topic with trailing slash => append the default topic without adding another slash [user-defined-topic]/process/[definition-id]/versions/[version-id]
+      if (topic.length && !topic.endsWith('/')) topic += '/';
 
       // send data if messaging server information is embedded in the bpmn
       if (mqttServer) {
         try {
           await messaging.publish(
-            `${topic}/instance`,
+            `${topic}${defaultTopic}`,
             instanceInformation,
             url,
             { retain: true },

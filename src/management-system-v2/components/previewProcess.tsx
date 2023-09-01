b940034100d276ab60b8bd0207c4627b2853289f
LaMaLein <76120220+LaMaLein@users.noreply.github.com>
2023-09-01 11:44:11 +0200

    Made previewer height adjustable

--- src/management-system-v2/components/previewProcess.tsx
+++ src/management-system-v2/components/previewProcess.tsx
@@ -6,7 +6,7 @@ import type { ElementLike } from 'diagram-js/lib/core/Types';
 
 import useModelerStateStore from '@/lib/use-modeler-state-store';
 
-import React, { FocusEvent, useEffect, useRef, useState } from 'react';
+import React, { FocusEvent, useCallback, useEffect, useRef, useState } from 'react';
 
 import { Card, Input, ColorPicker, Drawer, Space, Image } from 'antd';
 
@@ -30,9 +30,40 @@ const Preview: React.FC<PropertiesPanelProperties> = ({ selectedElement, setOpen
   const { data: bpmn, isSuccess } = useProcessBpmn(
     selectedElement ? selectedElement.definitionId : ''
   );
-
   const canvas = useRef<HTMLDivElement>(null);
   const previewer = useRef<ViewerType | null>(null);
+  const [drawerHeight, setDrawerHeight] = useState(200);
+
+  let resizingDrawer = useRef(false);
+
+  const handleMouseMove = useCallback((e) => {
+    const newHeight = document.body.clientHeight - e.clientY;
+    const [minHeight, maxHeight] = [150, 850];
+    if (newHeight > minHeight && newHeight < maxHeight) {
+      setDrawerHeight(newHeight);
+    }
+  }, []);
+  const handleMouseUp = useCallback(
+    (e) => {
+      if (!resizingDrawer.current) return;
+
+      resizingDrawer.current = false;
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+      setTimeout(() => {
+        previewer.current!.get('canvas').zoom('fit-viewport', 'auto');
+      }, 1_000);
+    },
+    [handleMouseMove]
+  );
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+    resizingDrawer.current = true;
+  };
 
   useEffect(() => {
     console.log('A');
@@ -64,6 +95,7 @@ const Preview: React.FC<PropertiesPanelProperties> = ({ selectedElement, setOpen
 
   const Panel = (
     <Drawer
+      height={drawerHeight}
       title={name}
       placement="bottom"
       onClose={(e) => {
@@ -72,6 +104,20 @@ const Preview: React.FC<PropertiesPanelProperties> = ({ selectedElement, setOpen
       open={true}
       mask={false}
     >
+      <div
+        style={{
+          position: 'absolute',
+          bottom: drawerHeight - 4,
+          zIndex: 1_000,
+          width: '100vw',
+          height: '8px',
+          margin: 0,
+          left: 0,
+          cursor: 'row-resize',
+        }}
+        className="Test"
+        onMouseDown={handleMouseDown}
+      ></div>
       <div style={{ height: '100%' }} ref={canvas}></div>
     </Drawer>
   );

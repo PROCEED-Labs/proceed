2b652872de66ec4cc208b8ece3c5699e6d304581
Felipe Trost <felipe.trost@gmail.com>
2023-09-11 13:28:21 +0200

    Added scripts for iam for ms v2

--- src/management-system-v2/next.config.js
+++ src/management-system-v2/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     API_URL:
       process.env.NODE_ENV === 'development' ? 'https://localhost:33080/api' : process.env.API_URL,
     BACKEND_URL: process.env.NODE_ENV === 'development' ? 'https://localhost:33080' : 'FIXME',
-    NEXT_PUBLIC_USE_AUTH: process.env.USE_AUTH === 'true',
+    NEXT_PUBLIC_USE_AUTH: process.env.USE_AUTHORIZATION === 'true',
   },
   async redirects() {
     return [

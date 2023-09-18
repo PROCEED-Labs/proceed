845c0207d967f5e20ce84b24739fe10984bc849b
Felipe Trost <felipe.trost@gmail.com>
2023-09-18 10:47:03 +0200

    Changed AuthCallbackListener to use useRef + refactor

--- src/management-system-v2/lib/iamComponents.tsx
+++ src/management-system-v2/lib/iamComponents.tsx
@@ -1,6 +1,15 @@
 'use client';
 
-import { ComponentProps, FC, PropsWithChildren, ReactNode, useEffect, useMemo } from 'react';
+import {
+  ComponentProps,
+  FC,
+  PropsWithChildren,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+} from 'react';
 import { useRouter } from 'next/navigation';
 import { Route } from 'next';
 import {
@@ -10,41 +19,43 @@ import {
 import { handleOauthCallback, useAuthStore } from './iam';
 import { App } from 'antd';
 
-let oneShot = true;
 export const AuthCallbackListener: FC = () => {
   const oauthCallback = useAuthStore((store) => store.oauthCallback);
   const router = useRouter();
   const { message: messageApi } = App.useApp();
+  const callbackPerformed = useRef(false);
+
+  const removeAuthCallbackParams = useCallback(() => {
+    const params = new URLSearchParams(window.location.search);
+    params.delete('code');
+    params.delete('state');
+
+    router.replace(
+      `${new URL(window.location.pathname, window.location.origin)}?${params.toString()}`,
+    );
+  }, [router]);
 
   useEffect(() => {
-    if (oneShot && process.env.NEXT_PUBLIC_USE_AUTH && router) {
-      oneShot = false;
+    if (!callbackPerformed.current && process.env.NEXT_PUBLIC_USE_AUTH && router) {
+      callbackPerformed.current = true;
       handleOauthCallback()
         .then((authRes) => oauthCallback(authRes))
-        .then(() => {
-          const params = new URLSearchParams(window.location.search);
-          params.delete('code');
-          params.delete('state');
-
-          router.replace(
-            `${new URL(window.location.pathname, window.location.origin)}?${params.toString()}`,
-          );
-        })
-        .catch(() => {
+        .then(removeAuthCallbackParams)
+        .catch((e) => {
           oauthCallback(undefined);
 
           const searchParamas = new URLSearchParams(window.location.search);
+
+          // if the url params have 'code' in them, it means the user succesfully logged in and was
+          // redirected to the frontend, but something failed with the Oauth callback
           if (searchParamas.has('code'))
-            // if the url params have 'code' in them, it means the user succesfully logged in and was
-            // redirected to the frontend, but something failed with the Oauth callback
             messageApi.open({ type: 'error', content: 'Login failed' });
 
-          if (process.env.NEXT_PUBLIC_USE_AUTH) {
-            router.push('/');
-          }
+          removeAuthCallbackParams();
+          router.push('/');
         });
     }
-  }, [oauthCallback, router, messageApi]);
+  }, [oauthCallback, router, messageApi, removeAuthCallbackParams]);
 
   return null;
 };

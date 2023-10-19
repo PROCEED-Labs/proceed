cce89bba06393cf9a949aa7ca59288c796b9efd1
LaMaLein <76120220+LaMaLein@users.noreply.github.com>
2023-10-19 12:17:34 +0200

    Removed unused import

--- src/management-system-v2/lib/user-preferences.ts
+++ src/management-system-v2/lib/user-preferences.ts
@@ -1,7 +1,6 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 import { useState, useEffect } from 'react';
-import { immer } from 'zustand/middleware/immer';
 
 type PreferencesType = Record<string, string | number | boolean>;
 

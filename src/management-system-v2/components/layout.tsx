0ed63cebcd5dc198d0fe4b2a7bd3d75b81c0cce2
Lucas <Lucasgold99@gmail.com>
2023-11-20 05:04:42 +0100

    remove scrollbar in sider and in main view

--- src/management-system-v2/components/layout.tsx
+++ src/management-system-v2/components/layout.tsx
@@ -29,7 +29,7 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
   const breakpoint = Grid.useBreakpoint();
 
   return (
-    <AntLayout>
+    <AntLayout style={{ height: '100vh' }}>
       <AntLayout hasSider>
         <AntLayout.Sider
           style={{

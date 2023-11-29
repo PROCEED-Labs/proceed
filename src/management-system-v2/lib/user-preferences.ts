561f753ca8225dfa394fe52c52f12db8aa49a5dd
Felipe Trost <felipe.trost@gmail.com>
2023-11-29 15:22:52 +0100

    Removed interface settings from user preferences

--- src/management-system-v2/lib/user-preferences.ts
+++ src/management-system-v2/lib/user-preferences.ts
@@ -24,8 +24,6 @@ const defaultPreferences = {
   'ask-before-deleting-single': true,
   'ask-before-copying': true,
   'process-copy-modal-accordion': true,
-  'interface-size': 'middle',
-  'dark-mode-enabled': false,
 } as const;
 
 export const useUserPreferencesStore = create<PreferencesStoreType>()(

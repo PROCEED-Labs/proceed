463e1d8714e073839ae16d3bfd5c69d3a3143748
Felipe Trost <felipe.trost@gmail.com>
2024-01-17 14:43:00 +0100

    updated global rules actions

--- src/management-system-v2/lib/authorization/globalRules.ts
+++ src/management-system-v2/lib/authorization/globalRules.ts
@@ -1,7 +1,7 @@
 // The rules in this file will be applied to all abilities
 // By default everything is allowed and these rules restrict access
 
-import { AbilityRule } from '../ability/caslAbility';
+import { AbilityRule, resourceAction } from '../ability/caslAbility';
 
 type SystemRules = (AbilityRule & { inverted: true })[];
 
@@ -12,7 +12,7 @@ export const globalOrganizationRules = [] satisfies SystemRules;
 export const globalUserRules = [
   {
     inverted: true,
-    action: ['view', 'manage'],
+    action: [...resourceAction],
     subject: ['Role', 'RoleMapping', 'User', 'Machine', 'Execution', 'EnvConfig'],
   },
 ] satisfies SystemRules;

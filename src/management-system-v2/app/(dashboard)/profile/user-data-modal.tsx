f3f45d2c4abba99c7f511fe1faf6e5483a46b2c5
Felipe Trost <felipe.trost@gmail.com>
2024-01-17 17:51:49 +0100

    Replaced api calls for server actions

--- src/management-system-v2/app/(dashboard)/profile/user-data-modal.tsx
+++ src/management-system-v2/app/(dashboard)/profile/user-data-modal.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { FC, useEffect, useState } from 'react';
-import { Button, Skeleton, Form, Input, Modal, App } from 'antd';
-import { ApiData, ApiRequestBody, useGetAsset, usePutAsset } from '@/lib/fetch-data';
-import { useSession } from 'next-auth/react';
-import { useParseServerErrors } from '@/lib/useParseServerErrors';
+import { FC, useTransition } from 'react';
+import { Button, Form, Input, Modal, App } from 'antd';
+import { ApiData, ApiRequestBody } from '@/lib/fetch-data';
+import { updateUser } from '@/lib/data/users';
+import { User, UserData, UserDataSchema } from '@/lib/data/user-schema';
+import { useRouter } from 'next/navigation';
+import useParseZodErrors from '@/lib/useParseZodErrors';
 
 type modalInputField = {
   userDataField: keyof ApiData<'/users/{id}', 'get'>;
@@ -13,159 +15,71 @@ type modalInputField = {
   password?: boolean;
 };
 
-type modalInput = ({ password: true } | { password: false; inputFields: modalInputField[] }) & {
+type modalInput = {
+  password: false;
+  inputFields: modalInputField[];
   title: string;
 };
 
 const UserDataModal: FC<{
+  userData: User;
   structure: modalInput;
   modalOpen: boolean;
   close: () => void;
-}> = ({ structure, modalOpen, close: propClose }) => {
+}> = ({ structure, modalOpen, close: propClose, userData }) => {
   const [form] = Form.useForm();
-  const [loading, setLoading] = useState(false);
+  const [loading, startTransition] = useTransition();
   const { message } = App.useApp();
+  const router = useRouter();
 
-  const { data } = useSession();
-  const user = data!.user;
-
-  const { data: userData, isLoading } = useGetAsset('/users/{id}', {
-    params: {
-      path: { id: user.id },
-    },
-  });
-  type User = NonNullable<typeof userData>;
-  const [formatErrors, parseErrors] = useParseServerErrors<User>({
-    firstName: 'First Name',
-    lastName: 'Last Name',
-    username: 'Username',
-  });
+  const [formatErrors, parseInput] = useParseZodErrors(UserDataSchema.partial());
 
   function close() {
+    parseInput(undefined);
     propClose();
-    parseErrors(null);
+    form.setFieldsValue(userData);
   }
 
-  useEffect(() => {
-    if (!modalOpen) {
-      const defaultValues: Record<string, any> = {};
+  const submitData = async (values: any) => {
+    startTransition(async () => {
+      try {
+        const data = parseInput(values);
+        if (!data) return;
 
-      if (!structure.password && userData)
-        for (const input of structure.inputFields)
-          defaultValues[input.submitField] = userData[input.userDataField];
+        const result = await updateUser(userData.id, values as UserData);
+        if (result && 'error' in result) throw new Error();
 
-      form.setFieldsValue(defaultValues);
-    }
-  }, [form, modalOpen, userData, structure]);
-
-  const { mutateAsync: changeUserData } = usePutAsset('/users/{id}', { onError: parseErrors });
-  const { mutateAsync: changePassword } = usePutAsset('/users/{id}/update-password');
-
-  const submitData = async (values: Record<string, any>) => {
-    if (!userData) return;
-
-    setLoading(true);
-
-    try {
-      if (structure.password) {
-        await changePassword({
-          params: { path: { id: user.id } },
-          body: { password: form.getFieldValue('password') },
-        });
-      } else {
-        const body = {
-          email: userData.email,
-          firstName: userData.firstName,
-          lastName: userData.lastName,
-          username: userData.username,
-          ...values,
-        };
-
-        await changeUserData({
-          params: { path: { id: user.id } },
-          body,
-        });
+        message.success({ content: 'Profile updated' });
+        router.refresh();
+        close();
+      } catch (e) {
+        message.error({ content: 'An error ocurred' });
       }
-
-      message.success({ content: 'Profile updated' });
-      close();
-    } catch (e) {
-      parseErrors(e);
-      message.error({ content: 'An error ocurred' });
-    } finally {
-      setLoading(false);
-    }
+    });
   };
 
   return (
     <Modal open={modalOpen} onCancel={close} footer={null} title={structure.title}>
-      <Skeleton loading={isLoading}>
-        <br />
-        <Form form={form} layout="vertical" onFinish={submitData}>
-          {structure.password ? (
-            <>
-              <Form.Item
-                name="password"
-                label="Password"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your password!',
-                  },
-                ]}
-                hasFeedback
-              >
-                <Input.Password />
-              </Form.Item>
-
-              <Form.Item
-                name="confirm_password"
-                label="Confirm Password"
-                dependencies={['password']}
-                hasFeedback
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please confirm your password!',
-                  },
-                  ({ getFieldValue }) => ({
-                    validator(_, value) {
-                      if (!value || getFieldValue('password') === value) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject(
-                        new Error('The new password that you entered do not match!'),
-                      );
-                    },
-                  }),
-                ]}
-              >
-                <Input.Password />
-              </Form.Item>
-              <br />
-            </>
-          ) : (
-            structure.inputFields.map((input) => (
-              <Form.Item
-                key={input.submitField}
-                label={input.label}
-                name={input.submitField}
-                validateStatus={input.submitField in formatErrors ? 'error' : ''}
-                help={input.submitField in formatErrors ? formatErrors[input.submitField] : ''}
-                required
-              >
-                <Input />
-              </Form.Item>
-            ))
-          )}
-
-          <Form.Item>
-            <Button type="primary" htmlType="submit" loading={loading}>
-              Submit
-            </Button>
+      <Form form={form} layout="vertical" onFinish={submitData} initialValues={userData}>
+        {structure.inputFields.map((input) => (
+          <Form.Item
+            key={input.submitField}
+            label={input.label}
+            name={input.submitField}
+            validateStatus={input.submitField in formatErrors ? 'error' : ''}
+            help={input.submitField in formatErrors ? formatErrors[input.submitField] : ''}
+            required
+          >
+            <Input />
           </Form.Item>
-        </Form>
-      </Skeleton>
+        ))}
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
--- src/management-system-v2/app/(dashboard)/profile/user-profile.tsx
+++ src/management-system-v2/app/(dashboard)/profile/user-profile.tsx
@@ -1,67 +1,36 @@
 'use client';
 
 import { FC, useState } from 'react';
-import { Space, Card, Avatar, Button, Skeleton, Typography, Result, App, Table } from 'antd';
+import { Space, Card, Avatar, Typography, App, Table } from 'antd';
 import styles from './user-profile.module.scss';
-import { useGetAsset, useDeleteAsset } from '@/lib/fetch-data';
 import { RightOutlined } from '@ant-design/icons';
-import { signOut, useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 import ConfirmationButton from '@/components/confirmation-button';
 import UserDataModal from './user-data-modal';
+import { User } from '@/lib/data/user-schema';
+import { deleteUsers } from '@/lib/data/users';
 
-const UserProfile: FC = () => {
-  const { data } = useSession();
-  const user = data!.user;
-
+const UserProfile: FC<{ userData: User }> = ({ userData }) => {
   const [changeNameModalOpen, setChangeNameModalOpen] = useState(false);
   const [changeEmailModalOpen, setChangeEmailModalOpen] = useState(false);
-  const [changePasswordOpen, setPasswordOpen] = useState(false);
 
   const { message: messageApi } = App.useApp();
-  const { mutateAsync: deleteUserMutation } = useDeleteAsset('/users/{id}');
-
-  const {
-    error,
-    data: userData,
-    isLoading,
-  } = useGetAsset('/users/{id}', { params: { path: { id: (user && user.id) || '' } } });
 
   async function deleteUser() {
     try {
-      // Since this should only be callable once the user was loaded, we can assume that the user is not false.
-      // Check is only for typescript.
-      if (user && user.id) {
-        await deleteUserMutation({
-          params: { path: { id: user.id } },
-        });
-        messageApi.success({ content: 'Your account was deleted' });
-        signOut();
-      }
+      await deleteUsers([userData.id]);
+
+      messageApi.success({ content: 'Your account was deleted' });
+      signOut();
     } catch (e) {
       messageApi.error({ content: 'An error ocurred' });
     }
   }
 
-  if (error)
-    return (
-      <Result
-        status="error"
-        title="Failed to fetch your profile"
-        subTitle="An error ocurred while fetching your profile, please try again."
-      />
-    );
-
   return (
     <>
       <UserDataModal
-        modalOpen={changePasswordOpen}
-        close={() => setPasswordOpen((open) => !open)}
-        structure={{
-          title: 'Change your password',
-          password: true,
-        }}
-      />
-      <UserDataModal
+        userData={userData}
         modalOpen={changeEmailModalOpen}
         close={() => setChangeEmailModalOpen((open) => !open)}
         structure={{
@@ -77,6 +46,7 @@ const UserProfile: FC = () => {
         }}
       />
       <UserDataModal
+        userData={userData}
         modalOpen={changeNameModalOpen}
         close={() => setChangeNameModalOpen((open) => !open)}
         structure={{
@@ -105,60 +75,58 @@ const UserProfile: FC = () => {
       <Space direction="vertical" className={styles.Container}>
         <Card className={styles.Card} style={{ margin: 'auto' }}>
           <Typography.Title level={3}>Account Information</Typography.Title>
-          <Skeleton loading={isLoading}>
-            <Avatar size={64} src={userData && userData.picture} style={{ marginBottom: 16 }} />
-            <Table
-              dataSource={[
-                {
-                  title: 'Name',
-                  value: `${(userData && userData.firstName) || ''} ${
-                    (userData && userData.lastName) || ''
+
+          <Avatar size={64} src={userData.image} style={{ marginBottom: 16 }}>
+            {userData.image ? null : userData.firstName.slice(0, 1) + userData.lastName.slice(0, 1)}
+          </Avatar>
+
+          <Table
+            dataSource={[
+              {
+                title: 'Name',
+                value: `${(userData && userData.firstName) || ''} ${(userData && userData.lastName) || ''
                   }`,
-                  action: () => setChangeNameModalOpen(true),
-                },
-                {
-                  title: 'Username',
-                  value: userData && userData.username,
-                  action: () => setChangeNameModalOpen(true),
-                },
-                {
-                  title: 'Email',
-                  value: userData && userData.email,
-                  action: () => setChangeEmailModalOpen(true),
-                },
-              ]}
-              columns={[
-                { dataIndex: 'title' },
-                { dataIndex: 'value' },
-                {
-                  render: () => <RightOutlined />,
-                },
-              ]}
-              onRow={(row) => ({
-                onClick: row.action,
-              })}
-              showHeader={false}
-              pagination={false}
-              className={styles.Table}
-              style={{ marginBottom: 16 }}
-            />
-            <Space direction="vertical">
-              <Button type="primary" onClick={() => setPasswordOpen(true)}>
-                Change Password
-              </Button>
-              <ConfirmationButton
-                title="Delete Account"
-                description="Are you sure you want to delete your account?"
-                onConfirm={deleteUser}
-                modalProps={{
-                  okText: 'Delete Account',
-                }}
-                buttonProps={{ danger: true }}
-              >
-                Delete Account
-              </ConfirmationButton>
-            </Space>
-          </Skeleton>
+                action: () => setChangeNameModalOpen(true),
+              },
+              {
+                title: 'Username',
+                value: userData && userData.username,
+                action: () => setChangeNameModalOpen(true),
+              },
+              {
+                title: 'Email',
+                value: userData && userData.email,
+                action: () => setChangeEmailModalOpen(true),
+              },
+            ]}
+            columns={[
+              { dataIndex: 'title' },
+              { dataIndex: 'value' },
+              {
+                render: () => <RightOutlined />,
+              },
+            ]}
+            onRow={(row) => ({
+              onClick: row.action,
+            })}
+            showHeader={false}
+            pagination={false}
+            className={styles.Table}
+            style={{ marginBottom: 16 }}
+          />
+          <Space direction="vertical">
+            <ConfirmationButton
+              title="Delete Account"
+              description="Are you sure you want to delete your account?"
+              onConfirm={deleteUser}
+              modalProps={{
+                okText: 'Delete Account',
+              }}
+              buttonProps={{ danger: true }}
+            >
+              Delete Account
+            </ConfirmationButton>
+          </Space>
         </Card>
       </Space>
     </>

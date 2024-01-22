bdd3d03f3243ad2ab40ec99d88cc34299196508f
Felipe Trost <felipe.trost@gmail.com>
2024-01-22 17:55:30 +0100

    Removed unnecessary code

--- src/management-system-v2/lib/useParseZodErrors.tsx
+++ src/management-system-v2/lib/useParseZodErrors.tsx
@@ -18,11 +18,6 @@ export default function useParseZodErrors<T extends ZodRawShape>(schema: z.ZodOb
   }
 
   function parseInput(input: unknown) {
-    if (!input) {
-      setErrors({});
-      return null;
-    }
-
     try {
       return schema.parse(input);
     } catch (e) {

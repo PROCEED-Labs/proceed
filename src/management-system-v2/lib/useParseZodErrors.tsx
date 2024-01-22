ef72ae0b66fc66ed25e849e2ca958feb10f2b043
Felipe Trost <felipe.trost@gmail.com>
2024-01-22 20:29:56 +0100

    feat: antDesignInputProps helper

--- src/management-system-v2/lib/useParseZodErrors.tsx
+++ src/management-system-v2/lib/useParseZodErrors.tsx
@@ -1,6 +1,25 @@
 import z, { ZodRawShape } from 'zod';
 import { ReactNode, useState } from 'react';
 
+/**
+ * This is just to avoid boilerplate when using the errors with an AntDesign Input
+ * @example
+ *
+ *<Form.Item name="name" {...antDesignInputProps(formErrors, 'name')} required>
+ *  <Input placeholder="Environment Name" />
+ *</Form.Item>
+ * */
+export function antDesignInputProps<T extends Partial<Record<string, ReactNode>>>(
+  errors: T,
+  key: keyof T,
+) {
+  return {
+    validateStatus: key in errors ? ('error' as const) : ('' as const),
+    help: errors[key] ?? '',
+    hasFeedback: true,
+  };
+}
+
 export default function useParseZodErrors<T extends ZodRawShape>(schema: z.ZodObject<T>) {
   type Schema = z.infer<typeof schema>;
   type ErrorsObject = Partial<Record<keyof Schema, ReactNode>>;

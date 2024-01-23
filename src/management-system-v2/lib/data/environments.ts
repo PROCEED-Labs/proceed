0a416b5a6a417e771c3dde3af33688219271d3c7
Felipe Trost <felipe.trost@gmail.com>
2024-01-23 16:04:30 +0100

    Environment server actions

---
+++ src/management-system-v2/lib/data/environments.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { getCurrentUser } from '@/components/auth';
+import {
+  EnvironmentInput,
+  UserOrganizationEnvironmentInput,
+  UserOrganizationEnvironmentInputSchema,
+} from './environment-schema';
+import { userError } from '../user-error';
+import { addEnvironment } from './legacy/iam/environments';
+
+export async function addOrganizationEnvironment(
+  environmentInput: UserOrganizationEnvironmentInput,
+) {
+  const { session } = await getCurrentUser();
+  const userId = session?.user.id || '';
+
+  try {
+    const environmentData = UserOrganizationEnvironmentInputSchema.parse(environmentInput);
+
+    return addEnvironment({
+      ownerId: userId,
+      organization: true,
+      ...environmentData,
+    });
+  } catch (e) {
+    console.error(e);
+    return userError('Error adding environment');
+  }
+}

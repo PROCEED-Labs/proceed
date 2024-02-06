1395054845b21d6e0b5d7e43516a547a54bc749e
Lucas <Lucasgold99@gmail.com>
2024-02-06 20:11:04 +0100

    enable selection of currency for planned cost in property panel

---
+++ src/management-system-v2/app/(dashboard)/processes/[processId]/planned-cost-input.tsx
@@ -0,0 +1,88 @@
+import { Input, InputNumber, Select } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from 'react';
+import styles from './planned-cost-input.module.scss';
+
+import worldCurrencies from '@/lib/worldCurrencies.json';
+
+type PlannedCostInputProperties = {
+  costsPlanned: { value?: number; currency?: string };
+  onInput: (costsPlanned: { value?: number; currency: string }) => void;
+};
+
+const PlannedCostInput: React.FC<PlannedCostInputProperties> = ({
+  costsPlanned: { value, currency = 'EUR' },
+  onInput,
+}) => {
+  const [costsPlanned, setCostsPlanned] = useState<{ value?: number; currency: string }>({
+    value,
+    currency: currency,
+  });
+
+  useEffect(() => {
+    setCostsPlanned({ value: value, currency: currency });
+  }, [value, currency]);
+
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <Input
+      type={!isEditing ? 'text' : 'number'}
+      className={styles.PlannedCostInput}
+      style={{ width: '100%' }}
+      addonBefore={
+        <Select
+          suffixIcon={null}
+          value={costsPlanned.currency}
+          options={worldCurrencies.map((currency) => ({ value: currency.cc, label: currency.cc }))}
+          optionRender={(option) => {
+            const currency = worldCurrencies.find((currency) => currency.cc === option.value)!;
+
+            return (
+              <span>
+                {currency.cc} - {currency.symbol}
+              </span>
+            );
+          }}
+          onChange={(value) => {
+            setCostsPlanned({ value: costsPlanned.value, currency: value });
+            if (costsPlanned.value) {
+              onInput({ value: costsPlanned.value, currency: value });
+            }
+          }}
+          dropdownStyle={{ width: '7rem' }}
+        ></Select>
+      }
+      readOnly={!isEditing}
+      placeholder="Planned Cost"
+      suffix={
+        isEditing ? null : (
+          <EditOutlined
+            onClick={() => {
+              setIsEditing(true);
+            }}
+          ></EditOutlined>
+        )
+      }
+      onChange={(val) => {
+        setCostsPlanned({
+          value: parseInt(val.target.value) || undefined,
+          currency: costsPlanned.currency,
+        });
+      }}
+      onBlur={() => {
+        setIsEditing(false);
+        onInput(costsPlanned);
+      }}
+      value={
+        !isEditing && costsPlanned.value
+          ? new Intl.NumberFormat('de-DE', {
+              style: 'currency',
+              currency: costsPlanned.currency,
+            }).format(costsPlanned.value)
+          : costsPlanned.value
+      }
+    />
+  );
+};
+
+export default PlannedCostInput;

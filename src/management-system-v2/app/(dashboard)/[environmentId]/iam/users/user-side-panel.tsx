da23bfbaf70df6b845502597ca33e2c5fe9e182d
winniel24 <winnie.lam2424@gmail.com>
2024-02-07 15:27:01 +0100

    Process list tablet view (#222)
    * implemented design choices in processes.tsx mainly regarding the tablet view, adjusted bar to two lines for every view
    
    * fixed >> and <<, i still has some issues
    
    * changed XS and SM breakpoints, adjusted floatButton box shadow
    
    * CSS, prettier
    
    * fix bug
    
    * dynamic truncation of content itle, dynamic display of user page side panel
    
    * adjusting users page to responsive layout of processes
    
    * further adjustments in iam/users
    
    * Fix linting
    
    ---------
    
    Co-authored-by: LaMaLein <M.Fisch-22@web.de>
    Co-authored-by: Kai Rohwer <rohwer.kai@icloud.com>

--- src/management-system-v2/app/(dashboard)/[environmentId]/iam/users/user-side-panel.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/iam/users/user-side-panel.tsx
@@ -1,88 +1,95 @@
 'use client';
 
-import { FC, useRef } from 'react';
-import { Avatar, Typography } from 'antd';
+import { Dispatch, FC, PropsWithChildren, SetStateAction, useRef, useState } from 'react';
+import { Avatar, Drawer, Grid, Typography } from 'antd';
 import CollapsibleCard from '@/components/collapsible-card';
 import { useUserPreferences } from '@/lib/user-preferences';
 import ResizableElement, { ResizableElementRefType } from '@/components/ResizableElement';
 import { ListUser } from '@/components/user-list';
+import UserSiderContent from './user-sider-content';
 
-const UserSidePanel: FC<{ user: ListUser | null }> = ({ user }) => {
+type UserSidePanelProps = PropsWithChildren<{
+  user: ListUser | null;
+  setShowMobileUserSider: Dispatch<SetStateAction<boolean>>;
+  showMobileUserSider: boolean;
+}>;
+
+const UserSidePanel: FC<UserSidePanelProps> = ({
+  user,
+  setShowMobileUserSider,
+  showMobileUserSider,
+}) => {
   const setUserPreferences = useUserPreferences.use.addPreferences();
   const hydrated = useUserPreferences.use._hydrated();
   const sidePanelOpen = useUserPreferences(
     (store) => store.preferences['user-page-side-panel'].open,
   );
   const resizableElementRef = useRef<ResizableElementRefType>(null);
-
+  const breakpoint = Grid.useBreakpoint();
   const userFullName = user ? `${user.firstName.value} ${user.lastName.value}` : null;
+
+  const closeMobileUserSider = () => {
+    setShowMobileUserSider(false);
+  };
 
   if (!hydrated) return null;
   return (
-    <ResizableElement
-      initialWidth={
-        sidePanelOpen ? useUserPreferences.getState().preferences['user-page-side-panel'].width : 30
-      }
-      minWidth={300}
-      maxWidth={600}
-      style={{ position: 'relative' }}
-      onWidthChange={(width) =>
-        setUserPreferences({
-          'user-page-side-panel': {
-            open: sidePanelOpen,
-            width: width,
-          },
-        })
-      }
-      ref={resizableElementRef}
-    >
-      <CollapsibleCard
-        title={userFullName ?? ''}
-        show={sidePanelOpen}
-        collapsedWidth="30px"
-        onCollapse={() => {
-          const resizeCard = resizableElementRef.current;
-          const sidepanelWidth =
-            useUserPreferences.getState().preferences['user-page-side-panel'].width;
-
-          if (resizeCard) {
-            if (sidePanelOpen) resizeCard(30);
-            else resizeCard(sidepanelWidth);
+    <>
+      {breakpoint.xl ? (
+        <ResizableElement
+          initialWidth={
+            sidePanelOpen
+              ? useUserPreferences.getState().preferences['user-page-side-panel'].width
+              : 30
           }
+          minWidth={sidePanelOpen ? 300 : 30}
+          maxWidth={600}
+          style={{ position: 'relative' }}
+          onWidthChange={(width) =>
+            setUserPreferences({
+              'user-page-side-panel': {
+                open: sidePanelOpen,
+                width: width,
+              },
+            })
+          }
+          ref={resizableElementRef}
+        >
+          <CollapsibleCard
+            title={userFullName ?? 'How to PROCEED?'}
+            show={sidePanelOpen}
+            collapsedWidth="30px"
+            onCollapse={() => {
+              const resizeCard = resizableElementRef.current;
+              const sidepanelWidth =
+                useUserPreferences.getState().preferences['user-page-side-panel'].width;
 
-          setUserPreferences({
-            'user-page-side-panel': {
-              open: !sidePanelOpen,
-              width: sidepanelWidth,
-            },
-          });
-        }}
-      >
-        {user ? (
-          <>
-            <Avatar src={user.image} size={60} style={{ marginBottom: 20 }}>
-              {user.image
-                ? null
-                : user.firstName.value.slice(0, 1) + user.lastName.value.slice(0, 1)}
-            </Avatar>
+              if (resizeCard) {
+                if (sidePanelOpen) resizeCard(30);
+                else resizeCard(sidepanelWidth);
+              }
 
-            <Typography.Title>First Name</Typography.Title>
-            <Typography.Text>{user.firstName.value}</Typography.Text>
-
-            <Typography.Title>Last Name</Typography.Title>
-            <Typography.Text>{user.lastName.value}</Typography.Text>
-
-            <Typography.Title>Username</Typography.Title>
-            <Typography.Text>{user.username.value}</Typography.Text>
-
-            <Typography.Title>Email</Typography.Title>
-            <Typography.Text>{user.email.value}</Typography.Text>
-          </>
-        ) : (
-          <Typography.Text>Select an element.</Typography.Text>
-        )}
-      </CollapsibleCard>
-    </ResizableElement>
+              setUserPreferences({
+                'user-page-side-panel': {
+                  open: !sidePanelOpen,
+                  width: sidepanelWidth,
+                },
+              });
+            }}
+          >
+            <UserSiderContent user={user} />
+          </CollapsibleCard>
+        </ResizableElement>
+      ) : (
+        <Drawer
+          onClose={closeMobileUserSider}
+          title={<span>{userFullName}</span>}
+          open={showMobileUserSider}
+        >
+          <UserSiderContent user={user} />
+        </Drawer>
+      )}
+    </>
   );
 };
 

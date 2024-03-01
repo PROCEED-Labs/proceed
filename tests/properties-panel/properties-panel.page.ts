cfe6342ec4530598c37d1eb383bb45077afef766
Lucas <Lucasgold99@gmail.com>
2024-03-01 11:24:02 +0100

    improved playwright tests

--- tests/properties-panel/properties-panel.page.ts
+++ tests/properties-panel/properties-panel.page.ts
@@ -1,17 +1,24 @@
 import { Locator, Page } from '@playwright/test';
-import { PlatformPath } from 'path';
 import * as path from 'path';
 
 export class PropertiesPanelPage {
   readonly page: Page;
-  readonly path: PlatformPath;
+  readonly generalSection: Locator;
+  readonly imageSection: Locator;
+  readonly descriptionSection: Locator;
+  readonly milestonesSection: Locator;
+  readonly customPropertiesSection: Locator;
   processName?: string;
   processDescription?: string;
   processDefinitionID?: string;
 
   constructor(page: Page) {
     this.page = page;
-    this.path = path;
+    this.generalSection = page.getByTestId('generalSection');
+    this.imageSection = page.getByTestId('imageSection');
+    this.descriptionSection = page.getByTestId('descriptionSection');
+    this.milestonesSection = page.getByTestId('milestonesSection');
+    this.customPropertiesSection = page.getByTestId('customPropertiesSection');
   }
 
   async goto() {
@@ -62,6 +69,101 @@ export class PropertiesPanelPage {
     this.processDefinitionID = processDefinitionID;
     this.processName = processName;
     this.processDescription = description;
+  }
+
+  async addImage(imageFileName: string) {
+    const page = this.page;
+    const imageSection = this.imageSection;
+
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await imageSection.getByText('Add Image').click();
+    const fileChooser = await fileChooserPromise;
+    const imagePath = path.join(__dirname, imageFileName);
+    await fileChooser.setFiles(imagePath);
+
+    await page.waitForTimeout(1000); // wait until image is loaded
+  }
+
+  async editPlannedDuration(durationValues: {
+    years?: number;
+    months?: number;
+    days?: number;
+    hours?: number;
+    minutes?: number;
+    seconds?: number;
+  }) {
+    const { years, months, days, hours, minutes, seconds } = durationValues;
+    const page = this.page;
+    await page.getByTestId('plannedDurationInputEdit').click();
+
+    if (years) {
+      await page.locator('input[name="years"]').fill(`${years}`);
+    }
+
+    if (months) {
+      await page.locator('input[name="months"]').fill(`${months}`);
+    }
+
+    if (days) {
+      await page.locator('input[name="days"]').fill(`${days}`);
+    }
+
+    if (hours) {
+      await page.locator('input[name="hours"]').fill(`${hours}`);
+    }
+
+    if (minutes) {
+      await page.locator('input[name="minutes"]').fill(`${minutes}`);
+    }
+
+    if (seconds) {
+      await page.locator('input[name="seconds"]').fill(`${seconds}`);
+    }
+
+    await page.getByRole('button', { name: 'Save' }).click();
+    await page.waitForTimeout(500);
+  }
+
+  async addMilestone(milestoneValues: { ID: string; name: string; description?: string }) {
+    const { ID, name, description } = milestoneValues;
+    const page = this.page;
+    const milestonesSection = this.milestonesSection;
+
+    await milestonesSection.getByLabel('plus').click();
+    const milestonesModal = page.getByLabel('Create new Milestone');
+    await milestonesModal.getByPlaceholder('Milestone ID').fill(ID);
+    await milestonesModal.getByPlaceholder('Milestone Name').fill(name);
+
+    if (description) {
+      await milestonesModal
+        .locator('.toastui-editor-ww-container > .toastui-editor > .ProseMirror')
+        .fill(description);
+    }
+
+    await milestonesModal.getByRole('button', { name: 'Create Milestone' }).click();
+    await page.waitForTimeout(500); // wait until milestone modal is closed
+  }
+
+  async addDescription(descriptionText: string) {
+    const page = this.page;
+    const descriptionSection = page.getByTestId('descriptionSection');
+
+    await descriptionSection.getByLabel('edit').click(); // click edit description button
+    await page
+      .locator('.toastui-editor-ww-container > .toastui-editor > .ProseMirror')
+      .fill(descriptionText);
+    await page.getByRole('button', { name: 'Save' }).click();
+    await page.waitForTimeout(500); // wait until description dialog is closed
+  }
+
+  async addCustomProperty(name: string, value: string) {
+    const page = this.page;
+    const customPropertiesSection = this.customPropertiesSection;
+
+    await customPropertiesSection.getByPlaceholder('Custom Name').fill(name);
+    await customPropertiesSection.getByPlaceholder('Custom Value').fill(value);
+    await customPropertiesSection.locator('form').getByRole('button', { name: 'plus' }).click();
+    await page.waitForTimeout(500);
   }
 
   async removeAllProcesses() {
--- tests/properties-panel/properties-panel.spec.ts
+++ tests/properties-panel/properties-panel.spec.ts
@@ -1,60 +1,35 @@
+import { Page } from '@playwright/test';
 import { test, expect } from './properties-panel.fixtures';
 
-test('open properties panel for process', async ({ propertiesPanelPage }) => {
+test('open properties panel for process and fill property values', async ({
+  propertiesPanelPage,
+}) => {
   const openButton = propertiesPanelPage.page.getByRole('button', { name: 'info-circle' });
   await openButton.click();
 
-  const generalSection = propertiesPanelPage.page.getByTestId('generalSection');
+  expect(await propertiesPanelPage.generalSection.locator('input[name="Name"]').isDisabled()).toBe(
+    true,
+  );
+  expect(await propertiesPanelPage.generalSection.locator('input[name="Type"]').isDisabled()).toBe(
+    true,
+  );
 
-  expect(await generalSection.locator('input[name="Name"]').isDisabled()).toBe(true);
+  expect(await propertiesPanelPage.imageSection.innerText()).toEqual('Add Image');
+  await propertiesPanelPage.addImage('example-image.jpg');
+  expect(await propertiesPanelPage.imageSection.innerText()).toEqual('Edit Image');
 
-  expect(await generalSection.locator('input[name="Type"]').isDisabled()).toBe(true);
+  await expect(
+    propertiesPanelPage.descriptionSection.getByText('Process Description', { exact: true }),
+  ).toBeVisible();
+  await propertiesPanelPage.addDescription('New Description');
+  expect(propertiesPanelPage.descriptionSection).toContainText('New Description');
 
-  const imageSection = propertiesPanelPage.page.getByTestId('imageSection');
-
-  expect(await imageSection.innerText()).toEqual('Add Image');
-
-  const fileChooserPromise = propertiesPanelPage.page.waitForEvent('filechooser');
-  await imageSection.getByText('Add Image').click();
-  const fileChooser = await fileChooserPromise;
-  const imagePath = propertiesPanelPage.path.join(__dirname, 'example-image.jpg');
-  await fileChooser.setFiles(imagePath);
-
-  await propertiesPanelPage.page.waitForTimeout(1000); // wait until image is loaded
-
-  expect(await imageSection.innerText()).toEqual('Edit Image');
-
-  const descriptionSection = propertiesPanelPage.page.getByTestId('descriptionSection');
-  await expect(descriptionSection.getByText('Process Description', { exact: true })).toBeVisible();
-
-  await descriptionSection.getByLabel('edit').click(); // click edit description button
-  await propertiesPanelPage.page
-    .locator('.toastui-editor-ww-container > .toastui-editor > .ProseMirror')
-    .fill('New Description');
-  await propertiesPanelPage.page.getByRole('button', { name: 'Save' }).click();
-
-  await propertiesPanelPage.page.waitForTimeout(500); // wait until description dialog is closed
-
-  expect(descriptionSection).toContainText('New Description');
-
-  const milestonesSection = propertiesPanelPage.page.getByTestId('milestonesSection');
-
-  await expect(milestonesSection.getByText('No data')).toBeVisible();
-
-  await milestonesSection.getByLabel('plus').click();
-
-  const milestonesModal = propertiesPanelPage.page.getByLabel('Create new Milestone');
-
-  await milestonesModal.getByPlaceholder('Milestone ID').fill('123');
-  await milestonesModal.getByPlaceholder('Milestone Name').fill('Milestone A');
-  await milestonesModal
-    .locator('.toastui-editor-ww-container > .toastui-editor > .ProseMirror')
-    .fill('Milestone Description');
-
-  await milestonesModal.getByRole('button', { name: 'Create Milestone' }).click();
-
-  await propertiesPanelPage.page.waitForTimeout(500); // wait until milestone modal is closed
-
+  await expect(propertiesPanelPage.milestonesSection.getByText('No data')).toBeVisible();
+  await propertiesPanelPage.addMilestone({
+    ID: '123',
+    name: 'Milestone A',
+    description: 'Milestone Description',
+  });
   // TODO: Check if milestone entry was added to table
 
   expect(await propertiesPanelPage.page.getByPlaceholder('Planned Cost').inputValue()).toEqual('');
@@ -67,93 +42,107 @@ test('open properties panel for process', async ({ propertiesPanelPage }) => {
   expect(await propertiesPanelPage.page.getByPlaceholder('Planned Duration').inputValue()).toEqual(
     '',
   );
-
-  await propertiesPanelPage.page.getByTestId('plannedDurationInputEdit').click();
-
-  await propertiesPanelPage.page.locator('input[name="years"]').fill('1');
-  await propertiesPanelPage.page.locator('input[name="months"]').fill('2');
-  await propertiesPanelPage.page.locator('input[name="days"]').fill('3');
-  await propertiesPanelPage.page.getByRole('button', { name: 'Save' }).click();
-
-  propertiesPanelPage.page.waitForTimeout(500);
+  await propertiesPanelPage.editPlannedDuration({ years: 1, months: 2, days: 3 });
   expect(await propertiesPanelPage.page.getByPlaceholder('Planned Duration').inputValue()).toEqual(
     '1 Years, 2 Months, 3 Days',
   );
-
-  await propertiesPanelPage.page.getByTestId('plannedDurationInputEdit').click();
-
-  await propertiesPanelPage.page.locator('input[name="years"]').fill('1');
-  await propertiesPanelPage.page.locator('input[name="months"]').fill('2');
-  await propertiesPanelPage.page.locator('input[name="days"]').fill('3');
-  await propertiesPanelPage.page.locator('input[name="hours"]').fill('4');
-  await propertiesPanelPage.page.locator('input[name="minutes"]').fill('5');
-  await propertiesPanelPage.page.locator('input[name="seconds"]').fill('6');
-  await propertiesPanelPage.page.getByRole('button', { name: 'Save' }).click();
-
-  propertiesPanelPage.page.waitForTimeout(500);
+  await propertiesPanelPage.editPlannedDuration({
+    years: 1,
+    months: 2,
+    days: 3,
+    hours: 4,
+    minutes: 5,
+    seconds: 6,
+  });
   expect(await propertiesPanelPage.page.getByPlaceholder('Planned Duration').inputValue()).toEqual(
     '1 Y, 2 M, 3 D, 4 H, 5 M, 6 S ',
   );
 
-  const customPropertiesSection = propertiesPanelPage.page.getByTestId('customPropertiesSection');
-
-  expect(await customPropertiesSection.getByPlaceholder('Custom Name').inputValue()).toEqual('');
-  await customPropertiesSection.getByPlaceholder('Custom Name').fill('New Custom Property');
-  expect(await customPropertiesSection.getByPlaceholder('Custom Value').inputValue()).toEqual('');
-  await customPropertiesSection.getByPlaceholder('Custom Value').fill('New Custom Value');
-
-  customPropertiesSection.locator('form').getByRole('button', { name: 'plus' }).click();
-  await propertiesPanelPage.page.waitForTimeout(500);
-
-  expect(await customPropertiesSection.getByPlaceholder('Custom Name').count()).toEqual(2);
-  expect(await customPropertiesSection.getByPlaceholder('Custom Value').count()).toEqual(2);
   expect(
-    await customPropertiesSection.getByPlaceholder('Custom Name').first().inputValue(),
+    await propertiesPanelPage.customPropertiesSection.getByPlaceholder('Custom Name').inputValue(),
+  ).toEqual('');
+  expect(
+    await propertiesPanelPage.customPropertiesSection.getByPlaceholder('Custom Value').inputValue(),
+  ).toEqual('');
+  await propertiesPanelPage.addCustomProperty('New Custom Property', 'New Custom Value');
+  expect(
+    await propertiesPanelPage.customPropertiesSection.getByPlaceholder('Custom Name').count(),
+  ).toEqual(2);
+  expect(
+    await propertiesPanelPage.customPropertiesSection.getByPlaceholder('Custom Value').count(),
+  ).toEqual(2);
+  expect(
+    await propertiesPanelPage.customPropertiesSection
+      .getByPlaceholder('Custom Name')
+      .first()
+      .inputValue(),
   ).toEqual('New Custom Property');
   expect(
-    await customPropertiesSection.getByPlaceholder('Custom Value').first().inputValue(),
+    await propertiesPanelPage.customPropertiesSection
+      .getByPlaceholder('Custom Value')
+      .first()
+      .inputValue(),
   ).toEqual('New Custom Value');
-  expect(await customPropertiesSection.getByPlaceholder('Custom Name').nth(1).inputValue()).toEqual(
-    '',
-  );
   expect(
-    await customPropertiesSection.getByPlaceholder('Custom Value').nth(1).inputValue(),
+    await propertiesPanelPage.customPropertiesSection
+      .getByPlaceholder('Custom Name')
+      .nth(1)
+      .inputValue(),
   ).toEqual('');
-
-  await customPropertiesSection.getByRole('button', { name: 'delete' }).click();
-
-  expect(await customPropertiesSection.getByPlaceholder('Custom Name').count()).toEqual(1);
-  expect(await customPropertiesSection.getByPlaceholder('Custom Name').inputValue()).toEqual('');
-  expect(await customPropertiesSection.getByPlaceholder('Custom Value').count()).toEqual(1);
-  expect(await customPropertiesSection.getByPlaceholder('Custom Value').inputValue()).toEqual('');
+  expect(
+    await propertiesPanelPage.customPropertiesSection
+      .getByPlaceholder('Custom Value')
+      .nth(1)
+      .inputValue(),
+  ).toEqual('');
+  await propertiesPanelPage.customPropertiesSection.getByRole('button', { name: 'delete' }).click();
+  expect(
+    await propertiesPanelPage.customPropertiesSection.getByPlaceholder('Custom Name').count(),
+  ).toEqual(1);
+  expect(
+    await propertiesPanelPage.customPropertiesSection.getByPlaceholder('Custom Name').inputValue(),
+  ).toEqual('');
+  expect(
+    await propertiesPanelPage.customPropertiesSection.getByPlaceholder('Custom Value').count(),
+  ).toEqual(1);
+  expect(
+    await propertiesPanelPage.customPropertiesSection.getByPlaceholder('Custom Value').inputValue(),
+  ).toEqual('');
 });
 
-test('open properties panel for element', async ({ propertiesPanelPage }) => {
+test('open properties panel for element and fill property values', async ({
+  propertiesPanelPage,
+}) => {
   await propertiesPanelPage.page.locator('rect').first().click(); // click on start event (which is the only element in this process)
 
   const openButton = propertiesPanelPage.page.getByRole('button', { name: 'info-circle' });
   await openButton.click();
 
-  const generalSection = propertiesPanelPage.page.getByTestId('generalSection');
-  expect(await generalSection.locator('input[name="Name"]').inputValue()).toEqual('');
-
-  await generalSection.getByTestId('nameInputEdit').click();
-
-  await generalSection.locator('input[name="Name"]').fill('New Name');
-  expect(await generalSection.locator('input[name="Name"]').inputValue()).toEqual('New Name');
-
-  expect(await generalSection.locator('input[name="Type"]').isDisabled()).toBe(true);
-  expect(await generalSection.locator('input[name="Type"]').inputValue()).toEqual(
-    'bpmn:StartEvent',
+  expect(
+    await propertiesPanelPage.generalSection.locator('input[name="Name"]').inputValue(),
+  ).toEqual('');
+  await propertiesPanelPage.generalSection.getByTestId('nameInputEdit').click();
+  await propertiesPanelPage.generalSection.locator('input[name="Name"]').fill('New Name');
+  expect(
+    await propertiesPanelPage.generalSection.locator('input[name="Name"]').inputValue(),
+  ).toEqual('New Name');
+  expect(await propertiesPanelPage.generalSection.locator('input[name="Type"]').isDisabled()).toBe(
+    true,
   );
+  expect(
+    await propertiesPanelPage.generalSection.locator('input[name="Type"]').inputValue(),
+  ).toEqual('bpmn:StartEvent');
+
+  expect(await propertiesPanelPage.imageSection.innerText()).toEqual('Add Image');
+  await propertiesPanelPage.addImage('example-image.jpg');
+  expect(await propertiesPanelPage.imageSection.innerText()).toEqual('Edit Image');
 
   const descriptionText = await propertiesPanelPage.page
     .getByTestId('descriptionViewer')
     .innerText();
   expect(descriptionText).toEqual('');
 
-  const milestonesSection = propertiesPanelPage.page.getByTestId('milestonesSection');
-  await expect(milestonesSection.getByText('No data')).toBeVisible();
+  await expect(propertiesPanelPage.milestonesSection.getByText('No data')).toBeVisible();
 
   expect(await propertiesPanelPage.page.getByPlaceholder('Planned Cost').inputValue()).toEqual('');
   expect(await propertiesPanelPage.page.getByPlaceholder('Planned Duration').inputValue()).toEqual(

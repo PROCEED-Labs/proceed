779d73597306932a02b497cb138f20ca77e6ba97
Felipe Trost <felipe.trost@gmail.com>
2024-03-06 17:18:38 +0100

    feat: server action for moving items into folders

--- src/management-system-v2/lib/data/folders.ts
+++ src/management-system-v2/lib/data/folders.ts
@@ -2,8 +2,17 @@
 
 import { getCurrentEnvironment, getCurrentUser } from '@/components/auth';
 import { FolderUserInput, FolderUserInputSchema } from './folder-schema';
-import { createFolder as _createFolder, getRootFolder } from './legacy/folders';
-import { userError } from '../user-error';
+import {
+  FolderChildren,
+  createFolder as _createFolder,
+  foldersMetaObject,
+  getFolderById,
+  getRootFolder,
+  moveFolder,
+} from './legacy/folders';
+import { UserErrorType, userError } from '../user-error';
+import { toCaslResource } from '../ability/caslAbility';
+import { getProcess, moveProcess } from './legacy/_process';
 
 export async function createFolder(folderInput: FolderUserInput) {
   try {
@@ -16,5 +25,25 @@ export async function createFolder(folderInput: FolderUserInput) {
     _createFolder({ ...folder, createdBy: userId }, ability);
   } catch (e) {
     return userError("Couldn't create folder");
+  }
+}
+
+export async function moveIntoFolder(item: FolderChildren, folderId: string) {
+  const folder = getFolderById(folderId);
+  if (!folder) return userError('Folder not found');
+
+  const { ability } = await getCurrentEnvironment(folder.environmentId);
+
+  if (!ability.can('update', toCaslResource('Folder', folder)))
+    return userError('Permission denied');
+
+  if (['process', 'project', 'process-instance'].includes(item.type)) {
+    moveProcess({
+      processDefinitionsId: item.id,
+      newFolderId: folderId,
+      ability: ability,
+    });
+  } else if (item.type === 'folder') {
+    moveFolder(item.id, folderId, ability);
   }
 }

ed43d29fff0387119cb7974aa38167c0ef5b4907
Felipe Trost <felipe.trost@gmail.com>
2024-03-10 19:20:51 +0100

    fix: removed signin-options and turned signin card into modal

--- src/management-system-v2/app/(auth)/signin/login.tsx
+++ src/management-system-v2/app/(auth)/signin/login.tsx
@@ -1,14 +1,23 @@
 'use client';
 
-import { FC } from 'react';
-import { Space, Card, Typography, Alert } from 'antd';
+import { FC, Fragment, ReactNode, useEffect, useState } from 'react';
+import {
+  Typography,
+  Alert,
+  Form,
+  Input,
+  Button,
+  Image as AntDesignImage,
+  Divider,
+  Modal,
+} from 'antd';
 
 import styles from './login.module.scss';
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
-import { type ExtractedProvider } from './page';
-import SignInOptions from '@/components/signin-options';
+import { signIn } from 'next-auth/react';
+import { type ExtractedProvider } from '@/app/api/auth/[...nextauth]/auth-options';
 
 const Login: FC<{
   providers: ExtractedProvider[];
@@ -17,45 +26,122 @@ const Login: FC<{
   const callbackUrl = searchParams.get('callbackUrl') ?? '/';
   const authError = searchParams.get('error');
 
+  // We need to wait until the component is mounted on the client
+  // to open the modal, otherwise it will cause a hydration mismatch
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    setOpen(true);
+  }, [setOpen]);
+
   return (
-    <Space
-      direction="horizontal"
+    <Modal
+      title={
+        <Image
+          src="/proceed.svg"
+          alt="PROCEED Logo"
+          width={160}
+          height={63}
+          priority
+          style={{ marginBottom: '1rem', display: 'block', margin: 'auto' }}
+        />
+      }
+      open={open}
+      closeIcon={null}
+      footer={null}
       style={{
-        width: '100%',
-        minHeight: '100vh',
-        justifyContent: 'center',
-        backgroundColor: '#f5f5f5',
+        maxWidth: '400px',
+        width: '90%',
       }}
+      styles={{
+        mask: { backdropFilter: 'blur(5px)' },
+      }}
+      className={styles.Card}
     >
-      <div style={{ marginTop: '-10rem' }}>
-        <Card
-          title={
-            <Image
-              src="/proceed.svg"
-              alt="PROCEED Logo"
-              width={160}
-              height={63}
-              priority
-              style={{ marginBottom: '1rem' }}
-            />
-          }
-          className={styles.Card}
-        >
-          <Typography.Title level={5} style={{ marginBottom: '1rem' }}>
-            Sign in
-          </Typography.Title>
+      <Typography.Title level={4} style={{ marginBottom: '2rem', textAlign: 'center' }}>
+        Sign in
+      </Typography.Title>
 
-          {authError && (
-            <Alert description={authError} type="error" style={{ marginBottom: '2rem' }} />
-          )}
+      {authError && <Alert description={authError} type="error" style={{ marginBottom: '2rem' }} />}
 
-          <SignInOptions providers={providers} callbackUrl={callbackUrl} />
-        </Card>
-        <Typography.Text style={{ display: 'block', textAlign: 'center', padding: '1rem' }}>
-          By signing in, you agree to our <Link href="/terms">Terms of Service</Link>
-        </Typography.Text>
-      </div>
-    </Space>
+      {providers.map((provider, idx) => {
+        let loginMethod: ReactNode;
+        if (provider.type === 'credentials') {
+          loginMethod = (
+            <Form
+              onFinish={(values) => signIn(provider.id, { ...values, callbackUrl })}
+              key={provider.id}
+              layout="vertical"
+            >
+              {Object.keys(provider.credentials).map((key) => (
+                <Form.Item name={key} key={key} style={{ marginBottom: '.5rem' }}>
+                  <Input placeholder={provider.credentials[key].label} />
+                </Form.Item>
+              ))}
+              <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                Continue with {provider.name}
+              </Button>
+            </Form>
+          );
+        } else if (provider.type === 'oauth') {
+          loginMethod = (
+            <Button
+              key={idx}
+              style={{
+                backgroundColor: provider.style?.bg,
+                color: provider.style?.text,
+                width: '100%',
+                marginBottom: '.5rem',
+              }}
+              icon={
+                <AntDesignImage
+                  width={20}
+                  src={`https://authjs.dev/img/providers${provider.style?.logo}`}
+                  alt={provider.name}
+                />
+              }
+              onClick={() => signIn(provider.id, { callbackUrl })}
+            >
+              Continue with {provider.name}
+            </Button>
+          );
+        } else if (provider.type === 'email') {
+          loginMethod = (
+            <Form
+              onFinish={(values) => signIn(provider.id, { ...values, callbackUrl })}
+              key={provider.id}
+              layout="vertical"
+            >
+              <Form.Item
+                name="email"
+                rules={[{ type: 'email', required: true }]}
+                style={{ marginBottom: '.5rem' }}
+              >
+                <Input placeholder="Email" />
+              </Form.Item>
+              <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                Continue with {provider.name}
+              </Button>
+            </Form>
+          );
+        }
+
+        return (
+          <Fragment key={provider.id}>
+            {loginMethod}
+            {idx < providers.length - 1 && provider.type !== 'oauth' && (
+              <Divider style={{ color: 'black' }}>
+                <Typography.Text style={{ display: 'block', textAlign: 'center', padding: '1rem' }}>
+                  OR
+                </Typography.Text>
+              </Divider>
+            )}
+          </Fragment>
+        );
+      })}
+      <Typography.Text style={{ display: 'block', textAlign: 'center', padding: '1rem' }}>
+        By signing in, you agree to our <Link href="/terms">Terms of Service</Link>
+      </Typography.Text>
+    </Modal>
   );
 };
 

5732fb39212d61bf7eeafda0befaff47d6b64c6d
Felipe Trost <felipe.trost@gmail.com>
2024-03-12 20:04:07 +0100

    feat: moved folder modal into separate component

--- src/management-system-v2/components/folder-creation-button.tsx
+++ src/management-system-v2/components/folder-creation-button.tsx
@@ -1,92 +1,60 @@
 'use client';
 
-import React, { ReactNode, useState, useTransition } from 'react';
-import { App, Button, Form, Input, Modal } from 'antd';
+import { FC, ReactNode, useTransition } from 'react';
+import { App, Button } from 'antd';
 import type { ButtonProps } from 'antd';
-import ProcessModal from './process-modal';
-import { addProcesses } from '@/lib/data/processes';
-import { useParams, useRouter, useSelectedLayoutSegments } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { useEnvironment } from './auth-can';
-import useParseZodErrors, { antDesignInputProps } from '@/lib/useParseZodErrors';
-import { FolderUserInputSchema } from '@/lib/data/folder-schema';
+import { FolderUserInput } from '@/lib/data/folder-schema';
 import { createFolder as serverCreateFolder } from '@/lib/data/folders';
-import TextArea from 'antd/es/input/TextArea';
+import useFolderModal from './folder-modal';
 
-type ProcessCreationButtonProps = ButtonProps & {
+type FolderCreationButtonProps = ButtonProps & {
   wrapperElement?: ReactNode;
 };
 
-/**
- *
- * Button to create Processes including a Modal for inserting needed values. Alternatively, a custom wrapper element can be used instead of a button.
- */
-const FolderCreationButton: React.FC<ProcessCreationButtonProps> = ({
-  wrapperElement,
-  ...props
-}) => {
+const FolderCreationButton: FC<FolderCreationButtonProps> = ({ wrapperElement, ...props }) => {
   const { message } = App.useApp();
-  const [form] = Form.useForm();
-  const [modalOpen, setModalOpen] = useState(false);
   const router = useRouter();
   const environmentId = useEnvironment();
   const folderId = useParams<{ folderId: string }>().folderId ?? '';
   const [isLoading, startTransition] = useTransition();
-  const [errors, parseInput] = useParseZodErrors(FolderUserInputSchema);
 
-  const createFolder = (values: Record<string, any>) => {
+  const createFolder = (values: FolderUserInput) => {
     startTransition(async () => {
       try {
-        const folderInput = parseInput({ ...values, parentId: folderId, environmentId });
-        if (!folderInput) throw new Error();
-
-        const response = await serverCreateFolder(folderInput);
+        const response = await serverCreateFolder(values);
         if (response && 'error' in response) throw new Error();
 
         router.refresh();
         message.open({ type: 'success', content: 'Folder Created' });
-        setModalOpen(false);
+        close();
       } catch (e) {
         message.open({ type: 'error', content: 'Something went wrong' });
       }
     });
   };
 
+  const { modal, open, close } = useFolderModal({
+    spaceId: environmentId,
+    parentId: folderId,
+    onSubmit: createFolder,
+    modalProps: {
+      title: 'Create Folder',
+      okButtonProps: { loading: isLoading },
+    },
+  });
+
   return (
     <>
       {wrapperElement ? (
-        <div onClick={() => setModalOpen(true)}>{wrapperElement}</div>
+        <div onClick={open}>{wrapperElement}</div>
       ) : (
-        <Button {...props} onClick={() => setModalOpen(true)}>
+        <Button {...props} onClick={open}>
           Create Folder
         </Button>
       )}
-      <Modal
-        title="Create Folder"
-        closeIcon={null}
-        open={modalOpen}
-        destroyOnClose
-        onOk={form.submit}
-        onCancel={() => setModalOpen(false)}
-        okButtonProps={{ loading: isLoading }}
-      >
-        <Form onFinish={createFolder} form={form} layout="vertical">
-          <Form.Item
-            name="name"
-            label="Folder name"
-            required
-            {...antDesignInputProps(errors, 'name')}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="description"
-            label="Description"
-            {...antDesignInputProps(errors, 'description')}
-          >
-            <TextArea />
-          </Form.Item>
-        </Form>
-      </Modal>
+      {modal}
     </>
   );
 };
---
+++ src/management-system-v2/components/folder-modal.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Form, Input, Modal } from 'antd';
+import type { ModalProps } from 'antd';
+import useParseZodErrors, { antDesignInputProps } from '@/lib/useParseZodErrors';
+import { FolderUserInput, FolderUserInputSchema } from '@/lib/data/folder-schema';
+import TextArea from 'antd/es/input/TextArea';
+
+type FolderModalProps = {
+  spaceId: string;
+  parentId: string;
+  onSubmit: (values: FolderUserInput) => void;
+  modalProps: ModalProps;
+};
+
+const useFolderModal = ({ spaceId, parentId, onSubmit, modalProps }: FolderModalProps) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [form] = Form.useForm();
+  const [errors, parseInput] = useParseZodErrors(FolderUserInputSchema);
+
+  function checkInput(formInput: any) {
+    const values = parseInput({ ...formInput, environmentId: spaceId, parentId });
+    if (!values) return;
+
+    onSubmit(values);
+  }
+
+  const modal = (
+    <Modal
+      title="Folder"
+      closeIcon={null}
+      destroyOnClose
+      {...modalProps}
+      open={modalOpen}
+      onOk={form.submit}
+      onCancel={() => setModalOpen(false)}
+    >
+      <Form onFinish={checkInput} form={form} layout="vertical">
+        <Form.Item
+          name="name"
+          label="Folder name"
+          required
+          {...antDesignInputProps(errors, 'name')}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="description"
+          label="Description"
+          {...antDesignInputProps(errors, 'description')}
+        >
+          <TextArea />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+
+  return {
+    modal,
+    open: () => setModalOpen(true),
+    close: () => setModalOpen(false),
+    errors,
+  };
+};
+
+export default useFolderModal;

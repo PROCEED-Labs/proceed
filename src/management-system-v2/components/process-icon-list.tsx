541683a9ff6a7fb80a967075e373faff81cd5f7c
Felipe Trost <felipe.trost@gmail.com>
2024-03-25 03:30:15 +0100

    feat(processes:icon-view): adapted ui to folders

--- src/management-system-v2/components/process-icon-list.tsx
+++ src/management-system-v2/components/process-icon-list.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { Dispatch, FC, Key, SetStateAction } from 'react';
+import { Dispatch, FC, Key, SetStateAction } from 'react';
 import styles from './process-icon-list.module.scss';
 import cn from 'classnames';
 
@@ -11,7 +11,7 @@ import { ProcessListProcess } from './processes';
 import { Grid } from 'antd';
 
 type IconViewProps = {
-  data?: ProcessListProcess[];
+  data: ProcessListProcess[];
   selection: Key[];
   setSelectionElements: Dispatch<SetStateAction<ProcessListProcess[]>>;
   setShowMobileMetaData: Dispatch<SetStateAction<boolean>>;
@@ -24,9 +24,42 @@ const IconView: FC<IconViewProps> = ({
   setShowMobileMetaData,
 }) => {
   const breakpoint = Grid.useBreakpoint();
+  const folders = data.filter((item) => item.type === 'folder') as Extract<
+    ProcessListProcess,
+    { type: 'folder' }
+  >[];
+  const processes = data.filter((item) => item.type !== 'folder') as Exclude<
+    ProcessListProcess,
+    { type: 'folder' }
+  >[];
+
   return (
     <>
       <ScrollBar width="12px">
+        {folders.length > 0 && (
+          <div
+            className={cn(breakpoint.xs ? styles.MobileIconView : styles.IconView)}
+            style={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+              justifyContent: 'space-between',
+              gridGap: '20px',
+              marginBottom: '20px',
+            }}
+          >
+            {folders.map((item) => (
+              <TabCard
+                setShowMobileMetaData={setShowMobileMetaData}
+                item={item}
+                completeList={data!}
+                selection={selection}
+                setSelectionElements={setSelectionElements}
+                tabcard={false}
+              />
+            ))}
+          </div>
+        )}
+
         <div
           className={cn(breakpoint.xs ? styles.MobileIconView : styles.IconView)}
           style={{
@@ -36,10 +69,9 @@ const IconView: FC<IconViewProps> = ({
             gridGap: '20px',
           }}
         >
-          {data?.map((item) => (
+          {processes.map((item) => (
             <TabCard
               setShowMobileMetaData={setShowMobileMetaData}
-              key={item.id}
               item={item}
               completeList={data!}
               selection={selection}

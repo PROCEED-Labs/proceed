d0f5c41acb71246d0200b7844e0a82ea19c5dc16
Felipe Trost <felipe.trost@gmail.com>
2024-04-29 12:30:32 +0200

    fix(FolderTree): make folder with no children leafs

--- src/management-system-v2/components/FolderTree.tsx
+++ src/management-system-v2/components/FolderTree.tsx
@@ -2,8 +2,8 @@
 
 import { getFolderChildren } from '@/lib/data/folders';
 import { Tree, TreeProps } from 'antd';
-type DirectoryTreeProps = ComponentProps<typeof Tree.DirectoryTree>;
-import React, { ComponentProps, useEffect, useRef, useState } from 'react';
+import { FolderOutlined, FolderOpenOutlined, FileOutlined } from '@ant-design/icons';
+import React, { useEffect, useRef, useState } from 'react';
 import { useEnvironment } from './auth-can';
 
 type FolderChildren = {
@@ -14,12 +14,22 @@ type FolderChildren = {
 
 type TreeNode = NonNullable<TreeProps['treeData']>[number] & { element: FolderChildren };
 
+const getIcon: TreeNode['icon'] = (element) => {
+  // @ts-ignore
+  if (element.element.type && element.element.type !== 'folder') return <FileOutlined />;
+
+  if (!element.expanded || element.isLeaf) return <FolderOutlined />;
+
+  return <FolderOpenOutlined />;
+};
+
 function generateNode(element: FolderChildren): TreeNode {
   let isLeaf = false;
 
   if (element.type && element.type !== 'folder') isLeaf = true;
 
   return {
+    icon: getIcon,
     key: element.id,
     title: element.name,
     isLeaf,
@@ -35,7 +45,7 @@ export const FolderTree = ({
   rootNodes?: FolderChildren[];
   /** The return value is used to update the tree */
   newChildrenHook?: (nodes: TreeNode[], parent?: TreeNode) => TreeNode[];
-  treeProps?: DirectoryTreeProps;
+  treeProps?: TreeProps;
 }) => {
   const spaceId = useEnvironment().spaceId;
 
@@ -60,7 +70,6 @@ export const FolderTree = ({
 
     const children = await getFolderChildren(spaceId, nodeId);
     if ('error' in children) return;
-    if (children.length === 0) return;
 
     let childrenNodes = children.map(generateNode);
 
@@ -71,6 +80,8 @@ export const FolderTree = ({
 
     if (nodeId) {
       actualNode!.children = childrenNodes;
+      actualNode!.isLeaf = childrenNodes.length === 0;
+
       // trigger re-render
       setTree((currentTree) => [...currentTree]);
     } else {
@@ -84,5 +95,5 @@ export const FolderTree = ({
     }
   }, [rootNodes]);
 
-  return <Tree.DirectoryTree {...treeProps} treeData={tree} loadedKeys={[]} loadData={loadData} />;
+  return <Tree showIcon={true} {...treeProps} treeData={tree} loadData={loadData} />;
 };

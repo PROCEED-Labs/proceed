2e25402678bf4377c4ee9f5eb72dca029ad90846
Felipe Trost <felipe.trost@gmail.com>
2024-05-03 10:46:18 +0200

    feat(process-list tests): test toggle column

--- tests/ms2/process-list/process-list.spec.ts
+++ tests/ms2/process-list/process-list.spec.ts
@@ -393,3 +393,28 @@ test('share-modal-test', async ({ processListPage }) => {
   await newPage.waitForURL(/processes/);
   await expect(newPage.locator(`tr[data-row-key="${newProcessId}"]`)).toBeVisible();
 });
+
+test('toggle process list columns', async ({ processListPage }) => {
+  const { page } = processListPage;
+
+  const ColumnHeader = ['Name', 'Description', 'Last Edited', 'Created On', 'File Size', 'Owner'];
+
+  const toggleMenu = () =>
+    page.getByRole('columnheader', { name: 'more' }).getByRole('button', { name: 'more' }).click();
+
+  for (const column of ColumnHeader) {
+    const checkbox = page.getByRole('checkbox', { name: column });
+
+    await toggleMenu(); // open
+    expect(checkbox).toBeVisible();
+    if (!(await checkbox.isChecked())) await checkbox.check();
+    await expect(page.getByRole('columnheader', { name: column })).toBeVisible();
+    await toggleMenu(); // close
+
+    await toggleMenu(); // open
+    expect(checkbox).toBeVisible();
+    if (await checkbox.isChecked()) await checkbox.uncheck();
+    await expect(page.getByRole('columnheader', { name: column })).not.toBeVisible();
+    await toggleMenu(); // close
+  }
+});

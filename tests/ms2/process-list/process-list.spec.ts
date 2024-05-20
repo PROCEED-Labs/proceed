43ae42deb25db71c2a3717977e579d052e648d85
Felipe Trost <felipe.trost@gmail.com>
2024-05-20 14:39:15 +0200

    fix(e2e-tests): deleted Name column from tests because it cannot be toggled anymore

--- tests/ms2/process-list/process-list.spec.ts
+++ tests/ms2/process-list/process-list.spec.ts
@@ -397,7 +397,7 @@ test('share-modal-test', async ({ processListPage }) => {
 test('toggle process list columns', async ({ processListPage }) => {
   const { page } = processListPage;
 
-  const ColumnHeader = ['Name', 'Description', 'Last Edited', 'Created On', 'File Size', 'Owner'];
+  const ColumnHeader = ['Description', 'Last Edited', 'Created On', 'File Size', 'Owner'];
 
   const toggleMenu = () =>
     page.getByRole('columnheader', { name: 'more' }).getByRole('button', { name: 'more' }).click();
@@ -423,7 +423,7 @@ test('test that selected columns are persisted on reload', async ({ processListP
   const { page } = processListPage;
 
   const VisibleColumns = ['Created On', 'File Size', 'Owner'];
-  const HiddenColumns = ['Name', 'Description', 'Last Edited'];
+  const HiddenColumns = ['Description', 'Last Edited'];
 
   const toggleMenu = () =>
     page.getByRole('columnheader', { name: 'more' }).getByRole('button', { name: 'more' }).click();

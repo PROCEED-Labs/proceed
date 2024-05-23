8b47f4518843c5a3c414a8a56ced277afb3b5757
bowserjuno <118111794+bowserjuno@users.noreply.github.com>
2024-05-23 17:10:00 +0200

    improve tool use
    tool definitions in extra json file

--- src/management-system-v2/components/bpmn-chatbot.tsx
+++ src/management-system-v2/components/bpmn-chatbot.tsx
@@ -2,9 +2,15 @@ import { BPMNCanvasRef } from '@/components/bpmn-canvas';
 import { Button, Card, Form, Input, List, Space, Tooltip } from 'antd';
 import React, { useState } from 'react';
 import { getNewShapePosition } from 'bpmn-js/lib/features/auto-place/BpmnAutoPlaceUtil';
-import { Shape } from 'bpmn-js/lib/model/Types';
+import { Element, Shape } from 'bpmn-js/lib/model/Types';
 import { MessageOutlined } from '@ant-design/icons';
 import ChatbotResponseModal, { ChatbotInteraction } from './bpmn-chatbot-response';
+
+/*For chatbots that have features like tool use or function calling.
+Defines the modeler functionality through JSON Scheme (see: https://json-schema.org/).*/
+import tools from './bpmn-chatbot-tools.json';
+
+import { Point } from 'bpmn-js/lib/features/modeling/BpmnFactory';
 
 type ChatbotDialogProps = {
   show: boolean;
@@ -26,9 +32,9 @@ const ChatbotDialog: React.FC<ChatbotDialogProps> = ({ show, modeler }) => {
 
   function onPrompt({ prompt }: FieldType) {
     setWaitForResponse(true);
-    getProcessXml()
-      .then((process) => {
-        fetchClaude(prompt, process).then((res) => {
+    getProcessXml().then((process) => {
+      fetchClaude(prompt, process)
+        .then((res) => {
           processResponse(res.content);
           setLastPrompts(
             lastPrompts.concat({
@@ -37,53 +43,106 @@ const ChatbotDialog: React.FC<ChatbotDialogProps> = ({ show, modeler }) => {
               chatbotResponse: res.content,
             }),
           );
-        });
-      })
-      .finally(() => setWaitForResponse(false));
+        })
+        .finally(() => setWaitForResponse(false));
+    });
   }
 
+  //see tools definitions
+  function create_element(
+    bpmn_type: string,
+    created: Shape[],
+    source_element_id_or_name?: string,
+    new_element_name?: string,
+    connection_label?: string,
+    position?: Point,
+  ): void {
+    const element = elementFactory.createShape({
+      type: 'bpmn:' + bpmn_type,
+    });
+    let shape: Shape;
+    if (source_element_id_or_name) {
+      //append element to existing one
+      let sourceShape = modeler?.getElement(source_element_id_or_name);
+      if (!sourceShape) {
+        sourceShape = created.find((e) => e.name == source_element_id_or_name)!;
+      }
+      const point = getNewShapePosition(sourceShape as Shape, element);
+      shape = modeling.appendShape(sourceShape, element, point, root!, {
+        connection: { name: connection_label, type: 'bpmn:SequenceFlow' },
+      });
+    } else {
+      shape = modeling.createShape(element, position!, root!);
+    }
+    if (new_element_name) {
+      modeling.updateLabel(shape, new_element_name);
+    }
+    created.push(shape);
+  }
+  function create_connection(
+    source_element_id_or_name: string,
+    target_element_id_or_name: string,
+    created: Shape[],
+    label?: string,
+  ): void {
+    let sourceShape = modeler?.getElement(source_element_id_or_name);
+    if (!sourceShape) {
+      sourceShape = created.find((e) => e.name == source_element_id_or_name)!;
+    }
+    let targetShape = modeler?.getElement(target_element_id_or_name);
+    if (!targetShape) {
+      targetShape = created.find((e) => e.name == target_element_id_or_name)!;
+    }
+    const connection = modeling.createConnection(
+      sourceShape,
+      targetShape,
+      { type: 'bpmn:SequenceFlow' },
+      root!,
+    );
+    if (label) {
+      modeling.updateLabel(connection, label);
+    }
+  }
+  function remove_elements(element_ids: string[]): void {
+    const elements: Element[] = [];
+    element_ids.forEach((id) => {
+      let element = modeler?.getElement(id);
+      if (element) {
+        elements.push(element);
+      }
+    });
+    modeling.removeElements(elements);
+  }
+
+  //parsing tool uses listed in response
   function processResponse(response: any[]) {
     const created: Shape[] = [];
     response.forEach((res) => {
       if (res.type == 'tool_use') {
-        if (res.name == 'create_and_append_task') {
-          const task = elementFactory.createShape({
-            type: 'bpmn:Task',
-            name: res.input.new_task_name,
-          });
-          let sourceShape = modeler?.getElement(res.input.source_task_id_or_name) as Shape;
-          if (!sourceShape) {
-            sourceShape = created.find((e) => e.name == res.input.source_task_id_or_name)!;
-          }
-          const point = getNewShapePosition(sourceShape, task);
-          modeling.appendShape(sourceShape, task, point, root!);
-          created.push(task);
-        } else if (res.name == 'create_end') {
-          const endEvent = elementFactory.createShape({
-            type: 'bpmn:EndEvent',
-          });
-          let sourceShape = modeler?.getElement(res.input.element_id_or_name) as Shape;
-          if (!sourceShape) {
-            sourceShape = created.find((e) => e.name == res.input.element_id_or_name)!;
-          }
-          const point = getNewShapePosition(sourceShape, endEvent);
-          modeling.appendShape(sourceShape, endEvent, point, root!);
-          created.push(endEvent);
-        } else if (res.name == 'create_start') {
-          const startEvent = modeling.createShape(
-            {
-              type: 'bpmn:StartEvent',
-              name: res.input.name,
-            },
-            { x: 350, y: 200 },
-            root!,
+        if (res.name == 'create_element') {
+          create_element(
+            res.input.bpmn_type,
+            created,
+            res.input.source_element_id_or_name,
+            res.input.new_element_name,
+            res.input.connection_label,
+            res.input.position,
           );
-          created.push(startEvent);
+        } else if (res.name == 'create_connection') {
+          create_connection(
+            res.input.source_element_id_or_name,
+            res.input.target_element_id_or_name,
+            created,
+            res.input.label,
+          );
+        } else if (res.name == 'remove_elements') {
+          remove_elements(res.input.element_ids);
         }
       }
     });
   }
 
+  //get current xml of the <process>...</process> part only
   function getProcessXml(): Promise<string> {
     return modeler!.getXML().then((res) => {
       if (res) {
@@ -99,13 +158,14 @@ const ChatbotDialog: React.FC<ChatbotDialogProps> = ({ show, modeler }) => {
     });
   }
 
+  //prompting an external service for the chatbot
   function fetchClaude(userPrompt: string, process: string): Promise<any> {
     return fetch('http://localhost:2000/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ userPrompt: userPrompt, process: process }),
+      body: JSON.stringify({ userPrompt: userPrompt, process: process, tools: tools }),
     }).then((res) => res.json());
   }
 

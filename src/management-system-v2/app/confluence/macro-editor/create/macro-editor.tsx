2b4ab9341aba2e516ada3df7b6474ea939315422
Lucas <Lucasgold99@gmail.com>
2024-05-24 23:55:35 +0200

    use modeler in macro

--- src/management-system-v2/app/confluence/macro-editor/create/macro-editor.tsx
+++ src/management-system-v2/app/confluence/macro-editor/create/macro-editor.tsx
@@ -1,13 +1,45 @@
 'use client';
 
-import { addProcesses } from '@/lib/data/processes';
+import { addProcesses, getProcess } from '@/lib/data/processes';
 import { useEnvironment } from '@/components/auth-can';
 import ProcessForm from '../../process-form';
+import { useState, useEffect } from 'react';
 
-const MacroEditor = () => {
+import styles from './macro-editor.module.scss';
+import { Process } from '@/lib/data/process-schema';
+import Modeler from '@/app/(dashboard)/[environmentId]/processes/[processId]/modeler';
+
+const MacroEditor = ({ processes }: { processes: Process[] }) => {
   const { spaceId } = useEnvironment();
+  const [process, setProcess] = useState<Process | undefined>(undefined);
 
-  return (
+  useEffect(() => {
+    if (window && window.AP && window.AP.confluence) {
+      window.AP.confluence.getMacroData((data: any) => {
+        console.log('macro data', data);
+        if (data && data.processId) {
+          const process = processes.find((process) => process.id === data.processId);
+          setProcess(process);
+        } else {
+          setProcess(undefined);
+        }
+      });
+    }
+    // else {
+    //   const mockProcessId = '_f9fd6cf7-20dc-459e-9bc9-e9f2c4c74a9a';
+    //   const process = processes.find((process) => process.id === mockProcessId);
+    //   console.log('process', process);
+    //   setProcess(process);
+    // }
+  }, []);
+
+  return process ? (
+    <Modeler
+      className={styles.Modeler}
+      process={{ name: process.name, id: process.id, bpmn: process.bpmn }}
+      versions={process.versions}
+    />
+  ) : (
     <div style={{ padding: '2px 24px' }}>
       <ProcessForm
         submit={(values) => {
--- src/management-system-v2/app/confluence/macro-editor/create/page.tsx
+++ src/management-system-v2/app/confluence/macro-editor/create/page.tsx
@@ -6,12 +6,23 @@ import { getProcesses } from '@/lib/data/legacy/_process';
 import { getEnvironmentById } from '@/lib/data/legacy/iam/environments';
 import { getUserOrganizationEnviroments } from '@/lib/data/legacy/iam/memberships';
 import { Process } from '@/lib/data/process-schema';
+import { getProcessBPMN } from '@/lib/data/processes';
 
 const MacroEditorPage = async () => {
   const { session, userId } = await getCurrentUser();
   const { ability } = await getCurrentEnvironment(userId);
   // get all the processes the user has access to
-  const ownedProcesses = (await getProcesses(ability)) as Process[];
+  const ownedProcesses = (
+    await Promise.all(
+      (await getProcesses(ability)).map(async (process) => {
+        const res = await getProcessBPMN(process.id, userId);
+        if (typeof res === 'string') {
+          return { ...process, bpmn: res };
+        }
+        return { ...process };
+      }),
+    )
+  ).filter((process) => !!('bpmn' in process)) as Process[];
 
   const userEnvironments: Environment[] = [getEnvironmentById(userId)];
   userEnvironments.push(
@@ -28,7 +39,7 @@ const MacroEditorPage = async () => {
         userEnvironments={userEnvironments}
         activeSpace={{ spaceId: userId || '', isOrganization: false }}
       >
-        <MacroEditor></MacroEditor>
+        <MacroEditor processes={ownedProcesses}></MacroEditor>
       </Layout>
     </>
   );

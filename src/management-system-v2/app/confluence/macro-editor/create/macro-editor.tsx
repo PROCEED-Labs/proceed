9ba8eabff0ae91c5792f310edbda340a7ac3045d
Lucas <Lucasgold99@gmail.com>
2024-05-24 22:58:47 +0200

    cleanup macro editor

--- src/management-system-v2/app/confluence/macro-editor/create/macro-editor.tsx
+++ src/management-system-v2/app/confluence/macro-editor/create/macro-editor.tsx
@@ -1,45 +1,16 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import ProcessModal from '../../process-modal';
 import { addProcesses } from '@/lib/data/processes';
 import { useEnvironment } from '@/components/auth-can';
-import { useRouter } from 'next/navigation';
 import ProcessForm from '../../process-form';
 
 const MacroEditor = () => {
   const { spaceId } = useEnvironment();
-  const router = useRouter();
-  const [processId, setProcessId] = useState('');
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      console.log('window', window);
-      if (window.AP && window.AP.confluence) {
-        console.log('window AP', window.AP);
-        window.AP.confluence.getMacroData((data: any) => {
-          console.log('confluence macroData', data);
-          if (data) {
-            setProcessId(data.processId || data.parameters?.processId || '');
-          }
-        });
-      }
-    }
-  }, []);
-
-  const saveMacro = () => {
-    if (window.AP && window.AP.confluence) {
-      window.AP.confluence.saveMacro({ processId });
-      window.AP.confluence.closeMacroEditor();
-    }
-    console.log('SAVE MACRO', processId);
-    // window.AP.dialog.close({ processId });
-  };
 
   return (
-    <ProcessForm
-      submit={(values) => {
-        if (values) {
+    <div style={{ padding: '2px 24px' }}>
+      <ProcessForm
+        submit={(values) => {
           addProcesses([{ ...values }], spaceId).then((res) => {
             console.log('added process', res);
             if ('error' in res) {
@@ -53,13 +24,15 @@ const MacroEditor = () => {
               }
             }
           });
-        }
-        console.log('close');
-      }}
-      cancel={() => {
-        console.log('OK');
-      }}
-    ></ProcessForm>
+        }}
+        cancel={() => {
+          if (window.AP && window.AP.confluence) {
+            window.AP.confluence.closeMacroEditor();
+          }
+          console.log('CLOSE MACRO');
+        }}
+      ></ProcessForm>
+    </div>
   );
 };
 

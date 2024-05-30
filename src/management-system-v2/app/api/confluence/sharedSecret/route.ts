59b53977fc2b0cfb2523d56c87cd3c1aa16eb2f0
Lucas <Lucasgold99@gmail.com>
2024-05-30 03:36:37 +0200

    store secret in variable

--- src/management-system-v2/app/api/confluence/sharedSecret/route.ts
+++ src/management-system-v2/app/api/confluence/sharedSecret/route.ts
@@ -1,24 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
 
-let sharedSecrets: { [key: string]: { sharedSecret: any; baseUrl: any } } = {}; // Variable zum Speichern der sharedSecrets
+const secretsFilePath = path.join(process.cwd(), 'app/api/confluence/sharedSecret/secretFile.json');
 
 const setSharedSecret = (clientKey: any, sharedSecret: any, baseUrl: any) => {
+  const sharedSecrets = JSON.parse(fs.readFileSync(secretsFilePath, 'utf-8'));
   sharedSecrets[`${clientKey}`] = { sharedSecret, baseUrl };
-};
-
-const getSharedSecret = (clientKey: string) => {
-  return sharedSecrets[clientKey];
-};
-
-const getAllSharedSecrets = () => {
-  return { ...sharedSecrets };
+  fs.writeFileSync(secretsFilePath, JSON.stringify(sharedSecrets, null, 2));
 };
 
 export async function GET(request: NextRequest) {
+  const sharedSecrets = JSON.parse(fs.readFileSync(secretsFilePath, 'utf-8'));
   return NextResponse.json({ sharedSecrets });
 }
 
 export async function PUT(request: NextRequest) {
+  console.log('PUT sharedsecret');
   const bodyData = await request.json();
   const { clientKey, sharedSecret, baseUrl } = bodyData;
 

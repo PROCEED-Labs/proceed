c10ce72dfae4986c0d302189ca96f440ccfc01ca
Maxi <M.Fisch-22@web.de>
2024-06-04 23:23:03 +0200

    added lazy viewer

--- src/management-system-v2/components/bpmn-viewer.tsx
+++ src/management-system-v2/components/bpmn-viewer.tsx
@@ -1,19 +1,28 @@
 'use client';
 
-import React, { useEffect, useMemo, useRef } from 'react';
+import React, { FC, Suspense, useMemo, useRef } from 'react';
 import cn from 'classnames';
 import BPMNCanvas, { BPMNCanvasRef } from './bpmn-canvas';
 import { getProcessBPMN } from '@/lib/data/processes';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useEnvironment } from './auth-can';
+import style from './bpmn-viewer.module.scss';
+import { useLazyRendering } from './scrollbar';
+import ProceedLoading from './loading-proceed';
 
 type BPMNViewerProps = {
   definitionId: string;
   reduceLogo?: boolean;
   fitOnResize?: boolean;
+  scrollable?: boolean;
 };
 
-const BPMNViewer = ({ definitionId, reduceLogo, fitOnResize }: BPMNViewerProps) => {
+const BPMNViewer: FC<BPMNViewerProps> = ({
+  definitionId,
+  reduceLogo,
+  fitOnResize,
+  scrollable = false,
+}) => {
   const viewer = useRef<BPMNCanvasRef | null>(null);
   const environment = useEnvironment();
 
@@ -37,19 +46,48 @@ const BPMNViewer = ({ definitionId, reduceLogo, fitOnResize }: BPMNViewerProps) 
   // Allows for rerendering when the process changes but not the BPMN.
   const bpmn = useMemo(() => ({ bpmn: data }), [data]);
 
-  useEffect(() => {
-    console.log('viewer', viewer.current);
-    //viewer.current!.fitViewport();
-  }, []);
+  return (
+    <div
+      className={cn(style['bpmn-viewer-container'], {
+        [style['no-scroll']]: !scrollable,
+      })} /* Prevents the Viewer from beeing scrollable / zoomable */
+    >
+      <BPMNCanvas
+        ref={viewer}
+        bpmn={bpmn}
+        type="viewer"
+        className={cn({ reduceLogo: reduceLogo })}
+        resizeWithContainer={fitOnResize}
+      />
+    </div>
+  );
+};
+
+type LazyLoadingBPMNViewerProps = BPMNViewerProps & {
+  fallback?: React.ReactNode;
+};
+
+export const LazyBPMNViewer: FC<LazyLoadingBPMNViewerProps> = ({
+  fallback = <ProceedLoading />,
+  ...props
+}) => {
+  const ViewerContainerRef = useRef(null);
+  const visible = useLazyRendering(ViewerContainerRef);
 
   return (
-    <BPMNCanvas
-      ref={viewer}
-      bpmn={bpmn}
-      type="viewer"
-      className={cn({ reduceLogo: reduceLogo })}
-      resizeWithContainer={fitOnResize}
-    />
+    <>
+      <div ref={ViewerContainerRef}>
+        {visible /* This ensures, that only elements, that are visible or close to beeing visible are rendered -> reduces requests for bpmn/xml */ ? (
+          <Suspense fallback={fallback}>
+            {' '}
+            {/* Prevent sequential rendereing/ get from showing the Icon-list */}
+            <BPMNViewer {...props} />
+          </Suspense>
+        ) : (
+          fallback
+        )}
+      </div>
+    </>
   );
 };
 

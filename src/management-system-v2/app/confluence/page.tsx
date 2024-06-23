3241fae4f1c54edef4cc6b2931a455a347010f63
Lucas <Lucasgold99@gmail.com>
2024-06-23 23:45:35 +0200

    return cookies and headers for checking

--- src/management-system-v2/app/confluence/page.tsx
+++ src/management-system-v2/app/confluence/page.tsx
@@ -6,6 +6,7 @@ import { Environment } from '@/lib/data/environment-schema';
 import { getEnvironmentById } from '@/lib/data/legacy/iam/environments';
 import { getUserOrganizationEnvironments } from '@/lib/data/legacy/iam/memberships';
 import ManagableProcessList from './managable-process-list';
+import { headers, cookies } from 'next/headers';
 
 const ConfluencePage = async ({ params, searchParams }: { params: any; searchParams: any }) => {
   // const jwt = searchParams.jwt;
@@ -17,29 +18,54 @@ const ConfluencePage = async ({ params, searchParams }: { params: any; searchPar
   // });
 
   const { userId } = await getCurrentUser();
-  const { ability } = await getCurrentEnvironment(userId);
-  // get all the processes the user has access to
-  const ownedProcesses = (await getProcesses(ability)) as Process[];
+  const headersList = headers();
+  const headersObject = Object.fromEntries(headersList.entries());
+  // console.log(headersObject);
+  const cookiesList = cookies().getAll();
+  console.log(cookiesList);
+  // const { ability } = await getCurrentEnvironment(userId);
+  // // get all the processes the user has access to
+  // const ownedProcesses = (await getProcesses(ability)) as Process[];
 
-  const userEnvironments: Environment[] = [getEnvironmentById(userId)];
-  userEnvironments.push(
-    ...getUserOrganizationEnvironments(userId).map((environmentId) =>
-      getEnvironmentById(environmentId),
-    ),
-  );
+  // const userEnvironments: Environment[] = [getEnvironmentById(userId)];
+  // userEnvironments.push(
+  //   ...getUserOrganizationEnvironments(userId).map((environmentId) =>
+  //     getEnvironmentById(environmentId),
+  //   ),
+  // );
 
   return (
-    <Layout
-      hideFooter={true}
-      loggedIn={!!userId}
-      layoutMenuItems={[]}
-      userEnvironments={userEnvironments}
-      activeSpace={{ spaceId: userId || '', isOrganization: false }}
-    >
-      <div style={{ padding: '1rem', width: '100%' }}>
-        <ManagableProcessList processes={ownedProcesses}></ManagableProcessList>
+    // <Layout
+    //   hideFooter={true}
+    //   loggedIn={!!userId}
+    //   layoutMenuItems={[]}
+    //   userEnvironments={userEnvironments}
+    //   activeSpace={{ spaceId: userId || '', isOrganization: false }}
+    // >
+    //   <div style={{ padding: '1rem', width: '100%' }}>
+    //     <ManagableProcessList processes={ownedProcesses}></ManagableProcessList>
+    //   </div>
+    // </Layout>
+    <div>
+      <div>
+        <span>COOKIES:</span>
+        {cookiesList.map((cookie) => (
+          <div key={cookie.name}>
+            <span>Name: {cookie.name}</span>
+            <span>Value: {cookie.value}</span>
+          </div>
+        ))}
       </div>
-    </Layout>
+      <div>
+        <span>HEADER:</span>
+        {Object.keys(headersObject).map((headerKey) => (
+          <div key={headerKey}>
+            <span>Key: {headerKey}</span>
+            <span>Value: {headersObject[headerKey]}</span>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 

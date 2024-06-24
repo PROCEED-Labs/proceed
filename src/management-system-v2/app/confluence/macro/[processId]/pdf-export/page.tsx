3af48e442fdd25d330444b9757699114c8f119d9
Lucas <Lucasgold99@gmail.com>
2024-06-24 11:12:44 +0200

    use bpmnsharedviwer directly instead of redirecting url

--- src/management-system-v2/app/confluence/macro/[processId]/pdf-export/page.tsx
+++ src/management-system-v2/app/confluence/macro/[processId]/pdf-export/page.tsx
@@ -1,15 +1,19 @@
+import BPMNSharedViewer from '@/app/shared-viewer/documentation-page';
+import { getProcess } from '@/lib/data/legacy/process';
 import { generateSharedViewerUrl } from '@/lib/sharing/process-sharing';
 import { redirect } from 'next/navigation';
 
 const Page = async ({ params: { processId } }: { params: { processId: string } }) => {
-  const url = await generateSharedViewerUrl({
-    processId,
-    timestamp: 100,
-  });
+  //   const url = await generateSharedViewerUrl({
+  //     processId,
+  //     timestamp: 100,
+  //   });
 
-  console.log('url', url);
+  //   console.log('url', url);
 
-  redirect(url);
+  const process = await getProcess(processId, true);
+
+  return <BPMNSharedViewer isOwner={false} processData={process} availableImports={{}} />;
 };
 
 export default Page;

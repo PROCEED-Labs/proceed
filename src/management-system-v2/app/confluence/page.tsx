a2446ce73973f6afcdbac0ebf140316a21b23056
Lucas <Lucasgold99@gmail.com>
2024-06-24 14:29:52 +0200

    revert cookie and header logging in confluence process list

--- src/management-system-v2/app/confluence/page.tsx
+++ src/management-system-v2/app/confluence/page.tsx
@@ -9,63 +9,64 @@ import ManagableProcessList from './managable-process-list';
 import { headers, cookies } from 'next/headers';
 
 const ConfluencePage = async ({ params, searchParams }: { params: any; searchParams: any }) => {
-  // const jwt = searchParams.jwt;
-  // const res = await fetch('https://proceed-test.atlassian.net/wiki/api/v2/pages', {
-  //   method: 'GET',
-  //   headers: {
-  //     Authorization: `JWT ${jwt}`,
-  //   },
-  // });
+  const jwt = searchParams.jwt;
+  const res = await fetch('https://proceed-test.atlassian.net/wiki/api/v2/pages', {
+    method: 'GET',
+    headers: {
+      Authorization: `JWT ${jwt}`,
+    },
+  });
+
+  // const headersList = headers();
+  // const headersObject = Object.fromEntries(headersList.entries());
+  // console.log(headersObject);
+  // const cookiesList = cookies().getAll();
+  // console.log(cookiesList);
 
   const { userId } = await getCurrentUser();
-  const headersList = headers();
-  const headersObject = Object.fromEntries(headersList.entries());
-  // console.log(headersObject);
-  const cookiesList = cookies().getAll();
-  console.log(cookiesList);
-  // const { ability } = await getCurrentEnvironment(userId);
-  // // get all the processes the user has access to
-  // const ownedProcesses = (await getProcesses(ability)) as Process[];
+  const { ability } = await getCurrentEnvironment(userId);
+  // get all the processes the user has access to
+  const ownedProcesses = (await getProcesses(ability)) as Process[];
 
-  // const userEnvironments: Environment[] = [getEnvironmentById(userId)];
-  // userEnvironments.push(
-  //   ...getUserOrganizationEnvironments(userId).map((environmentId) =>
-  //     getEnvironmentById(environmentId),
-  //   ),
-  // );
+  const userEnvironments: Environment[] = [getEnvironmentById(userId)];
+  userEnvironments.push(
+    ...getUserOrganizationEnvironments(userId).map((environmentId) =>
+      getEnvironmentById(environmentId),
+    ),
+  );
 
   return (
-    // <Layout
-    //   hideFooter={true}
-    //   loggedIn={!!userId}
-    //   layoutMenuItems={[]}
-    //   userEnvironments={userEnvironments}
-    //   activeSpace={{ spaceId: userId || '', isOrganization: false }}
-    // >
-    //   <div style={{ padding: '1rem', width: '100%' }}>
-    //     <ManagableProcessList processes={ownedProcesses}></ManagableProcessList>
+    <Layout
+      hideFooter={true}
+      loggedIn={!!userId}
+      layoutMenuItems={[]}
+      userEnvironments={userEnvironments}
+      activeSpace={{ spaceId: userId || '', isOrganization: false }}
+    >
+      <div style={{ padding: '1rem', width: '100%' }}>
+        <ManagableProcessList processes={ownedProcesses}></ManagableProcessList>
+      </div>
+    </Layout>
+    // <div>
+    //   <div>
+    //     <span>COOKIES:</span>
+    //     {cookiesList.map((cookie) => (
+    //       <div key={cookie.name}>
+    //         <span>Name: {cookie.name}</span>
+    //         <span>Value: {cookie.value}</span>
+    //       </div>
+    //     ))}
     //   </div>
-    // </Layout>
-    <div>
-      <div>
-        <span>COOKIES:</span>
-        {cookiesList.map((cookie) => (
-          <div key={cookie.name}>
-            <span>Name: {cookie.name}</span>
-            <span>Value: {cookie.value}</span>
-          </div>
-        ))}
-      </div>
-      <div>
-        <span>HEADER:</span>
-        {Object.keys(headersObject).map((headerKey) => (
-          <div key={headerKey}>
-            <span>Key: {headerKey}</span>
-            <span>Value: {headersObject[headerKey]}</span>
-          </div>
-        ))}
-      </div>
-    </div>
+    //   <div>
+    //     <span>HEADER:</span>
+    //     {Object.keys(headersObject).map((headerKey) => (
+    //       <div key={headerKey}>
+    //         <span>Key: {headerKey}</span>
+    //         <span>Value: {headersObject[headerKey]}</span>
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
   );
 };
 

e69238a6fb92ad013bea33453dec8eb1dff4ab59
anish <anishsapkota054@gmail.com>
2024-06-30 21:40:10 +0200

    schema adjustments

--- src/management-system-v2/prisma/migrations/20240630184744_updated/migration.sql
+++ src/management-system-v2/prisma/migrations/20240630184744_updated/migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE "User" (
     "username" TEXT,
     "email" TEXT,
     "isGuest" BOOLEAN NOT NULL,
-    "emailVerifiedOn" TEXT,
+    "emailVerifiedOn" TIMESTAMP(3),
     "image" TEXT,
     "favourites" TEXT[],
 
@@ -30,15 +30,15 @@ CREATE TABLE "Process" (
     "originalId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "createdOn" TEXT NOT NULL,
-    "lastEditedOn" TEXT NOT NULL,
+    "createdOn" TIMESTAMP(3) NOT NULL,
+    "lastEditedOn" TIMESTAMP(3) NOT NULL,
     "inEditingBy" JSONB,
     "processIds" TEXT[],
     "type" TEXT NOT NULL,
     "folderId" TEXT NOT NULL,
     "sharedAs" TEXT NOT NULL,
-    "shareTimestamp" INTEGER NOT NULL,
-    "allowIframeTimestamp" INTEGER NOT NULL,
+    "shareTimestamp" BIGINT NOT NULL,
+    "allowIframeTimestamp" BIGINT NOT NULL,
     "environmentId" TEXT NOT NULL,
     "bpmn" XML NOT NULL,
     "ownerId" TEXT NOT NULL,
@@ -49,7 +49,7 @@ CREATE TABLE "Process" (
 );
 
 -- CreateTable
-CREATE TABLE "Workspace" (
+CREATE TABLE "Space" (
     "id" TEXT NOT NULL,
     "name" TEXT,
     "isOrganization" BOOLEAN NOT NULL,
@@ -59,7 +59,7 @@ CREATE TABLE "Workspace" (
     "logo" TEXT,
     "ownerId" TEXT NOT NULL,
 
-    CONSTRAINT "Workspace_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Space_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -67,7 +67,7 @@ CREATE TABLE "Membership" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "environmentId" TEXT NOT NULL,
-    "createdOn" TEXT NOT NULL,
+    "createdOn" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Membership_pkey" PRIMARY KEY ("id")
 );
@@ -76,12 +76,13 @@ CREATE TABLE "Membership" (
 CREATE TABLE "Folder" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
     "parentId" TEXT,
     "type" TEXT NOT NULL DEFAULT 'folder',
     "createdBy" TEXT,
     "environmentId" TEXT NOT NULL,
-    "lastEditedOn" TEXT NOT NULL,
-    "createdOn" TEXT NOT NULL,
+    "lastEditedOn" TIMESTAMP(3) NOT NULL,
+    "createdOn" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
 );
@@ -90,23 +91,15 @@ CREATE TABLE "Folder" (
 CREATE TABLE "Version" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "version" INTEGER NOT NULL,
+    "version" BIGINT NOT NULL,
     "description" TEXT NOT NULL,
     "processId" TEXT NOT NULL,
-    "versionBasedOn" INTEGER,
-    "createdOn" TEXT NOT NULL,
-    "lastEditedOn" TEXT NOT NULL,
+    "versionBasedOn" BIGINT,
+    "createdOn" TIMESTAMP(3) NOT NULL,
+    "lastEditedOn" TIMESTAMP(3) NOT NULL,
+    "bpmn" XML NOT NULL,
 
     CONSTRAINT "Version_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "VersionBPMN" (
-    "id" TEXT NOT NULL,
-    "xml" XML NOT NULL,
-    "versionId" TEXT NOT NULL,
-
-    CONSTRAINT "VersionBPMN_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -119,8 +112,8 @@ CREATE TABLE "Role" (
     "permissions" JSONB NOT NULL,
     "expiration" TEXT,
     "default" BOOLEAN,
-    "createdOn" TEXT NOT NULL,
-    "lastEditedOn" TEXT NOT NULL,
+    "createdOn" TIMESTAMP(3) NOT NULL,
+    "lastEditedOn" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
 );
@@ -130,7 +123,7 @@ CREATE TABLE "RoleMember" (
     "id" TEXT NOT NULL,
     "roleId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
-    "createdOn" TEXT NOT NULL,
+    "createdOn" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "RoleMember_pkey" PRIMARY KEY ("id")
 );
@@ -142,13 +135,10 @@ CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "OauthAccount_provider_providerAccountId_key" ON "OauthAccount"("provider", "providerAccountId");
+CREATE UNIQUE INDEX "OauthAccount_providerAccountId_key" ON "OauthAccount"("providerAccountId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Membership_userId_environmentId_key" ON "Membership"("userId", "environmentId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "VersionBPMN_versionId_key" ON "VersionBPMN"("versionId");
+CREATE UNIQUE INDEX "Version_version_key" ON "Version"("version");
 
 -- AddForeignKey
 ALTER TABLE "OauthAccount" ADD CONSTRAINT "OauthAccount_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -157,19 +147,19 @@ ALTER TABLE "OauthAccount" ADD CONSTRAINT "OauthAccount_userId_fkey" FOREIGN KEY
 ALTER TABLE "Process" ADD CONSTRAINT "Process_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Process" ADD CONSTRAINT "Process_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Process" ADD CONSTRAINT "Process_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Process" ADD CONSTRAINT "Process_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Workspace" ADD CONSTRAINT "Workspace_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Space" ADD CONSTRAINT "Space_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Membership" ADD CONSTRAINT "Membership_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Membership" ADD CONSTRAINT "Membership_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Membership" ADD CONSTRAINT "Membership_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -178,19 +168,16 @@ ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentId_fkey" FOREIGN KEY ("parentI
 ALTER TABLE "Folder" ADD CONSTRAINT "Folder_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Folder" ADD CONSTRAINT "Folder_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Version" ADD CONSTRAINT "Version_processId_fkey" FOREIGN KEY ("processId") REFERENCES "Process"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "VersionBPMN" ADD CONSTRAINT "VersionBPMN_versionId_fkey" FOREIGN KEY ("versionId") REFERENCES "Version"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Role" ADD CONSTRAINT "Role_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Role" ADD CONSTRAINT "Role_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "RoleMember" ADD CONSTRAINT "RoleMember_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "RoleMember" ADD CONSTRAINT "RoleMember_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "RoleMember" ADD CONSTRAINT "RoleMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "RoleMember" ADD CONSTRAINT "RoleMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;

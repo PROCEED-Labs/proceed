3f805bc3e00eda65d4f4f79aa8c4bd92cc56acfe
Lucas <Lucasgold99@gmail.com>
2024-07-03 18:37:30 +0200

    improve style of macro with header and footer

--- src/management-system-v2/app/confluence/macro/[processId]/macro.tsx
+++ src/management-system-v2/app/confluence/macro/[processId]/macro.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import Modeler from '../../macro-editor/create/confluence-modeler';
 import { Process } from '@/lib/data/process-schema';
 import { useRouter } from 'next/navigation';
+import Link from '@atlaskit/link';
 
 const Macro = ({ process }: { process: Process }) => {
   const router = useRouter();
@@ -18,11 +19,42 @@ const Macro = ({ process }: { process: Process }) => {
 
   return (
     <>
-      {/* <Viewer definitionId={processId}></Viewer> */}
-      <div style={{ height: '100vh' }}>
-        {/* <BPMNCanvas ref={viewer} type="navigatedviewer" bpmn={{ bpmn: BPMN }}></BPMNCanvas> */}
-        {/* <ModelerZoombar canvasRef={viewer.current!}></ModelerZoombar> */}
-        <Modeler isViewer process={{ name: process.name, id: process.id, bpmn: process.bpmn }} />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '400px',
+          border: '1px solid #f0f0f0',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: '0.75rem',
+            borderBottom: '1px solid #f0f0f0',
+            backgroundColor: '#f0f0f0',
+            alignItems: 'center',
+          }}
+        >
+          <div>
+            <img style={{ width: '3rem', marginRight: '0.75rem' }} src="/proceed-icon.png"></img>
+            <span style={{ fontWeight: 'bold' }}>{process.name}</span>
+          </div>
+          <Link href={`/processes/${process.id}`} rel="noopener noreferrer" target="_blank">
+            View in PROCEED
+          </Link>
+        </div>
+        <div style={{ flexGrow: 1, position: 'relative' }}>
+          <Modeler isViewer process={{ name: process.name, id: process.id, bpmn: process.bpmn }} />
+        </div>
+        <div
+          style={{ padding: '0.75rem', borderTop: '1px solid #f0f0f0', backgroundColor: '#f0f0f0' }}
+        >
+          <span style={{ fontWeight: 'bold' }}>Description:</span>{' '}
+          <span> {process.description}</span>
+        </div>
       </div>
     </>
   );

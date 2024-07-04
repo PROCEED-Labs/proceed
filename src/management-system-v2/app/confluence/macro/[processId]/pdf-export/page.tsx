e0c306b64f4be0f612abe66b1b94059086942fc9
Lucas <Lucasgold99@gmail.com>
2024-07-04 05:32:47 +0200

    retrieve process information from confluence-stored bpmn

--- src/management-system-v2/app/confluence/macro/[processId]/pdf-export/page.tsx
+++ src/management-system-v2/app/confluence/macro/[processId]/pdf-export/page.tsx
@@ -1,20 +1,46 @@
-import { getAttachmentProcessBase64Image, getAttachments } from '@/app/confluence/helpers';
-import BPMNSharedViewer from '@/app/shared-viewer/documentation-page';
-import { getProcess } from '@/lib/data/legacy/process';
-import { generateSharedViewerUrl } from '@/lib/sharing/process-sharing';
-import { redirect } from 'next/navigation';
+import {
+  getAttachmentProcessBase64Image,
+  getAttachmentProcessBpmn,
+} from '@/app/confluence/helpers';
+import { getDefinitionsName, getProcessDocumentation } from '@proceed/bpmn-helper';
 
 const Page = async ({ params: { processId } }: { params: { processId: string } }) => {
-  //   const url = await generateSharedViewerUrl({
-  //     processId,
-  //     timestamp: 100,
-  //   });
-
-  //   console.log('url', url);
-
   const base64 = await getAttachmentProcessBase64Image('14712843', processId);
+  const bpmn = await getAttachmentProcessBpmn('14712843', processId);
+  const documentation = await getProcessDocumentation(bpmn);
+  const definitionsName = await getDefinitionsName(bpmn);
 
-  return <img src={`data:image/png;base64, ${base64}`}></img>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '400px',
+        border: '1px solid #f0f0f0',
+        borderRadius: '0.5rem',
+      }}
+    >
+      <div
+        style={{
+          padding: '0.75rem',
+          borderBottom: '1px solid #f0f0f0',
+          backgroundColor: '#f0f0f0',
+          alignItems: 'center',
+        }}
+      >
+        <img style={{ width: '3rem', marginRight: '0.75rem' }} src="/proceed-icon.png"></img>
+        <span style={{ fontWeight: 'bold' }}>{definitionsName}</span>
+      </div>
+      <div style={{ flexGrow: 1 }}>
+        <img src={`data:image/png;base64, ${base64}`}></img>
+      </div>
+      <div
+        style={{ padding: '0.75rem', borderTop: '1px solid #f0f0f0', backgroundColor: '#f0f0f0' }}
+      >
+        <span style={{ fontWeight: 'bold' }}>Description:</span> <span> {documentation}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Page;

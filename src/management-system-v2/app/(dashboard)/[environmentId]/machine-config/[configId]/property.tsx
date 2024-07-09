88fcf04f3b9a3b4b24e23cb62331f6b63f4f46e0
Luisa Wittig <wittig.luisa@yahoo.de>
2024-07-09 16:04:58 +0200

    experimental property module

--- src/management-system-v2/app/(dashboard)/[environmentId]/machine-config/[configId]/property.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/machine-config/[configId]/property.tsx
@@ -1,6 +1,12 @@
 'use client';
 
-import { ParentConfig, ConfigParameter } from '@/lib/data/machine-config-schema';
+import {
+  ParentConfig,
+  ConfigParameter,
+  AbstractConfig,
+  ConfigField,
+  PropertyContent,
+} from '@/lib/data/machine-config-schema';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 import { PlusOutlined, DeleteOutlined, CaretRightOutlined } from '@ant-design/icons';
@@ -12,18 +18,21 @@ import { TreeFindStruct, defaultConfiguration, findConfig } from '../configurati
 import Text from 'antd/es/typography/Text';
 import getAddButton from './add-button';
 import getTooltips from './getTooltips';
+import { Config } from 'winston/lib/winston/config';
 
 type MachineDataViewProps = {
   configId: string;
   selectedConfig: TreeFindStruct;
   parentConfig: ParentConfig;
   backendSaveParentConfig: Function;
+  customConfig?: AbstractConfig;
   editingEnabled: boolean;
+  field: ConfigField | ConfigParameter;
 };
 
 const LATEST_VERSION = { version: -1, name: 'Latest Version', description: '' };
 
-export default function Parameters(props: MachineDataViewProps) {
+export default function Property(props: MachineDataViewProps) {
   const router = useRouter();
   const environment = useEnvironment();
   const query = useSearchParams();
@@ -37,40 +46,28 @@ export default function Parameters(props: MachineDataViewProps) {
   const saveMachineConfig = props.backendSaveParentConfig;
   const configId = props.configId;
   const selectedVersionId = query.get('version');
-  const [nestedParameters, setNestedParameters] = useState<ConfigParameter[]>([]); // State for nested parameters
 
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
+  const propertyField = props.field;
 
   const editable = props.editingEnabled;
-
-  const saveParameters = () => {
-    if (refEditingMachineConfig) {
-      refEditingMachineConfig.selection.parameters = nestedParameters;
-      saveMachineConfig(configId, parentConfig).then(() => {});
-      router.refresh();
-    }
-  };
 
   useEffect(() => {
     if (firstRender.current) {
       firstRender.current = false;
       return;
     }
-    if (refEditingMachineConfig) setNestedParameters(refEditingMachineConfig.selection.parameters);
   }, [props.selectedConfig]);
 
   const showMobileView = useMobileModeler();
 
-  const parameterItemHeader = (parameter: ConfigParameter) => (
+  const propertyItemHeader = (propertyItem: PropertyContent) => (
     <Space.Compact block size="small">
       <Flex align="center" justify="space-between" style={{ width: '100%' }}>
         <Space>
-          <Text>{parameter.content[0].displayName}: </Text>
-          <Text>{parameter.content[0].value}</Text>
-          <Text>{parameter.content[0].unit}</Text>
-          <Text type="secondary">({parameter.content[0].language})</Text>
+          <Text>{propertyItem.displayName}: </Text>
+          <Text>{propertyItem.value}</Text>
+          <Text>{propertyItem.unit}</Text>
+          <Text type="secondary">({propertyItem.language})</Text>
         </Space>
         {getTooltips(editable, ['copy', 'edit', 'delete'])}
       </Flex>
@@ -85,21 +82,7 @@ export default function Parameters(props: MachineDataViewProps) {
     border: 'solid 1px #d9d9d9',
   };
 
-  const getNestedParameters = () => {
-    if (nestedParameters && nestedParameters.length > 0) {
-      return (
-        <Collapse
-          bordered={false}
-          expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
-          items={nestedParameters}
-          style={{ display: nestedParameters.length > 0 ? 'block' : 'none' }}
-        />
-      );
-    }
-    return getAddButton('Add Nested Parameter', undefined, () => {});
-  };
-
-  const parameterContent = (parameter: ConfigParameter) => (
+  const propertyContent = (propertyItem: PropertyContent) => (
     <div>
       {editable && (
         <>
@@ -109,7 +92,7 @@ export default function Parameters(props: MachineDataViewProps) {
               Key{' '}
             </Col>
             <Col span={20} className="gutter-row">
-              <Input disabled={!editable} value={parameter.content[0].displayName} />
+              <Input disabled={!editable} value={propertyItem.displayName} />
             </Col>
           </Row>
           <Row gutter={[24, 24]} align="middle" style={{ margin: '10px 0' }}>
@@ -118,7 +101,7 @@ export default function Parameters(props: MachineDataViewProps) {
               Value{' '}
             </Col>
             <Col span={20} className="gutter-row">
-              <Input disabled={!editable} value={parameter.content[0].value} />
+              <Input disabled={!editable} value={propertyItem.value} />
             </Col>
             <Col span={1} className="gutter-row">
               <Tooltip title="Delete">
@@ -132,7 +115,7 @@ export default function Parameters(props: MachineDataViewProps) {
               Unit{' '}
             </Col>
             <Col span={20} className="gutter-row">
-              <Input disabled={!editable} value={parameter.content[0].unit} />
+              <Input disabled={!editable} value={propertyItem.unit} />
             </Col>
             <Col span={1} className="gutter-row">
               <Tooltip title="Delete">
@@ -146,7 +129,7 @@ export default function Parameters(props: MachineDataViewProps) {
               Language{' '}
             </Col>
             <Col span={20} className="gutter-row">
-              <Input disabled={!editable} value={parameter.content[0].language} />
+              <Input disabled={!editable} value={propertyItem.language} />
             </Col>
             <Col span={1} className="gutter-row">
               <Tooltip title="Delete">
@@ -156,76 +139,44 @@ export default function Parameters(props: MachineDataViewProps) {
           </Row>
         </>
       )}
-      <Row gutter={[24, 24]} align="middle" style={{ margin: '10px 0' }}>
-        <Col span={3} className="gutter-row">
-          Linked Parameters
-        </Col>
-        <Col span={20} className="gutter-row">
-          <Space>
-            <Tag color="purple">Key XY</Tag>
-            <Tag color="blue">Key AB</Tag>
-            <Tooltip title="Add Parameter Link">
-              <Button disabled={!editable} icon={<PlusOutlined />} size="small" />
-            </Tooltip>
-          </Space>
-        </Col>
-        <Col span={1} className="gutter-row">
-          <Tooltip title="Delete">
-            <Button disabled={!editable} icon={<DeleteOutlined />} type="text" />
-          </Tooltip>
-        </Col>
-      </Row>
-      <Row gutter={[24, 24]} align="middle" style={{ margin: '10px 0' }}>
-        <Col span={3} className="gutter-row">
-          Nested Parameters
-        </Col>
-        <Col span={20} className="gutter-row">
-          {getNestedParameters()}
-          {editable && (
-            <Space style={{ margin: '10px 0 0 0' }}>
-              {getAddButton('Add Nested Parameter', undefined, () => {})}
-            </Space>
-          )}
-        </Col>
-        <Col span={1} className="gutter-row">
-          <Tooltip title="Delete">
-            <Button disabled={!editable} icon={<DeleteOutlined />} type="text" />
-          </Tooltip>
-        </Col>
-      </Row>
     </div>
   );
 
-  const getParameterItems = (): any => {
+  const getPropertyItems = (): any => {
     let list = [];
-    for (let parameter of editingConfig.parameters) {
+    for (let propertyItem of propertyField.content) {
+      //TODO
       list.push({
-        key: parameter.id,
-        label: parameterItemHeader(parameter),
-        children: [parameterContent(parameter)],
+        key: propertyField.content.indexOf(propertyItem),
+        label: propertyItemHeader(propertyItem),
+        children: [propertyContent(propertyItem)],
         style: panelStyle,
       });
     }
     return list;
   };
 
-  const parameterItems = getParameterItems();
+  const propertyItems = getPropertyItems();
 
   return (
     <>
-      {(editable || parameterItems.length > 0) && (
+      {(editable || propertyItems.length > 0) && (
         <Row gutter={[24, 24]} style={{ margin: '16px 0' }}>
           <Col span={3} className="gutter-row">
-            Parameters
+            {
+              //TODO
+              propertyField.key[0].toUpperCase() + propertyField.key.slice(1) ||
+                propertyField.content[0].displayName
+            }
           </Col>
           <Col span={21} className="gutter-row">
             <Collapse
               expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
               ghost
               size="small"
-              items={parameterItems}
+              items={propertyItems}
             />
-            {editable && <Space>{getAddButton('Add Parameter', undefined, () => {})}</Space>}
+            {editable && <Space>{getAddButton('Add Property Item', undefined, () => {})}</Space>}
           </Col>
         </Row>
       )}

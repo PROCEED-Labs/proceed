ba88805e2e26734e582b94b16c7b5a8dea994bff
Felipe Trost <felipe.trost@gmail.com>
2024-08-01 13:56:08 +0200

    Reapply "Merge branch 'ms2/environment-variables' of github.com:PROCEED-Labs/proceed into ms2/environment-variables"
    This reverts commit 07ba45f095ea370ff69f43863c7ba3f7c2f56bff.

--- src/management-system-v2/app/(dashboard)/[environmentId]/executions/page.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/executions/page.tsx
@@ -1,19 +1,33 @@
 import Content from '@/components/content';
-import { Space } from 'antd';
 import { getCurrentEnvironment } from '@/components/auth';
 import { notFound } from 'next/navigation';
 import { env } from '@/lib/env-vars';
+import DeploymentsView from './deployments-view';
+import { getRootFolder, getFolderById, getFolderContents } from '@/lib/data/legacy/folders';
+import { getUsersFavourites } from '@/lib/data/users';
 
 const ExecutionsPage = async ({ params }: { params: { environmentId: string } }) => {
   if (!env.ENABLE_EXECUTION) {
     return notFound();
   }
 
-  const { ability } = await getCurrentEnvironment(params.environmentId);
+  const { ability, activeEnvironment } = await getCurrentEnvironment(params.environmentId);
+
+  const favs = await getUsersFavourites();
+
+  const rootFolder = getRootFolder(activeEnvironment.spaceId, ability);
+
+  const folder = getFolderById(rootFolder.id);
+
+  const folderContents = await getFolderContents(folder.id, ability);
 
   return (
     <Content title="Executions">
-      <Space direction="vertical" size="large" style={{ display: 'flex', height: '100%' }}></Space>
+      <DeploymentsView
+        processes={folderContents}
+        folder={folder}
+        favourites={favs as string[]}
+      ></DeploymentsView>
     </Content>
   );
 };
--- src/management-system-v2/components/favouriteStar.tsx
+++ src/management-system-v2/components/favouriteStar.tsx
@@ -8,9 +8,10 @@ import { App } from 'antd';
 type StarType = {
   id: string;
   className?: string;
+  viewOnly?: boolean;
 };
 
-const FavouriteStar: FC<StarType> = ({ id, className }) => {
+const FavouriteStar: FC<StarType> = ({ id, className, viewOnly = false }) => {
   const { favourites: favs, updateFavouriteProcesses } = useFavouriteProcesses();
   const { message } = App.useApp();
 
@@ -34,10 +35,14 @@ const FavouriteStar: FC<StarType> = ({ id, className }) => {
         style={{
           color: favs?.includes(id) ? '#FFD700' : undefined,
         }}
-        onClick={(e) => {
-          e.stopPropagation();
-          updateFavs(id);
-        }}
+        onClick={
+          viewOnly
+            ? undefined
+            : (e) => {
+                e.stopPropagation();
+                updateFavs(id);
+              }
+        }
         className={favs?.includes(id) ? undefined : className}
       />
     </>

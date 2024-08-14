e49d4bcd4a8eed03c5f6b9e94a8102bd1cf4d860
Felipe Trost <felipe.trost@gmail.com>
2024-08-14 15:04:22 +0200

    feat(ms2/FolderTree): show root as folder

--- src/management-system-v2/components/FolderTree.tsx
+++ src/management-system-v2/components/FolderTree.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { getFolderContents } from '@/lib/data/folders';
+import { getFolder, getFolderContents } from '@/lib/data/folders';
 import { Spin, Tree, TreeProps } from 'antd';
 import React, { useEffect, useRef, useState } from 'react';
 import { useEnvironment } from './auth-can';
@@ -44,15 +44,18 @@ export const FolderTree = ({
   rootNodes,
   newChildrenHook,
   treeProps,
+  showRootAsFolder,
 }: {
   rootNodes?: FolderChildren[];
   /** The return value is used to update the tree */
   newChildrenHook?: (nodes: TreeNode[], parent?: TreeNode) => TreeNode[];
   treeProps?: TreeProps<TreeNode>;
+  showRootAsFolder?: boolean;
 }) => {
   const spaceId = useEnvironment().spaceId;
 
   const nodeMap = useRef(new Map<React.Key, TreeNode>());
+  const loadedKeys = useRef(new Map<React.Key, boolean>());
 
   const [tree, setTree] = useState<TreeNode[]>(() => {
     if (!rootNodes) return [];
@@ -71,25 +74,42 @@ export const FolderTree = ({
   const loadData = async (node?: TreeNode) => {
     const nodeId = node?.element.id;
 
+    let rootNode: TreeNode | undefined;
+    if (!nodeId && showRootAsFolder) {
+      const rootFolder = await getFolder(spaceId);
+      if ('error' in rootFolder) return;
+
+      rootNode = generateNode({ type: 'folder', name: '< root >', id: rootFolder.id });
+      if (newChildrenHook) rootNode = newChildrenHook?.([rootNode])[0];
+
+      if (!rootNode) return;
+
+      rootNode.style;
+      nodeMap.current.set(rootNode.key, rootNode);
+
+      console.log({ rootNode });
+    }
+
+    const parentNode = nodeId ? nodeMap.current.get(nodeId) : rootNode;
+
+    // get children nodes
     const children = await getFolderContents(spaceId, nodeId);
-    console.log(children);
     if ('error' in children) return;
-
     let childrenNodes = children.map(generateNode);
-
-    const actualNode = nodeId ? nodeMap.current.get(nodeId) : undefined;
-    if (newChildrenHook) childrenNodes = newChildrenHook(childrenNodes, actualNode);
-
+    if (newChildrenHook) childrenNodes = newChildrenHook(childrenNodes, parentNode);
     for (const node of childrenNodes) nodeMap.current.set(node.key, node);
 
-    if (nodeId) {
-      actualNode!.children = childrenNodes;
-      actualNode!.isLeaf = childrenNodes.length === 0;
+    if (parentNode) {
+      parentNode!.children = childrenNodes;
+      parentNode!.isLeaf = childrenNodes.length === 0;
+      loadedKeys.current.set(parentNode.key, true);
+    }
 
+    if (nodeId) {
       // trigger re-render
       setTree((currentTree) => [...currentTree]);
     } else {
-      setTree(childrenNodes);
+      setTree(parentNode ? [parentNode] : childrenNodes);
     }
   };
 
@@ -110,7 +130,16 @@ export const FolderTree = ({
 
   return (
     <Spin spinning={loading}>
-      <Tree showIcon={true} {...treeProps} treeData={tree} loadData={loadData} />
+      <Tree
+        showIcon={true}
+        {...treeProps}
+        treeData={tree}
+        loadData={loadData}
+        // It is okay to not use a state variable, because whenever loadedKeys is updated in
+        // loadData, we also trigger a rerender, such that the rendered Tree will always have the
+        // correct loadedKeys
+        loadedKeys={[...loadedKeys.current.keys()]}
+      />
     </Spin>
   );
 };

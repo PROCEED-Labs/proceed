7b22199fc22f6a01344ea0d4393557afc7e018cb
Felipe Trost <felipe.trost@gmail.com>
2024-08-22 16:37:46 +0200

    feat(ms2/iam-users): show user roles

--- src/management-system-v2/app/(dashboard)/[environmentId]/iam/users/page.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/iam/users/page.tsx
@@ -5,6 +5,9 @@ import UnauthorizedFallback from '@/components/unauthorized-fallback';
 import { getMemebers } from '@/lib/data/legacy/iam/memberships';
 import { getUserById } from '@/lib/data/legacy/iam/users';
 import { AuthenticatedUser } from '@/lib/data/user-schema';
+import { ComponentProps } from 'react';
+import { getRoleMappingByUserId } from '@/lib/data/legacy/iam/role-mappings';
+import { getRoleById } from '@/lib/data/legacy/iam/roles';
 
 const Page = async ({ params }: { params: { environmentId: string } }) => {
   const { ability, activeEnvironment } = await getCurrentEnvironment(params.environmentId);
@@ -12,7 +15,14 @@ const Page = async ({ params }: { params: { environmentId: string } }) => {
   if (!ability.can('manage', 'User')) return <UnauthorizedFallback />;
 
   const memberships = getMemebers(activeEnvironment.spaceId, ability);
-  const users = memberships.map((user) => getUserById(user.userId)) as AuthenticatedUser[];
+  const users: ComponentProps<typeof UsersPage>['users'] = memberships.map((user) =>
+    getUserById(user.userId),
+  ) as AuthenticatedUser[];
+
+  for (const user of users) {
+    const mappings = getRoleMappingByUserId(user.id, activeEnvironment.spaceId);
+    if (mappings.length > 0) user.roles = mappings.map((mapping) => getRoleById(mapping.roleId));
+  }
 
   return (
     <Content title="Identity and Access Management">

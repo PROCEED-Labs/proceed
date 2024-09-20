39ef5bcea4cfa07eac9f4fcd802784297d037178
Felipe Trost <felipe.trost@gmail.com>
2024-09-20 17:38:49 +0200

    feat(engine): initial code execution
    There are still some rough edges and some features that need to be
    implemented.

---
+++ src/engine/native/node/native-vm2/src/childProcess.js
@@ -0,0 +1,79 @@
+const ivm = require('isolated-vm');
+
+const [processId, processInstanceId, tokenId, scriptString, token] = process.argv.slice(2);
+
+if (!processId || !processInstanceId || !tokenId || !scriptString || !token)
+  throw new Error(
+    'Expected the following args processId, processInstanceId, tokenId, scriptString, token',
+  );
+
+async function callToExecutor(endpoint, body) {
+  try {
+    // TODO: don't hardcode address and port
+    const response = await fetch(
+      `http://localhost:33040/${processId}/${processInstanceId}/${endpoint}`,
+      {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          ['content-type']: 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    if (!response.ok) throw new Error('Response not ok');
+
+    const contentType = response.headers.get('content-type');
+    if (!contentType) return;
+    else if (contentType.includes('application/json')) return await response.json();
+    else return await response.text();
+  } catch (e) {
+    // TODO: log error
+    console.error(e);
+  }
+}
+
+const isolate = new ivm.Isolate({ memoryLimit: 128 });
+const context = isolate.createContextSync();
+
+context.global.setSync('global', context.global.derefInto());
+
+context.global.setSync('_stdout_log', function (...args) {
+  console.log(...args);
+});
+
+// TODO: setProgress(<number between 0 - 100>)
+// TODO: await setIntervalAsync( <clb>, <number in milliseconds> )
+// TODO: await setTimeoutAsync( <clb>, <number in milliseconds> )
+// TODO: getService('capabilities')
+// TODO: getService('network')
+// TODO: throw new BpmnError( ["<reference>",] "explanation" )
+// TODO: throw new BpmnEscalation( ["<reference>",] "explanation" );
+
+// NOTE: Extern capabilities (provided by neo-engine)
+// log, console, variable, getService, BpmnEscalation, BpmnError
+// TODO: pass these in as a process argument
+for (const functionName of ['log', 'console', 'variable', 'getService', 'yuhu']) {
+  context.global.setSync(
+    `_${functionName}`,
+    new ivm.Reference(async function (...args) {
+      return callToExecutor('call', { functionName: functionName, args });
+    }),
+  );
+
+  context.evalSync(
+    `function ${functionName}(...args) {
+      return _${functionName}.apply(null, args, { result: { promise: true, copy: true } });
+    }`,
+  );
+}
+
+const hostile = isolate.compileScriptSync(`function main(){ ${scriptString} }; main();`);
+
+hostile
+  .run(context)
+  .then((result) => {
+    callToExecutor('result', { result });
+  })
+  .catch((err) => console.error(err));
--- src/engine/native/node/native-vm2/src/index.js
+++ src/engine/native/node/native-vm2/src/index.js
@@ -1,19 +1,232 @@
-/* eslint-disable class-methods-use-this */
 const NativeModule = require('@proceed/native-module');
-const scriptExecutor = require('./scriptExecutor');
+const childProcess = require('node:child_process');
+const Fastify = require('fastify');
 
 /**
  * @class
  */
-class VM2 extends NativeModule {
-  constructor() {
+class ScriptExecutor extends NativeModule {
+  /**
+   * @param {{
+   * processCommunicationPort?: number
+   * logChildProcessOutput?: boolean
+   * }} [options={}]
+   * */
+  constructor(options = {}) {
     super();
+
+    /** @type{string} */
     this.id = require.resolve('@proceed/native-vm2');
+
+    /** @type{Map<
+     *  string,
+     *  {
+     *    process: import('node:child_process').ChildProcess,
+     *    token: string,
+     *    stdout: string,
+     *    stderr: string,
+     *    result?: Object
+     *    dependencies?: Object
+     *  }
+     * >} */
+    this.childProcesses = new Map();
+
+    this.#setupRouter(options.processCommunicationPort);
+    this.logChildProcessOutput = !!options.logChildProcessOutput;
   }
 
+  #setupRouter(port = 33040) {
+    this.fastify = Fastify({
+      logger: false,
+    });
+
+    this.fastify.post('/:processId/:processInstanceId/result', async (req, res) => {
+      if (req.headers['content-type'] !== 'application/json' || !req.body.result)
+        return res.code(400).send('You have to send a JSON body that includes a result key.');
+
+      // TODO: refactor to middleware/plugin
+      const { processId, processInstanceId } = req.params;
+      const process = this.#getProcess(processId, processInstanceId);
+      if (!process) return res.code(404).send();
+
+      const auth = req.headers.authorization;
+      if (!auth?.startsWith('Bearer ')) return res.code(401).send();
+      const token = auth.substring('Bearer '.length);
+      // NOTE: a simple comparison may be vulnerable to a timing attack
+      if (token !== process.token) return res.code(401).send();
+
+      process.result = req.body.result;
+    });
+
+    this.fastify.post(
+      '/:processId/:processInstanceId/call',
+      {
+        schema: {
+          body: {
+            type: 'object',
+            properties: {
+              functionName: { type: 'string' },
+              args: { type: 'array' },
+            },
+          },
+        },
+      },
+      async (req, res) => {
+        // TODO: refactor to middleware/plugin
+        const { processId, processInstanceId } = req.params;
+        const process = this.#getProcess(processId, processInstanceId);
+        if (!process) return res.code(404).send();
+
+        const auth = req.headers.authorization;
+        if (!auth?.startsWith('Bearer ')) return res.code(401).send();
+        const token = auth.substring('Bearer '.length);
+        // NOTE: a simple comparison may be vulnerable to a timing attack
+        if (token !== process.token) return res.code(401).send();
+
+        const { functionName, args } = req.body;
+
+        const targetFunction = process?.dependencies[functionName];
+        if (!targetFunction) return res.code(404).send('Function not found');
+        if (typeof targetFunction !== 'function') return res.code(500).send('Bad dependency');
+
+        try {
+          const result = await targetFunction(...args);
+          res.code(200).send({ result });
+        } catch (e) {
+          return res.code(500).send(`Error: ${e.message}`);
+        }
+      },
+    );
+
+    this.fastify.listen({ port });
+  }
+
+  /**
+   * @param {string} processId
+   * @param {string} processInstanceId
+   * @param {typeof this.childProcesses} processInstanceId
+   */
+  #setProcess(processId, processInstanceId, processEntry) {
+    return this.childProcesses.set(JSON.stringify([processId, processInstanceId]), processEntry);
+  }
+
+  /**
+   * @param {string} processId
+   * @param {string} processInstanceId
+   */
+  #getProcess(processId, processInstanceId) {
+    return this.childProcesses.get(JSON.stringify([processId, processInstanceId]));
+  }
+
+  #getAllProcesses() {
+    return this.childProcesses.values();
+  }
+
+  /**
+   * @param {string} processId
+   * @param {string} processInstanceId
+   */
+  #deleteProcess(processId, processInstanceId) {
+    return this.childProcesses.delete(JSON.stringify([processId, processInstanceId]));
+  }
+
+  #deleteAllProcess() {
+    this.childProcesses.clear();
+  }
+
+  /**
+   * @param {string} processId
+   * @param {string} processInstanceId
+   * @param {string} tokenId
+   * @param {string} scriptString
+   * @param {Object} scriptString
+   */
+  execute(processId, processInstanceId, tokenId, scriptString, dependencies) {
+    // TODO: maybe just warn
+    if (this.#getProcess(processId, processInstanceId))
+      throw new Error('This process is already executing a script');
+
+    try {
+      const token = crypto.randomUUID();
+
+      // TODO: make this an absolute path
+      const scriptTask = childProcess.spawn(`node`, [
+        require.resolve('./childProcess.js'),
+        processId,
+        processInstanceId,
+        tokenId,
+        scriptString,
+        token,
+      ]);
+
+      const processEntry = {
+        process: scriptTask,
+        token,
+        dependencies,
+        stdout: '',
+        stderr: '',
+      };
+      this.#setProcess(processId, processInstanceId, processEntry);
+
+      // TODO: handle script failure properly
+
+      scriptTask.stdout.on('data', (data) => {
+        if (this.logChildProcessOutput) console.log(`stdout: ${data}`);
+        processEntry.stdout += data.toString();
+      });
+
+      scriptTask.stderr.on('data', (data) => {
+        if (this.logChildProcessOutput) console.error(`stderr: ${data}`);
+        processEntry.stderr += data.toString();
+      });
+
+      return new Promise((resolve, reject) => {
+        scriptTask.on('close', (code) => {
+          // TODO: use code's output
+          if (code === 0) resolve(processEntry.result);
+          else reject();
+        });
+      });
+    } catch (e) {
+      // TODO: use proper logger
+    }
+  }
+
+  /**
+   * @param {string} processId
+   * @param {string} processInstanceId
+   */
+  stop(processId, processInstanceId) {
+    // NOTE: mabybe the onClose function from a process should remove them from the map
+
+    // required by the neo engine
+    if (processId === '*' && processInstanceId === '*') {
+      for (const process of this.#getAllProcesses()) {
+        process.kill();
+      }
+
+      return this.#deleteAllProcess();
+    }
+
+    const scriptTask = this.#getProcess(processId, processInstanceId);
+
+    // NOTE: maybe give a warning?
+    if (!scriptTask) return;
+
+    scriptTask.kill();
+    this.#deleteProcess(processId, processInstanceId);
+  }
+
+  // /** @param {import('@proceed/core/src/engine/engine').default} engine  */
   onAfterEngineLoaded(engine) {
-    engine.provideScriptExecutor(scriptExecutor({}));
+    engine.provideScriptExecutor(this);
+    this.id;
+  }
+
+  destroy() {
+    this.#deleteAllProcess();
+    return this.fastify.close();
   }
 }
 
-module.exports = VM2;
+module.exports = ScriptExecutor;

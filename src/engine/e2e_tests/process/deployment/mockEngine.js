124f600aae6302fa6e08f2a598cb89d4de8ce2a6
Felipe Trost <felipe.trost@gmail.com>
2024-10-29 17:06:16 +0100

    feat(engine/e2e-tests): use ChildProcess script execution

--- src/engine/e2e_tests/process/deployment/mockEngine.js
+++ src/engine/e2e_tests/process/deployment/mockEngine.js
@@ -6,6 +6,7 @@ const MockMachine = require('./mockNativeMachine.js');
 const Nmdns = require('@proceed/native-mdns');
 const Ncapabilities = require('@proceed/native-capabilities');
 const Nconsole = require('@proceed/native-console');
+const ChildProcessExecutor = require('@proceed/native-vm2');
 
 native.registerModule(new Nfs({ dir: process.argv[2] }));
 native.registerModule(new Nexpress());
@@ -14,5 +15,6 @@ native.registerModule(new MockMachine(process.argv[2]));
 native.registerModule(new Nmdns());
 native.registerModule(new Ncapabilities());
 native.registerModule(new Nconsole());
+native.registerModule(new ChildProcessExecutor());
 
 native.startEngine({ childProcess: false });

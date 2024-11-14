724300ab4eabfa901b3143bc8ca5be95c969e64b
Felipe Trost <felipe.trost@gmail.com>
2024-11-14 14:46:24 +0100

    feat(ms2): db table for storing engines for spaces

---
+++ src/management-system-v2/prisma/migrations/20241114120458_user_configured_engines/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Engine" (
+    "id" TEXT NOT NULL,
+    "environmentId" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "createdOn" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastEditedOn" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Engine_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Engine" ADD CONSTRAINT "Engine_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;

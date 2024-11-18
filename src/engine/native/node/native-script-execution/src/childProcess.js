a409cccf3fd6431c9f3a1b93accafacf4cb0fa62
Felipe Trost H. <felipe.trost@gmail.com>
2024-11-18 19:36:08 +0100

    engine/script execution (#388)
    * fix(ms-old): correct import
    
    * feat(engine): initial code execution
    
    There are still some rough edges and some features that need to be
    implemented.
    
    * fix(engine/native-node): don't instantiate classes 2 times when childProcess=false
    
    * fix(engine/native-script-execution): allow empty results and explicitly return 200
    
    * fix(engine/native-script-execution): exit with code 1 on failure
    
    If the result can't be posted, then we exit with code 1
    
    * fix(engine/native-script-execution): refactor middleware
    
    * fix(engine/native-script-execution): pass http port to childProcess
    
    * fix(engine/native-script-execution): kill process
    
    * fix(engine/native-script-execution): accept no result
    
    * fix(engine/native-script-execution): function calls
    
    * refactor: error message
    
    * fix(engine/native-script-execution): turned functions from async to sync
    
    * fix(engine/native-script-execution): bpmn errors
    
    * fix(engine/native-script-execution): setIntervalAsync and setTimeoutAsync
    
    * feat(engine/natve-express): include headers in request
    
    * refactor(engine/native-script-execution): use network module
    
    * fix(engine/subprocess-script-execution): correctly return fn call output
    
    * refactor(engine/script-execution): moved script execution to universal part
    
    * fix(engine/messaging): call to undefined variable
    
    * refactor(engine): optionally init subprocess script execution
    
    * refactor(engine/script-execution): don't use private classes
    
    * removed dated todo comments
    
    * refactor(engine/script-execution): stopSubprocess
    
    * refactor(engine/script-execution): multiple scripts per process instance
    
    * feat(native-node-engine/child-process): sync or async return values
    
    * fix(engine-universal/script-execution): allow instance to start multiple scripts
    
    * feat(engine/e2e-tests): use ChildProcess script execution
    
    * fix(engine/build): use webpack supported syntax
    
    * fix(engine/build): webpack node-loader for isolated-vm
    
    * fix(engine/build): external dependency child_process
    
    * fix(engine/script-execution): don't stop all scripts of a process once one finishes
    
    * fix(engine/script-execution): don't stop all scripts of a process once one finishes
    
    * refactor(engine/script-execution): renamed module
    
    * refactor: removed console.log
    
    * refactor(engine/native-script-execution): setIntervalAsync
    
    ---------
    
    Co-authored-by: Janis Joderi Shoferi <janis.joderi@web.de>

---
+++ src/engine/native/node/native-script-execution/src/childProcess.js
@@ -0,0 +1,172 @@
+const ivm = require('isolated-vm');
+
+const [
+  processId,
+  processInstanceId,
+  scriptIdentifier,
+  tokenId,
+  scriptString,
+  token,
+  processCommunicationAdress,
+] = process.argv.slice(2);
+
+if (
+  !processId ||
+  !processInstanceId ||
+  !scriptIdentifier ||
+  !tokenId ||
+  !scriptString ||
+  !token ||
+  !processCommunicationAdress
+)
+  throw new Error(
+    'Expected the following args processId, processInstanceId, tokenId, scriptString, token',
+  );
+
+async function callToExecutor(endpoint, body) {
+  try {
+    const response = await fetch(
+      `${processCommunicationAdress}/${processId}/${processInstanceId}/${scriptIdentifier}/${endpoint}`,
+      {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          ['content-type']: 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    if (!response.ok) throw new Error(`${endpoint}: Response not ok`);
+
+    const contentType = response.headers.get('content-type');
+
+    if (!contentType) {
+      return;
+    } else if (contentType.includes('application/json')) {
+      const json = await response.json();
+      return 'result' in json ? json.result : json;
+    } else {
+      return await response.text();
+    }
+  } catch (e) {
+    console.error(e);
+
+    if (endpoint === 'result') process.exit(1);
+  }
+}
+
+const isolate = new ivm.Isolate({ memoryLimit: 128 });
+const context = isolate.createContextSync();
+
+context.global.setSync('global', context.global.derefInto());
+
+context.global.setSync('_stdout_log', function (...args) {
+  console.log(...args);
+});
+
+// NOTE: Extern capabilities (provided by neo-engine)
+// log, console, variable, getService, BpmnEscalation, BpmnError
+// TODO: pass these in as a process argument
+
+// TODO: setProgress(<number between 0 - 100>)
+// TODO: getService('capabilities')
+// TODO: getService('network')
+
+const structure = {
+  log: ['get'],
+  console: ['trace', 'debug', 'info', 'warn', 'error', 'log', 'time', 'timeEnd'],
+  variable: ['get', 'set', 'getAll'],
+};
+
+for (const objName of Object.keys(structure)) {
+  const functionNames = structure[objName];
+
+  context.evalSync(`globalThis["${objName}"] = {}`);
+
+  // NOTE: maybe replace JSON for isolated-vm solution
+  for (const functionName of functionNames) {
+    context.evalClosureSync(
+      `globalThis["${objName}"]["${functionName}"] = function (...args) {
+        return $0.applySyncPromise(null, [JSON.stringify(args)], {}).copyInto();
+      }`,
+      [
+        new ivm.Reference(async function (args) {
+          const result = await callToExecutor('call', {
+            functionName: `${objName}.${functionName}`,
+            args: JSON.parse(args),
+          });
+
+          return new ivm.ExternalCopy(result);
+        }),
+      ],
+    );
+  }
+}
+
+const errorClasses = ['BpmnError', 'BpmnEscalation'];
+
+for (const errorName of errorClasses)
+  context.evalClosureSync(
+    `class ${errorName} {
+      constructor(...args) {
+        this.errorArgs = args;
+        this.errorClass = '${errorName}';
+      }
+  }
+  globalThis["${errorName}"] = ${errorName};
+  `,
+  );
+
+function wrapScriptWithErrorHandling(script) {
+  return `try {
+    ${script}
+  } catch(e){
+    if(${errorClasses.map((error) => 'e instanceof ' + error).join(' || ')})
+      throw JSON.stringify(e);
+    else throw e;
+  }
+  `;
+}
+
+const sleep = new ivm.Reference((ms) => new Promise((res) => setTimeout(res, ms)));
+
+context.evalClosureSync(
+  async function setTimeoutAsync(cb, ms) {
+    await $0.apply(null, [ms], { result: { promise: true } });
+    cb();
+  }.toString() + `globalThis["setTimeoutAsync"] = setTimeoutAsync;`,
+  [sleep],
+);
+
+context.evalClosureSync(
+  async function setIntervalAsync(cb, ms) {
+    do {
+      await $0.apply(null, [ms], { result: { promise: true } });
+    } while (!(await cb()));
+  }.toString() + 'globalThis["setIntervalAsync"]=setIntervalAsync;',
+  [sleep],
+);
+
+function wrapScriptInAsyncFunction(script) {
+  return `async function main() { ${script} }; main();`;
+}
+
+context
+  .eval(wrapScriptInAsyncFunction(wrapScriptWithErrorHandling(scriptString)), {
+    promise: true,
+    externalCopy: true,
+  })
+  .then((result) => {
+    callToExecutor('result', { result: result.copy() });
+  })
+  .catch((err) => {
+    let result = err;
+    try {
+      result = JSON.parse(err);
+    } catch (_) {}
+
+    console.error(result);
+
+    callToExecutor('result', { result }).finally(() => process.exit(1));
+  });

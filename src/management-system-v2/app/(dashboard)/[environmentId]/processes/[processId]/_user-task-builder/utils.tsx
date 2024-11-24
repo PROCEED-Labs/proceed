1917a91910b3e95120212a900641f6c33f528343
Janis Joderi Shoferi <janis.joderi@web.de>
2024-11-24 21:03:25 +0100

    Added a submit button and some data to the generated html files to make them submittable on the engine

--- src/management-system-v2/app/(dashboard)/[environmentId]/processes/[processId]/_user-task-builder/utils.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/processes/[processId]/_user-task-builder/utils.tsx
@@ -106,6 +106,21 @@ body {
   margin: 3px 3px 6px 0;
 }
 
+.user-task-form-button {
+  background-color: #eee;
+  box-shadow: rgba(0,0,0, 0.02) 0 2px 0 0;
+  padding: 4px 15px;
+  border-radius: 6px;
+  border: 1px solid lightgrey;
+}
+
+.user-task-form-button.primary-button {
+  box-shadow: rgba(5, 145, 255, 0.1) 0 2px 0 0;
+  background-color: rgb(22, 119, 255);
+  color: white;
+  border-color: rgb(22, 119, 255);
+}
+
 .user-task-form-image {
   width: 100%;
   display: flex;
@@ -165,9 +180,14 @@ export function toHtml(json: string) {
     <style>
       ${styles}
     </style>
+    <script>
+      {script}
+    </script>
   </head>
   <body>
-    ${markup}
+    <form class="form">
+      ${markup}
+    </form>
   </body>
 </html>
   `;
@@ -190,7 +210,7 @@ export const iframeDocument = `
 
       .frame-content > div {
         box-sizing: border-box;
-        padding: 0 10px;    
+        padding: 0 10px;
       }
 
       .user-task-form-column {
@@ -202,6 +222,10 @@ export const iframeDocument = `
       }
 
       .user-task-form-image {
+        position: relative;
+      }
+
+      .user-task-form-button {
         position: relative;
       }
 
@@ -267,6 +291,7 @@ export const iframeDocument = `
     <div id="mountHere">
     </div>
   </body>
+
 </html>
 `;
 

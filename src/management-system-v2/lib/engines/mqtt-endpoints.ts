0444023206d8147fbe93a9a748957a5aba234ff5
Felipe Trost <felipe.trost@gmail.com>
2024-11-29 11:44:01 +0100

    feat(ms2/mqtt): collectData

--- src/management-system-v2/lib/engines/mqtt-endpoints.ts
+++ src/management-system-v2/lib/engines/mqtt-endpoints.ts
@@ -19,45 +19,11 @@ export function getClient(options?: mqtt.IClientOptions): Promise<mqtt.MqttClien
   });
 }
 
+const mqttClient = (globalThis as any).mqttClient || ((globalThis as any).mqttClient = getClient());
+
 function getEnginePrefix(engineId: string) {
   return `${baseTopicPrefix}proceed-pms/engine/${engineId}`;
 }
-
-export async function getEngines() {
-  const client = await getClient({
-    connectTimeout: mqttTimeout,
-  });
-
-  const engines: { id: string; running: boolean; version: string }[] = [];
-
-  await client.subscribeAsync(`${getEnginePrefix('+')}/status`);
-
-  // All retained messages are sent at once
-  // The broker should bundle them in one tcp packet,
-  // after it is parsed all messages are in the queue, and handled before close
-  // is handled, as the packets where pushed to the queue before the close event was emitted.
-  // This is of course subject to the implementation of the broker,
-  // however for a small amount of engines it should be fine.
-  await new Promise<void>((res) => {
-    setTimeout(res, mqttTimeout); // Timeout in case we receive no messages
-
-    client.on('message', (topic, message) => {
-      const match = topic.match(new RegExp(`^${getEnginePrefix('')}([^\/]+)\/status`));
-      if (match) {
-        const id = match[1];
-        const status = JSON.parse(message.toString());
-        engines.push({ id, ...status });
-        res();
-      }
-    });
-  });
-
-  await client.endAsync();
-
-  return engines;
-}
-
-const requestClient = getClient();
 
 export async function mqttRequest(
   engineId: string,
@@ -69,7 +35,7 @@ export async function mqttRequest(
     page?: number;
   },
 ) {
-  const client = await requestClient;
+  const client = await mqttClient;
 
   const requestId = crypto.randomUUID();
   const requestTopic = getEnginePrefix(engineId) + '/api' + url;
@@ -109,4 +75,70 @@ export async function mqttRequest(
   client.removeListener('message', handler);
 
   return response;
+}
+
+const collectedDataEntries: Map<string, { onMessageCallback: mqtt.OnMessageCallback; data?: any }> =
+  (globalThis as any).collectedDataEntries ||
+  ((globalThis as any).collectedDataEntries = new Map());
+
+export async function collectMqttData<TData>(
+  topic: string,
+  accumulator: (topic?: string, data?: any, previousState?: TData) => TData | undefined,
+  validTopic?: (topic: string) => boolean,
+): Promise<TData | undefined> {
+  let collectedData = collectedDataEntries.get(topic);
+
+  if (!collectedData) {
+    collectedData = {
+      onMessageCallback(messageTopic, message) {
+        if (validTopic ? !validTopic(messageTopic) : messageTopic !== topic) return;
+
+        collectedData!.data = accumulator(messageTopic, message.toString(), collectedData!.data);
+
+        // TODO: do something with message
+      },
+    };
+
+    collectedDataEntries.set(topic, collectedData);
+
+    const client = await mqttClient;
+    client.on('message', collectedData.onMessageCallback);
+    await client.subscribeAsync(topic);
+  } else {
+    // if the accumulator returns data -> valid data
+    const data = accumulator(undefined, undefined, collectedData.data);
+    if (data) return data;
+  }
+
+  // If we get here, we need to fetch the data from scratch
+  // So we wait to get messages for a while
+  await new Promise((res) => setTimeout(res, mqttTimeout));
+
+  // Call without message, to let the accumulator remove stale data if it needs to
+  // Don't change the actual stored data to avoid race conditions (i'm not 100% sure about this)
+  return accumulator(undefined, undefined, collectedData.data);
+}
+
+type EngineStatus = Map<string, { id: string; running: boolean; version: string }>;
+const engineStatusRegex = new RegExp(`^${getEnginePrefix('')}([^\/]+)\/status`);
+function engineAccumulator(topic?: string, message?: string, state?: EngineStatus) {
+  if (!topic || !message) return state;
+
+  const id = topic.match(engineStatusRegex)![1];
+  const body = JSON.parse(message);
+
+  state = state || new Map();
+  state.set(id, { id, ...body });
+
+  return state;
+}
+
+export async function getEngines() {
+  const engines = await collectMqttData(
+    `${baseTopicPrefix}proceed-pms/engine/+/status`,
+    engineAccumulator,
+    (topic) => engineStatusRegex.test(topic),
+  );
+  if (engines) return Array.from(engines.values());
+  else return [];
 }

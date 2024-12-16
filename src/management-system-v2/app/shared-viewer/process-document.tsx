bdb98b704fbb3c4f7fdba9018edefba3651b7963
Felipe Trost <felipe.trost@gmail.com>
2024-12-16 03:18:14 +0100

    quickfix(ms2/documentation-page): creatorId not defined on legacy store

--- src/management-system-v2/app/shared-viewer/process-document.tsx
+++ src/management-system-v2/app/shared-viewer/process-document.tsx
@@ -256,7 +256,7 @@ const ProcessDocument: React.FC<ProcessDocumentProps> = ({
               <div className={cn(styles.Title, { [styles.TitlePage]: settings.titlepage })}>
                 <Title>{processData.name}</Title>
                 <div className={styles.TitleInfos}>
-                  <div>Owner: {processData.creatorId.split('|').pop()}</div>
+                  <div>Owner: {processData.creatorId?.split('|').pop()}</div>
                   {version.id ? (
                     <>
                       <div>Version: {version.name || version.id}</div>

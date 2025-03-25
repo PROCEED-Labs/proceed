0ab186c886a423effa3547273f6cef21ab46ef65
Felipe Trost <felipe.trost@gmail.com>
2025-03-25 17:49:05 +0100

    fix(ms2): don't create no-iam-user on build

--- src/management-system-v2/instrumentation.ts
+++ src/management-system-v2/instrumentation.ts
@@ -1,6 +1,8 @@
 export async function register() {
+  const isBuild = process.env.NEXT_PHASE === 'phase-production-build';
+
   // Register default admin user if IAM is not activated
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  if (process.env.NEXT_RUNTIME === 'nodejs' && !isBuild) {
     const { userId, createUserArgs } = await import('./lib/no-iam-user');
     const { addUser, getUserById } = await import('./lib/data/db/iam/users');
 

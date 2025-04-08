f2d4d6b014392a01fd75accffbd2cda7de378872
Felipe Trost <felipe.trost@gmail.com>
2025-04-08 10:25:26 +0200

    fix(ms2): filter admin panel options when PROCEED_PUBLIC_IAM_ACTIVATE=false

--- src/management-system-v2/app/admin/layout.tsx
+++ src/management-system-v2/app/admin/layout.tsx
@@ -4,6 +4,38 @@ import { AreaChartOutlined, AppstoreOutlined, FileOutlined } from '@ant-design/i
 import { MdOutlineComputer } from 'react-icons/md';
 import { FaUsers } from 'react-icons/fa';
 import { RiAdminFill } from 'react-icons/ri';
+import { env } from '@/lib/env-vars';
+
+let adminViews = [
+  {
+    key: 'dashboard',
+    label: <Link href="/admin">Dashboard</Link>,
+    icon: <AreaChartOutlined />,
+  },
+  {
+    key: 'spaces',
+    label: <Link href="/admin/spaces">Spaces</Link>,
+    icon: <AppstoreOutlined />,
+  },
+  {
+    key: 'users',
+    label: <Link href="/admin/users">Users</Link>,
+    icon: <FaUsers />,
+  },
+  {
+    key: 'systemadmins',
+    label: <Link href="/admin/systemadmins">Manage admins</Link>,
+    icon: <RiAdminFill />,
+  },
+  {
+    key: 'engines',
+    label: <Link href="/admin/engines">Engines</Link>,
+    icon: <MdOutlineComputer />,
+  },
+];
+
+if (!env.PROCEED_PUBLIC_IAM_ACTIVATE)
+  adminViews = adminViews.filter(({ key }) => !['users', 'systemadmins'].includes(key));
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -25,34 +57,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
         {
           type: 'group',
           label: 'System Admin views',
-          children: [
-            {
-              key: 'dashboard',
-              label: <Link href="/admin">Dashboard</Link>,
-              icon: <AreaChartOutlined />,
-            },
-            {
-              key: 'spaces',
-              label: <Link href="/admin/spaces">Spaces</Link>,
-              icon: <AppstoreOutlined />,
-            },
-            {
-              key: 'users',
-              label: <Link href="/admin/users">Users</Link>,
-              icon: <FaUsers />,
-            },
-            {
-              key: 'systemadmins',
-              label: <Link href="/admin/systemadmins">Manage admins</Link>,
-              icon: <RiAdminFill />,
-            },
-
-            {
-              key: 'engines',
-              label: <Link href="/admin/engines">Engines</Link>,
-              icon: <MdOutlineComputer />,
-            },
-          ],
+          children: adminViews,
         },
       ]}
     >

b43b6103da9f95ccfea80b0b503bfa3ba0c5b20c
Kai Rohwer <rohwer.kai@icloud.com>
2025-04-10 11:53:19 +0200

    Update next.config.js

--- src/management-system-v2/next.config.js
+++ src/management-system-v2/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
-    instrumentationHook: true,
+    //instrumentationHook: true,
   },
   /**
    *

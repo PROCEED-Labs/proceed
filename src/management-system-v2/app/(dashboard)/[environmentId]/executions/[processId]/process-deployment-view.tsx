d143bd24d8085c59f7d9b3b9f191a932f5ea954a
jjoderis <58050428+jjoderis@users.noreply.github.com>
2025-04-23 14:33:29 +0200

    MS2 Instance State Buttons (#548)
    * Using the colors from the info panel for the tokens
    
    * Added buttons in the instance view that allow a user to pause,resume and stop an instance
    
    * When starting a new instance the user gets an indication that the system is in the starting process and the new instance will be shown when it is started without deselecting the previous instance in the time between clicking the button and the new instance being started
    
    * Removed a TODO comment that is has already been adressed
    
    * Removed some redundancy
    
    * Stop the loading indicator in the pause,resume,stop buttons if the operation fails
    
    * Fixed: If starting an instance fails the loading indicator is not removed
    
    * Fixed linting

--- src/management-system-v2/app/(dashboard)/[environmentId]/executions/[processId]/process-deployment-view.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/executions/[processId]/process-deployment-view.tsx
@@ -1,23 +1,35 @@
 // TODO: remove the use client if this page is used in server
 'use client';
 
-import { Button, Select, Tooltip, Space, Dropdown, Result, Skeleton } from 'antd';
+import { Button, Select, Tooltip, Space, Dropdown, Result } from 'antd';
 import Content from '@/components/content';
 import BPMNCanvas, { BPMNCanvasRef } from '@/components/bpmn-canvas';
 import { Toolbar, ToolbarGroup } from '@/components/toolbar';
-import { PlusOutlined, InfoCircleOutlined, FilterOutlined } from '@ant-design/icons';
-import { Suspense, useCallback, useMemo, useRef, useState } from 'react';
+import {
+  PlusOutlined,
+  InfoCircleOutlined,
+  FilterOutlined,
+  CaretRightOutlined,
+  PauseOutlined,
+  StopOutlined,
+} from '@ant-design/icons';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import contentStyles from './content.module.scss';
-import styles from '@/app/(dashboard)/[environmentId]/processes/[processId]/modeler-toolbar.module.scss';
+import toolbarStyles from '@/app/(dashboard)/[environmentId]/processes/[processId]/modeler-toolbar.module.scss';
+import styles from './process-deployment-view.module.scss';
 import InstanceInfoPanel from './instance-info-panel';
 import { useSearchParamState } from '@/lib/use-search-param-state';
 import { MdOutlineColorLens } from 'react-icons/md';
 import { ColorOptions, colorOptions } from './instance-coloring';
 import { RemoveReadOnly } from '@/lib/typescript-utils';
 import type { ElementLike } from 'diagram-js/lib/core/Types';
-import { startInstance } from '@/lib/engines/server-actions';
+import {
+  startInstance,
+  pauseInstance,
+  resumeInstance,
+  stopInstance,
+} from '@/lib/engines/server-actions';
 import { useEnvironment } from '@/components/auth-can';
-import { useRouter } from 'next/navigation';
 import { wrapServerCall } from '@/lib/wrap-server-call';
 import useDeployment from '../deployment-hook';
 import { getLatestDeployment, getVersionInstances, getYoungestInstance } from './instance-helpers';
@@ -38,17 +50,33 @@ export default function ProcessDeploymentView({
   const [selectedColoring, setSelectedColoring] = useState<ColorOptions>('processColors');
   const [selectedElement, setSelectedElement] = useState<ElementLike | undefined>();
 
+  const [startingInstance, setStartingInstance] = useState(false);
+  const [resumingInstance, setResumingInstance] = useState(false);
+  const [pausingInstance, setPausingInstance] = useState(false);
+  const [stoppingInstance, setStoppingInstance] = useState(false);
+
   const canvasRef = useRef<BPMNCanvasRef>(null);
   const [infoPanelOpen, setInfoPanelOpen] = useState(false);
 
   const { spaceId } = useEnvironment();
 
-  const { data: deploymentInfo } = useDeployment(processId, initialDeploymentInfo);
+  const { data: deploymentInfo, refetch } = useDeployment(processId, initialDeploymentInfo);
 
-  const router = useRouter();
-
-  const { selectedVersion, instances, selectedInstance, currentVersion } = useMemo(() => {
+  const {
+    selectedVersion,
+    instances,
+    selectedInstance,
+    currentVersion,
+    instanceIsRunning,
+    instanceIsPausing,
+    instanceIsPaused,
+  } = useMemo(() => {
     let selectedVersion, instances, selectedInstance, currentVersion;
+    let instanceIsRunning = false;
+    let instanceIsPausing = false;
+    let instanceIsPaused = false;
+
+    const activeStates = ['PAUSED', 'RUNNING', 'READY', 'DEPLOYMENT-WAITING', 'WAITING'];
 
     if (deploymentInfo) {
       selectedVersion = deploymentInfo.versions.find((v) => v.versionId === selectedVersionId);
@@ -61,6 +89,11 @@ export default function ProcessDeploymentView({
       let currentVersionId = getLatestDeployment(deploymentInfo).versionId;
       if (selectedInstance) {
         currentVersionId = selectedInstance.processVersion;
+        instanceIsRunning = selectedInstance.instanceState.some((state) =>
+          activeStates.includes(state),
+        );
+        instanceIsPausing = selectedInstance.instanceState.some((state) => state === 'PAUSING');
+        instanceIsPaused = selectedInstance.instanceState.some((state) => state === 'PAUSED');
       } else if (selectedVersionId) {
         currentVersionId = selectedVersionId;
       }
@@ -69,7 +102,15 @@ export default function ProcessDeploymentView({
       );
     }
 
-    return { selectedVersion, instances, selectedInstance, currentVersion };
+    return {
+      selectedVersion,
+      instances,
+      selectedInstance,
+      currentVersion,
+      instanceIsRunning,
+      instanceIsPausing,
+      instanceIsPaused,
+    };
   }, [deploymentInfo, selectedVersionId, selectedInstanceId]);
 
   const selectedBpmn = useMemo(() => {
@@ -103,7 +144,7 @@ export default function ProcessDeploymentView({
           height: '100%',
         }}
       >
-        <Toolbar className={styles.Toolbar}>
+        <Toolbar className={toolbarStyles.Toolbar}>
           <Space
             aria-label="general-modeler-toolbar"
             style={{
@@ -130,19 +171,22 @@ export default function ProcessDeploymentView({
               <Tooltip title="Start new instance">
                 <Button
                   icon={<PlusOutlined />}
-                  onClick={() => {
-                    wrapServerCall({
+                  loading={startingInstance}
+                  onClick={async () => {
+                    setStartingInstance(true);
+                    await wrapServerCall({
                       fn: () =>
                         startInstance(
                           deploymentInfo.definitionId,
                           getLatestDeployment(deploymentInfo).versionId,
                           spaceId,
                         ),
-                      onSuccess: (instanceId) => {
+                      onSuccess: async (instanceId) => {
+                        await refetch();
                         setSelectedInstanceId(instanceId);
-                        router.refresh();
                       },
                     });
+                    setStartingInstance(false);
                   }}
                 />
               </Tooltip>
@@ -208,9 +252,69 @@ export default function ProcessDeploymentView({
               </Tooltip>
             </ToolbarGroup>
 
+            {selectedInstance && (
+              <ToolbarGroup>
+                <Tooltip
+                  title={instanceIsPausing ? 'Abort pausing the instance' : 'Resume the instance'}
+                >
+                  <Button
+                    className={styles.PlayIcon}
+                    icon={<CaretRightOutlined />}
+                    loading={resumingInstance}
+                    disabled={!instanceIsPausing && !instanceIsPaused}
+                    onClick={async () => {
+                      setResumingInstance(true);
+                      await wrapServerCall({
+                        fn: () =>
+                          resumeInstance(processId, selectedInstance.processInstanceId, spaceId),
+                        onSuccess: async () => await refetch(),
+                      });
+                      setResumingInstance(false);
+                    }}
+                  />
+                </Tooltip>
+
+                <Tooltip title="Pause the instance">
+                  <Button
+                    className={styles.PauseIcon}
+                    icon={<PauseOutlined />}
+                    loading={pausingInstance || instanceIsPausing}
+                    disabled={!instanceIsRunning || instanceIsPausing || instanceIsPaused}
+                    onClick={async () => {
+                      setPausingInstance(true);
+                      await wrapServerCall({
+                        fn: async () =>
+                          pauseInstance(processId, selectedInstance.processInstanceId, spaceId),
+                        onSuccess: async () => await refetch(),
+                      });
+                      setPausingInstance(false);
+                    }}
+                  />
+                </Tooltip>
+
+                <Tooltip title="Stop the instance">
+                  <Button
+                    className={styles.StopIcon}
+                    icon={<StopOutlined />}
+                    loading={stoppingInstance}
+                    disabled={!instanceIsRunning}
+                    onClick={async () => {
+                      setStoppingInstance(true);
+                      await wrapServerCall({
+                        fn: async () =>
+                          stopInstance(processId, selectedInstance.processInstanceId, spaceId),
+                        onSuccess: async () => await refetch(),
+                      });
+                      setStoppingInstance(false);
+                    }}
+                  />
+                </Tooltip>
+              </ToolbarGroup>
+            )}
+
             <Space style={{ alignItems: 'start' }}>
               <ToolbarGroup>
-                <Tooltip title="Start new instance">
+                <Tooltip title={infoPanelOpen ? 'Close Info Panel' : 'Open Info Panel'}>
                   <Button
                     icon={<InfoCircleOutlined />}
                     onClick={() => setInfoPanelOpen((prev) => !prev)}

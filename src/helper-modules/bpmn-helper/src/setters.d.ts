cde02c5e872b3a02ca93609f746dc67d41cd9b93
anish <anishsapkota054@gmail.com>
2025-04-27 23:51:33 +0200

    regenerate types for helper modules

--- src/helper-modules/bpmn-helper/src/setters.d.ts
+++ src/helper-modules/bpmn-helper/src/setters.d.ts
@@ -8,7 +8,7 @@
  * @param {string} id - the id we want to set the definitions element to
  * @returns {Promise<string|object>} the modified BPMN process as bpmn-moddle object or XML string based on input
  */
-export function setDefinitionsId(bpmn: string | object, id: string): Promise<string | object>;
+export function setDefinitionsId(bpmn: (string | object), id: string): Promise<string | object>;
 /**
  *  Sets name in definitions element to given name
  *
@@ -16,7 +16,7 @@ export function setDefinitionsId(bpmn: string | object, id: string): Promise<str
  * @param {string} name - the id we want to set the definitions element to
  * @returns {Promise<string|object>} the modified BPMN process as bpmn-moddle object or XML string based on input
  */
-export function setDefinitionsName(bpmn: string | object, name: string): Promise<string | object>;
+export function setDefinitionsName(bpmn: (string | object), name: string): Promise<string | object>;
 /**
  * Will set a version in the definitions element
  *
@@ -29,22 +29,13 @@ export function setDefinitionsName(bpmn: string | object, name: string): Promise
  * @param {(string)} [versionInformation.versionCreatedOn] - a timestamp (UTC) when the version was created
  * @returns {Promise<string|object>} the modified BPMN process as bpmn-moddle object or XML string based on input
  */
-export function setDefinitionsVersionInformation(
-  bpmn: string | object,
-  {
-    versionId,
-    versionName,
-    versionDescription,
-    versionBasedOn,
-    versionCreatedOn,
-  }: {
-    versionId?: string;
+export function setDefinitionsVersionInformation(bpmn: (string | object), { versionId, versionName, versionDescription, versionBasedOn, versionCreatedOn }: {
+    versionId?: (string);
     versionName?: string;
     versionDescription?: string;
-    versionBasedOn?: string;
-    versionCreatedOn?: string;
-  },
-): Promise<string | object>;
+    versionBasedOn?: (string);
+    versionCreatedOn?: (string);
+}): Promise<string | object>;
 /**
  *  Sets process Id in definitions element
  *
@@ -68,7 +59,7 @@ export function setTemplateId(bpmn: string, id: string): Promise<string | object
  * @param {string} id the id to be used for the targetNamespace
  * @returns {Promise<string|object>} the modified BPMN process as bpmn-moddle object or XML string based on input
  */
-export function setTargetNamespace(bpmn: string | object, id: string): Promise<string | object>;
+export function setTargetNamespace(bpmn: (string | object), id: string): Promise<string | object>;
 /**
  * Sets exporter, exporterVersion, expressionLanguage, typeLanguage and needed namespaces on defintions element
  * stores the previous values of exporter and exporterVersion if there are any
@@ -78,12 +69,7 @@ export function setTargetNamespace(bpmn: string | object, id: string): Promise<s
  * @param {string} exporterVersion - the exporter version
  * @returns {Promise<string|object>} the modified BPMN process as bpmn-moddle object or XML string based on input
  */
-export function setStandardDefinitions(
-  bpmn: string | object,
-  exporterName: string,
-  exporterVersion: string,
-): Promise<string | object>;
-/**
+export function setStandardDefinitions(bpmn: (string | object), exporterName: string, exporterVersion: string): Promise<string | object>;
 /**
  * Sets deployment method of a process
  *
@@ -91,10 +77,7 @@ export function setStandardDefinitions(
  * @param {string} method the method we want to set (dynamic/static)
  * @returns {Promise<string|object>} the modified BPMN process as bpmn-moddle object or XML string based on input
  */
-export function setDeploymentMethod(
-  bpmn: string | object,
-  method: string,
-): Promise<string | object>;
+export function setDeploymentMethod(bpmn: (string | object), method: string): Promise<string | object>;
 /**
  * Function that sets the machineInfo of all elements in the given xml with the given machineIds
  *
@@ -102,15 +85,12 @@ export function setDeploymentMethod(
  * @param {{[elementId: string]: {machineAddress?: string, machineId?: string}}} machineInfo the machineAddresses and machineIps of all the elements we want to set
  * @returns {Promise<string|object>} the BPMN process as XML string or BPMN-Moddle Object based on input
  */
-export function setMachineInfo(
-  bpmn: string | object,
-  machineInfo: {
+export function setMachineInfo(bpmn: (string | object), machineInfo: {
     [elementId: string]: {
-      machineAddress?: string;
-      machineId?: string;
+        machineAddress?: string;
+        machineId?: string;
     };
-  },
-): Promise<string | object>;
+}): Promise<string | object>;
 /**
  * Sets the 'fileName' and 'implementation' attributes of a UserTask with new values.
  *
@@ -120,12 +100,7 @@ export function setMachineInfo(
  * @param {string} [newImplementation] - the new value of 'implementation' attribute; will default to html implementation
  * @returns {Promise<string|object>} the BPMN process as XML string or BPMN-Moddle Object based on input
  */
-export function setUserTaskData(
-  bpmn: string | object,
-  userTaskId: string,
-  newFileName: string,
-  newImplementation?: string,
-): Promise<string | object>;
+export function setUserTaskData(bpmn: (string | object), userTaskId: string, newFileName: string, newImplementation?: string): Promise<string | object>;
 /**
  * Sets the 'fileName' attributes of a ScriptTask with new values.
  *
@@ -134,11 +109,7 @@ export function setUserTaskData(
  * @param {string} newFileName - the new value of 'fileName' attribute
  * @returns {Promise<string|object>} the BPMN process as XML string or BPMN-Moddle Object based on input
  */
-export function setScriptTaskData(
-  bpmn: string | object,
-  scriptTaskId: string,
-  newFileName: string,
-): Promise<string | object>;
+export function setScriptTaskData(bpmn: (string | object), scriptTaskId: string, newFileName: string): Promise<string | object>;
 /**
  * Adds the given constraints to the bpmn element with the given id
  *
@@ -147,11 +118,7 @@ export function setScriptTaskData(
  * @param {object} constraints object containing the hardConstraints and softConstraints
  * @returns {Promise<string|object>} the BPMN process as XML string or BPMN-Moddle Object based on input
  */
-export function addConstraintsToElementById(
-  bpmn: string | object,
-  elementId: string,
-  constraints: object,
-): Promise<string | object>;
+export function addConstraintsToElementById(bpmn: (string | object), elementId: string, constraints: object): Promise<string | object>;
 /**
  * Add meta information of the called bpmn process to the bpmn file where it's getting called from. This includes a custom namespace in the definitions part,
  * an import element as first child of definitions and the calledElement attribute of the call activity bpmn element
@@ -162,12 +129,7 @@ export function addConstraintsToElementById(
  * @param {string} calledProcessLocation The DefinitionId of the calledBpmn. Combination of process name and process id
  * @returns {Promise<string|object>} the BPMN process as XML string or BPMN-Moddle Object based on input
  */
-export function addCallActivityReference(
-  bpmn: string | object,
-  callActivityId: string,
-  calledBpmn: string,
-  calledProcessLocation: string,
-): Promise<string | object>;
+export function addCallActivityReference(bpmn: (string | object), callActivityId: string, calledBpmn: string, calledProcessLocation: string): Promise<string | object>;
 /**
  * Remove the reference to the called process added in {@link addCallActivityReference} but remains the actual bpmn element
  *
@@ -175,17 +137,14 @@ export function addCallActivityReference(
  * @param {string} callActivityId The ID of the bpmn element for which the meta information should be removed
  * @returns {Promise<string|object>} the BPMN process as XML string or BPMN-Moddle Object based on input
  */
-export function removeCallActivityReference(
-  bpmn: string | object,
-  callActivityId: string,
-): Promise<string | object>;
+export function removeCallActivityReference(bpmn: (string | object), callActivityId: string): Promise<string | object>;
 /**
  * Look up the given bpmn document for unused imports/custom namespaces which don't get referenced by a call activity inside this bpmn document.
  *
  * @param {(string|object)} bpmn - the process definition as XML string or BPMN-Moddle Object
  * @returns {Promise<string|object>} the BPMN process as XML string or BPMN-Moddle Object based on input
  */
-export function removeUnusedCallActivityReferences(bpmn: string | object): Promise<string | object>;
+export function removeUnusedCallActivityReferences(bpmn: (string | object)): Promise<string | object>;
 /**
  * Remove color from all elements of given process
  *
@@ -200,10 +159,7 @@ export function removeColorFromAllElements(bpmn: string | object): Promise<strin
  * @param {string} [description] the content for the documentation element
  * @returns {Promise<string|object>} the BPMN process as XML string or BPMN-Moddle Object based on input
  */
-export function addDocumentation(
-  bpmn: string | object,
-  description?: string,
-): Promise<string | object>;
+export function addDocumentation(bpmn: (string | object), description?: string): Promise<string | object>;
 /**
  * Adds documentation to a given process object
  *
@@ -225,23 +181,25 @@ export function updatePerformersOnElement(element: object, performers: any[]): P
  * @param {string} elementId
  * @param {Array} performers the performer data to emplace in the element
  */
-export function updatePerformersOnElementById(
-  bpmn: string | object,
-  elementId: string,
-  performers: any[],
-): Promise<string | object>;
-
+export function updatePerformersOnElementById(bpmn: (string | object), elementId: string, performers: any[]): Promise<string | object>;
 /**
- * Updates the general BPMN XML attributes of the given BPMN process.
+ * Updates the creator BPMN XML attributes of the given BPMN process.
  *
- * @param bpmn - The BPMN process definition as an XML string or a BPMN-Moddle object.
- * @param attributes - The attributes to update in the BPMN process.
- * @returns A promise that resolves to the modified BPMN process,
+ * @param {(string | object)} bpmn - The BPMN process definition as an XML string or a BPMN-Moddle object.
+ * @param {object} attributes - The attributes to update in the BPMN process.
+ * @param {string} [attributes.id] - The id of the BPMN process.
+ * @param {string} [attributes.name] - The name of the BPMN process.
+ * @param {string} [attributes.creatorId] - The unique identifier of the creator.
+ * @param {string} [attributes.creatorName] - The name of the creator.
+ * @param {string} [attributes.creatorUsername] - The username of the creator.
+ * @param {string} [attributes.creatorSpaceId] - The unique identifier of the creator's space.
+ * @param {string} [attributes.creatorSpaceName] - The name of the creator's space.
+ * @param {string} [attributes.userDefinedId] - The user-defined ID of the BPMN process.
+ * @param {string} [attributes.creationDate] - The creation date of the BPMN process in string format.
+ * @returns {Promise<string | object>} A promise that resolves to the modified BPMN process,
  * either as a BPMN-Moddle object or an XML string, depending on the input format.
  */
-export function updateBpmnCreatorAttributes(
-  bpmn: string | object,
-  attributes: {
+export function updateBpmnCreatorAttributes(bpmn: (string | object), { id, name, creatorId, creatorName, creatorUsername, creatorSpaceName, creatorSpaceId, userDefinedId, creationDate, }: {
     id?: string;
     name?: string;
     creatorId?: string;
@@ -251,35 +209,42 @@ export function updateBpmnCreatorAttributes(
     creatorSpaceName?: string;
     userDefinedId?: string;
     creationDate?: string;
-  },
-): Promise<string | object>;
-
+}): Promise<string | object>;
 /**
  * Updates the original attributes of a copied/imported BPMN process.
  *
- * @param bpmn - The BPMN process definition as an XML string or a BPMN-Moddle object.
- * @param attributes - The original attributes to update in the BPMN process.
- * @returns A promise that resolves to the modified BPMN process,
+ * @param {(string | object)} bpmn - The BPMN process definition as an XML string or a BPMN-Moddle object.
+ * @param {object} attributes - The original attributes to update in the BPMN process.
+ * @param {string} [attributes.originalId] - The original of a copied/imported BPMN process.
+ * @param {string} [attributes.originalName] - The original name of a copied/imported BPMN process.
+ * @param {string} [attributes.originalUserDefinedId] - The original user-defined ID of a copied/imported BPMN process.
+ * @param {string} [attributes.originalCreationDate] - The original creation date of a copied/imported BPMN process.
+ * @param {string} [attributes.originalProcessVersionId] - The original process version ID of a copied/imported BPMN process.
+ * @param {string} [attributes.originalProcessVersionName] - The original process version name of a copied/imported BPMN process.
+ * @param {string} [attributes.originalExporter] - The original exporter of a copied/imported BPMN process.
+ * @param {string} [attributes.originalExporterVersion] - The original exporter version of a copied/imported BPMN process.
+ * @param {string} [attributes.originalTargetNamespace] - The original target namespace of a copied/imported BPMN process.
+ * @param {string} [attributes.originalCreatorSpaceId] - The original creator space ID of a copied/imported BPMN process.
+ * @param {string} [attributes.originalCreatorSpaceName] - The original creator space name of a copied/imported BPMN process.
+ * @param {string} [attributes.originalCreatorId] - The original creator ID of a copied/imported BPMN process.
+ * @param {string} [attributes.originalCreatorName] - The original creator name of a copied/imported BPMN process.
+ * @param {string} [attributes.originalCreatorUsername] - The original creator username of a copied/imported BPMN process.
+ * @returns {Promise<string | object>} A promise that resolves to the modified BPMN process,
  * either as a BPMN-Moddle object or an XML string, depending on the input format.
  */
-export function updateBpmnOriginalAttributes(
-  bpmn: string | object,
-  attributes: {
+export function updateBpmnOriginalAttributes(bpmn: (string | object), { originalId, originalName, originalUserDefinedId, originalCreationDate, originalProcessVersionId, originalProcessVersionName, originalExporter, originalExporterVersion, originalTargetNamespace, originalCreatorSpaceId, originalCreatorSpaceName, originalCreatorId, originalCreatorName, originalCreatorUsername, }: {
     originalId?: string;
     originalName?: string;
-    originalCreationDate?: string;
-    originalCreatorId?: string;
-    originalCreatorName?: string;
-    originalCreatorUsername?: string;
-    originalCreatorSpaceId?: string;
-    originalCreatorSpaceName?: string;
     originalUserDefinedId?: string;
-    originalVersionId?: string;
-    originalVersionName?: string;
+    originalCreationDate?: string;
     originalProcessVersionId?: string;
     originalProcessVersionName?: string;
     originalExporter?: string;
     originalExporterVersion?: string;
     originalTargetNamespace?: string;
-  },
-): Promise<string | object>;
+    originalCreatorSpaceId?: string;
+    originalCreatorSpaceName?: string;
+    originalCreatorId?: string;
+    originalCreatorName?: string;
+    originalCreatorUsername?: string;
+}): Promise<string | object>;

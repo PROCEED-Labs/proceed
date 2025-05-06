fafc5af25594e1b1bf0a720ecdd8a83cc23d4f40
Janis Joderi Shoferi <janis.joderi@web.de>
2025-05-06 17:30:33 +0200

    Removed unnecessary id entry in space settings db table and using the unique environmentId as the tables id

--- src/management-system-v2/prisma/migrations/20250506150528_space_settings/migration.sql
+++ src/management-system-v2/prisma/migrations/20250506150528_space_settings/migration.sql
@@ -1,10 +1,9 @@
 -- CreateTable
 CREATE TABLE "space_settings" (
-    "id" TEXT NOT NULL,
     "environmentId" TEXT NOT NULL,
     "settings" JSONB NOT NULL,
 
-    CONSTRAINT "space_settings_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "space_settings_pkey" PRIMARY KEY ("environmentId")
 );
 
 -- CreateIndex

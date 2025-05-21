5f7bdbc775ad5283fc46c6e10f9bddf0196253ec
Felipe Trost <felipe.trost@gmail.com>
2025-05-21 10:20:27 +0200

    comments

--- src/management-system-v2/instrumentation.ts
+++ src/management-system-v2/instrumentation.ts
@@ -1,14 +1,15 @@
 export async function register() {
-  // Register default admin user if IAM is not activated
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { getMSConfigDBValuesAndEnsureDefaults } = await import('./lib/ms-config/ms-config');
     getMSConfigDBValuesAndEnsureDefaults();
 
+    // Register default admin user if IAM is not activated
     const { userId, createUserArgs } = await import('./lib/no-iam-user');
     const { addUser, getUserById } = await import('./lib/data/db/iam/users');
     if (!process.env.PROCEED_PUBLIC_IAM_ACTIVATE && !(await getUserById(userId)))
       await addUser(createUserArgs);
 
+    // Import db seed
     const { importSeed } = await import('./lib/db-seed');
     await importSeed();
   }

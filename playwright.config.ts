8f310ed46ce5ca6fad0abe4e7366c290e1a92e39
Kai Rohwer <rohwer.kai@icloud.com>
2025-06-06 04:17:45 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     timeout: 7000,
   },
   /* Run tests in files in parallel */
-  //  fullyParallel: true,
+  // fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */

2265b9e8f4ee2968a116a8c72889c7a39917d732
Felipe Trost <felipe.trost@gmail.com>
2025-06-22 15:18:54 +0200

    more merge conflicts

--- src/management-system-v2/lib/engines/server-actions.ts
+++ src/management-system-v2/lib/engines/server-actions.ts
@@ -8,7 +8,7 @@ import {
   getDeployments as fetchDeployments,
   removeDeploymentFromMachines,
 } from './deployment';
-import { Engine, SpaceEngine, getCorrectTargetEngines } from './machines';
+import { Engine, SpaceEngine } from './machines';
 import { savedEnginesToEngines } from './saved-engines-helpers';
 import { getCurrentEnvironment } from '@/components/auth';
 import { enableUseDB } from 'FeatureFlags';
@@ -45,6 +45,34 @@ import {
   updateUserTask,
   deleteUserTask,
 } from '../data/user-tasks';
+
+async function getCorrectTargetEngines(
+  spaceId: string,
+  onlyProceedEngines = false,
+  validatorFunc?: (engine: Engine) => Promise<boolean>,
+) {
+  const { ability } = await getCurrentEnvironment(spaceId);
+
+  let engines: Engine[] = [];
+  if (onlyProceedEngines) {
+    // force that only proceed engines are supposed to be used
+    const proceedSavedEngines = await getDbEngines(null, undefined, 'dont-check');
+    engines = await savedEnginesToEngines(proceedSavedEngines);
+  } else {
+    // use all available engines
+    const [proceedEngines, spaceEngines] = await Promise.allSettled([
+      getDbEngines(null, undefined, 'dont-check').then(savedEnginesToEngines),
+      getDbEngines(spaceId, ability).then(savedEnginesToEngines),
+    ]);
+
+    if (proceedEngines.status === 'fulfilled') engines = proceedEngines.value;
+    if (spaceEngines.status === 'fulfilled') engines = engines.concat(spaceEngines.value);
+  }
+
+  if (validatorFunc) engines = await asyncFilter(engines, validatorFunc);
+
+  return engines;
+}
 
 export async function deployProcess(
   definitionId: string,

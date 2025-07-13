56be48d2ab8eaf66a202caa63fa5de0c42e766ca
Kai Rohwer <rohwer.kai@icloud.com>
2025-07-14 00:05:12 +0200

    MQTT link status checking

--- src/management-system-v2/lib/custom-links/get-link-state.ts
+++ src/management-system-v2/lib/custom-links/get-link-state.ts
@@ -1,5 +1,6 @@
 import 'server-only';
 import { CustomNavigationLink } from './custom-link';
+import mqtt from 'mqtt';
 
 export async function checkCustomLinkStatus(link: CustomNavigationLink): Promise<boolean> {
   try {
@@ -7,8 +8,15 @@ export async function checkCustomLinkStatus(link: CustomNavigationLink): Promise
       const response = await fetch(link.address);
       return response.ok;
     } else if (link.address.startsWith('mqtt')) {
-      // TODO: mqtt
-      return false;
+      // TODO: cache connections, but also different solution for serverless deployments.
+      return await mqtt
+        .connectAsync(link.address)
+        .then((client) => {
+          // TODO: check topic if given.
+          client.end();
+          return true;
+        })
+        .catch(() => false);
     } else {
       throw new Error('Unsupported URL protocol');
     }

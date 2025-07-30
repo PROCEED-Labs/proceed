0116920b83b311aee71225d2e648edeb8b714b3a
Felipe Trost H. <felipe.trost@gmail.com>
2025-07-30 09:28:11 +0200

    ms2/user management features (#625)
    * feat(ms2/profile): change password
    
    * feat(ms2/db): flag for temporary passwords
    
    * feat(ms2): show hint when user has a temporary password
    
    * feat(ms2/org-user-dashboard): create users and add them to organizations
    
    * feat(ms2/create-user): select roles for added user
    
    * refactor(ms2/favorite-star): removed unnecessary server call
    
    * format

--- src/management-system-v2/app/(dashboard)/[environmentId]/layout.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/layout.tsx
@@ -41,6 +41,7 @@ import { CustomLinkStateProvider } from '@/lib/custom-links/client-state';
 import { CustomLink } from '@/lib/custom-links/state';
 import { customLinkIcons } from '@/lib/custom-links/icons';
 import { CustomNavigationLink } from '@/lib/custom-links/custom-link';
+import { getUserPassword } from '@/lib/data/db/iam/users';
 
 const DashboardLayout = async ({
   children,
@@ -72,6 +73,9 @@ const DashboardLayout = async ({
   const topCustomNavLinks = customNavLinks.filter((link) => link.position === 'top');
   const middleCustomNavLinks = customNavLinks.filter((link) => link.position === 'middle');
   const bottomCustomNavLinks = customNavLinks.filter((link) => link.position === 'bottom');
+
+  const userPassword = await getUserPassword(user!.id);
+  const userNeedsToChangePassword = userPassword ? userPassword.isTemporaryPassword : false;
 
   let layoutMenuItems: MenuProps['items'] = [];
 
@@ -301,6 +305,7 @@ const DashboardLayout = async ({
           layoutMenuItems={layoutMenuItems}
           activeSpace={activeEnvironment}
           customLogo={logo}
+          userNeedsToChangePassword={userNeedsToChangePassword}
           bottomMenuItems={bottomCustomNavLinks.map((link, idx) => ({
             key: idx,
             label: <CustomLink link={link} />,

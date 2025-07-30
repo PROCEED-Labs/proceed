6482b878518bc560c9c5afa94fe75d0926e8c91e
Felipe Trost <felipe.trost@gmail.com>
2025-07-30 09:57:10 +0200

    remove "New Organization" button

--- src/management-system-v2/app/(dashboard)/[environmentId]/spaces/environments-page.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/spaces/environments-page.tsx
@@ -3,11 +3,10 @@
 import Bar from '@/components/bar';
 import { OrganizationEnvironment } from '@/lib/data/environment-schema';
 import { Button } from 'antd';
-import { FC, use } from 'react';
+import { FC } from 'react';
 import useFuzySearch, { ReplaceKeysWithHighlighted } from '@/lib/useFuzySearch';
 import ElementList from '@/components/item-list-view';
 import Link from 'next/link';
-import { EnvVarsContext } from '@/components/env-vars-context';
 
 const highlightedKeys = ['name', 'description'] as const;
 export type FilteredEnvironment = ReplaceKeysWithHighlighted<
@@ -18,7 +17,6 @@ export type FilteredEnvironment = ReplaceKeysWithHighlighted<
 const EnvironmentsPage: FC<{ organizationEnvironments: OrganizationEnvironment[] }> = ({
   organizationEnvironments,
 }) => {
-  const env = use(EnvVarsContext);
   const { searchQuery, filteredData, setSearchQuery } = useFuzySearch({
     data: organizationEnvironments,
     keys: ['name', 'description'],
@@ -29,13 +27,6 @@ const EnvironmentsPage: FC<{ organizationEnvironments: OrganizationEnvironment[]
   return (
     <>
       <Bar
-        leftNode={
-          !env.PROCEED_PUBLIC_IAM_ONLY_ONE_ORGANIZATIONAL_SPACE ? (
-            <Link href="/create-organization">
-              <Button type="primary">New Organization</Button>
-            </Link>
-          ) : null
-        }
         searchProps={{
           value: searchQuery,
           onChange: (e) => setSearchQuery(e.target.value),

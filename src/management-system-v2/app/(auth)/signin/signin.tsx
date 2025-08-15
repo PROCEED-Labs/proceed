7e40bef77a3b8857a7703aae757c9ae2fd8bd8a3
Felipe Trost <felipe.trost@gmail.com>
2025-08-15 13:04:39 +0200

    refactor(ms2/signin): unify signin option tabs

--- src/management-system-v2/app/(auth)/signin/signin.tsx
+++ src/management-system-v2/app/(auth)/signin/signin.tsx
@@ -1,27 +1,16 @@
 'use client';
 
-import { FC, Fragment, use, useEffect, useState } from 'react';
+import { FC, use } from 'react';
 import {
   Typography,
   Alert,
   Form,
-  Input,
   Button as AntDesignButton,
   Divider,
   ButtonProps,
   ConfigProvider,
-  TabsProps,
-  Tabs,
-  Grid,
-  Select,
   AlertProps,
 } from 'antd';
-
-import { GoOrganization } from 'react-icons/go';
-import { MdLogin } from 'react-icons/md';
-import { BsFillPersonPlusFill } from 'react-icons/bs';
-import { MdEmail } from 'react-icons/md';
-import { FaCircleArrowUp } from 'react-icons/fa6';
 
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
@@ -30,6 +19,7 @@ import { signIn } from 'next-auth/react';
 import { type ExtractedProvider } from '@/lib/auth';
 import { EnvVarsContext } from '@/components/env-vars-context';
 import AuthModal from '../auth-modal';
+import { SigninOptions } from '@/components/signin-options';
 
 const verticalGap = '1rem';
 
@@ -64,35 +54,6 @@ const signInTitle = (
   </Typography.Title>
 );
 
-const CredentialsSignIn = ({
-  provider,
-  callbackUrl,
-}: {
-  provider: Extract<ExtractedProvider, { type: 'credentials' }>;
-  callbackUrl?: string;
-}) => {
-  return (
-    <Form
-      onFinish={(values) => signIn(provider.id, { ...values, callbackUrl })}
-      key={provider.id}
-      layout="vertical"
-    >
-      {Object.keys(provider.credentials).map((key) => (
-        <Form.Item name={key} key={key} style={{ marginBottom: '.5rem' }}>
-          <Input
-            placeholder={provider.credentials[key].label}
-            type={provider.credentials[key].type as string}
-            defaultValue={provider.credentials[key].value as string}
-          />
-        </Form.Item>
-      ))}
-      <Button htmlType="submit" style={{ marginBottom: verticalGap }}>
-        {provider.name}
-      </Button>
-    </Form>
-  );
-};
-
 const SignIn: FC<{
   providers: ExtractedProvider[];
   userType: 'guest' | 'user' | 'none';
@@ -100,7 +61,6 @@ const SignIn: FC<{
   logoUrl?: string;
 }> = ({ providers, userType, guestReferenceToken, logoUrl }) => {
   const env = use(EnvVarsContext);
-  const breakpoint = Grid.useBreakpoint();
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') ?? undefined;
   const callbackUrlWithGuestRef = guestReferenceToken
@@ -115,156 +75,7 @@ const SignIn: FC<{
     authErrorType = errorTypeMatch.groups.type;
   }
 
-  const oauthProviders = providers.filter((provider) => ['oauth', 'oidc'].includes(provider.type));
   const guestProvider = providers.find((provider) => provider.id === 'guest-signin');
-
-  const emailProvider = providers.find((provider) => provider.type === 'email');
-  const passwordSigninProvider = providers.find(
-    (provider) => provider.id === 'username-password-signin',
-  );
-  const passwordSignupProvider = providers.find(
-    (provider) => provider.id === 'register-as-new-user',
-  );
-  const developmentUsersProvider = providers.find(
-    (provider) => provider.id === 'development-users',
-  );
-
-  const [_activeIndex, setActiveIndex] = useState<string | null>(null);
-
-  // We need to wait until the component is mounted on the client
-  // to open the modal, otherwise it will cause a hydration mismatch
-  const [open, setOpen] = useState(false);
-  useEffect(() => {
-    setOpen(true);
-  }, [setOpen]);
-
-  type Tab = NonNullable<TabsProps['items']>[number];
-  const tabs: (Tab & { href?: string; onClick?: () => void })[] = [];
-
-  if (developmentUsersProvider) {
-    tabs.push({
-      icon: <FaCircleArrowUp size={26} />,
-      label: 'Sign In as Development User',
-      key: 'development-users',
-      children: (
-        <Form
-          onFinish={(values) =>
-            signIn(developmentUsersProvider.id, { ...values, callbackUrl: callbackUrlWithGuestRef })
-          }
-          key={developmentUsersProvider.id}
-          layout="vertical"
-        >
-          <Form.Item name="username" initialValue="admin">
-            <Select
-              options={[
-                {
-                  value: 'admin',
-                },
-                { value: 'johndoe' },
-              ]}
-            />
-          </Form.Item>
-          <Button htmlType="submit" style={{ marginBottom: verticalGap }}>
-            {developmentUsersProvider.name}
-          </Button>
-        </Form>
-      ),
-    });
-  }
-
-  if (emailProvider) {
-    tabs.push({
-      icon: <MdEmail size={26} />,
-      label: (
-        <span>
-          Sign In with <span style={{ whiteSpace: 'nowrap' }}>E-Mail</span>
-        </span>
-      ),
-      key: 'signin-email',
-      children: (
-        <>
-          <Form
-            onFinish={(values) =>
-              signIn(emailProvider.id, { ...values, callbackUrl: callbackUrlWithGuestRef })
-            }
-            key={emailProvider.id}
-            layout="vertical"
-            style={{ marginBottom: verticalGap }}
-          >
-            <Form.Item
-              name="email"
-              rules={[{ type: 'email', required: true }]}
-              style={{ marginBottom: verticalGap }}
-            >
-              <Input placeholder="E-Mail" />
-            </Form.Item>
-            <Button htmlType="submit">Continue with E-Mail</Button>
-          </Form>
-
-          <Alert
-            message="Note: Simply sign in with your e-mail address and we will send you an access link."
-            type="info"
-          />
-        </>
-      ),
-    });
-  }
-
-  if (passwordSigninProvider) {
-    tabs.push({
-      icon: <MdLogin size={26} />,
-      label: 'Sign In with Password',
-      key: 'signin-password',
-      children: (
-        <CredentialsSignIn
-          provider={passwordSigninProvider as any}
-          callbackUrl={callbackUrlWithGuestRef}
-        />
-      ),
-    });
-  }
-
-  if (passwordSignupProvider && env.PROCEED_PUBLIC_IAM_PERSONAL_SPACES_ACTIVE) {
-    tabs.push({
-      icon: <BsFillPersonPlusFill size={26} />,
-      label: 'Register as New User',
-      key: 'signup-password',
-      children: (
-        <CredentialsSignIn
-          provider={passwordSignupProvider as any}
-          callbackUrl={callbackUrlWithGuestRef}
-        />
-      ),
-    });
-  }
-
-  if (!env.PROCEED_PUBLIC_IAM_ONLY_ONE_ORGANIZATIONAL_SPACE) {
-    tabs.push({
-      icon: <GoOrganization size={26} />,
-      label: 'Create Organization',
-      key: 'create-organization',
-      href: '/create-organization',
-      children: null,
-    });
-  }
-
-  for (const provider of oauthProviders) {
-    tabs.push({
-      key: provider.id,
-      label: `Sign in with ${provider.name}`,
-      onClick: () => signIn(provider.id, { callbackUrl: callbackUrlWithGuestRef }),
-      icon: (
-        // eslint-disable-next-line
-        <img
-          src={`https://authjs.dev/img/providers/${provider.id}.svg`}
-          alt={provider.name}
-          style={{ width: '1.5rem', height: 'auto' }}
-        />
-      ),
-    });
-  }
-
-  const activeIndex = _activeIndex || tabs[0]?.key;
 
   return (
     <ConfigProvider
@@ -325,67 +136,7 @@ const SignIn: FC<{
         {/* If the user isn't none, we already show the signIn title at the top of the modal */}
         {signInTitle}
 
-        <Tabs
-          items={tabs}
-          renderTabBar={() => (
-            <>
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: breakpoint.xs ? 'column' : 'row',
-                  flexWrap: 'wrap',
-                  alignItems: 'stretch',
-                  justifyContent: 'stretch',
-                  gap: '1rem',
-                  width: '100%',
-                }}
-              >
-                {tabs.map((option, index) => {
-                  const Wrapper = option.href ? Link : Fragment;
-
-                  return (
-                    <Wrapper key={index} href={option.href as any} passHref legacyBehavior>
-                      <AntDesignButton
-                        style={{
-                          flex: breakpoint.xs ? '' : '1 1 0', // evenly fill container
-                          height: 'auto', // Allow for vertical stretching
-                          minHeight: 'min-content',
-                          padding: '.5rem',
-                          display: 'flex',
-                          flexDirection: 'column',
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                          overflow: 'hidden',
-                          whiteSpace: 'normal',
-                          wordBreak: 'keep-all',
-                          minWidth: '22%', // -> 4 per row
-                        }}
-                        color={option.key === activeIndex ? 'primary' : 'default'}
-                        variant="outlined"
-                        onClick={() => {
-                          if (option.onClick) option.onClick();
-                          else if (!option.href) setActiveIndex(option.key);
-                        }}
-                      >
-                        {option.icon}
-                        <Typography.Text
-                          style={{
-                            textAlign: 'center',
-                            fontSize: '0.75rem',
-                          }}
-                        >
-                          {option.label}
-                        </Typography.Text>
-                      </AntDesignButton>
-                    </Wrapper>
-                  );
-                })}
-              </div>
-              <Divider />
-            </>
-          )}
-          activeKey={activeIndex}
-        />
+        <SigninOptions providers={providers} callbackUrl={callbackUrlWithGuestRef} />
 
         {userType === 'guest' && guestProvider && (
           <>

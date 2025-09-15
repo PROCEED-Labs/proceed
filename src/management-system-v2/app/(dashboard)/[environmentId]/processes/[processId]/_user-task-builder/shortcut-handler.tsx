4e08547e29c2e6e257037a9bdc3a36e770fc2689
Janis Joderi Shoferi <janis.joderi@web.de>
2025-09-15 15:30:37 +0200

    WIP: Testing what causes rerender bug

--- src/management-system-v2/app/(dashboard)/[environmentId]/processes/[processId]/_user-task-builder/shortcut-handler.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/processes/[processId]/_user-task-builder/shortcut-handler.tsx
@@ -71,7 +71,7 @@ const ShortcutHandler: React.FC<ShortcutHandlerProps> = ({ onClose }) => {
     () => {
       if (canUndo) undo();
     },
-    { dependencies: [canUndo, undo] },
+    { dependencies: [undo] },
   );
 
   // useAddControlCallback(

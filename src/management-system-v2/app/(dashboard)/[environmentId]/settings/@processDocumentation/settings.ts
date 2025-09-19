105ed75512a373b77d644414271acfa9b69d85d2
Kai <70608052+iaktern@users.noreply.github.com>
2025-09-19 15:55:47 +0200

    Env Variables reordered in schema (#649)
    * Env Variables reordered in schema
    
    * fix(ms2): access PROCEED_PUBLIC_GANTT_ACTIVE over msconfig
    
    ---------
    
    Co-authored-by: Felipe Trost <felipe.trost@gmail.com>

--- src/management-system-v2/app/(dashboard)/[environmentId]/settings/@processDocumentation/settings.ts
+++ src/management-system-v2/app/(dashboard)/[environmentId]/settings/@processDocumentation/settings.ts
@@ -1,60 +1,63 @@
 import { SettingGroup } from '../type-util';
-import { env } from '@/lib/ms-config/env-vars';
 import { ganttViewSettingsDefinition } from '@/components/bpmn-timeline/gantt-settings-definition';
+import { getMSConfig } from '@/lib/ms-config/ms-config';
 
-const ganttViewSettings =
-  env.PROCEED_PUBLIC_GANTT_ACTIVE === true ? ganttViewSettingsDefinition : null;
+export async function getSettings(): Promise<SettingGroup> {
+  const msConfig = await getMSConfig();
+  const ganttViewSettings =
+    msConfig.PROCEED_PUBLIC_GANTT_ACTIVE === true ? ganttViewSettingsDefinition : null;
 
-export const settings: SettingGroup = {
-  key: 'process-documentation',
-  name: 'Process Documentation',
-  children: [
-    {
-      key: 'active',
-      name: 'Enabled',
-      type: 'boolean',
-      description: 'Controls whether this view is activated in this space.',
-      value: true,
-    },
-    {
-      key: 'list',
-      name: 'Process List',
-      children: [
-        {
-          key: 'active',
-          name: 'Enabled',
-          type: 'boolean',
-          description: 'Controls whether this view is activated in this space.',
-          value: true,
-        },
-      ],
-    },
-    {
-      key: 'editor',
-      name: 'Process Editor',
-      children: [
-        {
-          key: 'active',
-          name: 'Enabled',
-          type: 'boolean',
-          description: 'Controls whether this view is activated in this space.',
-          value: true,
-        },
-      ],
-    },
-    // {
-    //   key: 'templates',
-    //   name: 'Process Templates',
-    //   children: [
-    //     {
-    //       key: 'active',
-    //       name: 'Enabled',
-    //       type: 'boolean',
-    //       description: 'Controls whether this view is activated in this space.',
-    //       value: true,
-    //     },
-    //   ],
-    // },
-    ...(ganttViewSettings ? [ganttViewSettings] : []),
-  ],
-};
+  return {
+    key: 'process-documentation',
+    name: 'Process Documentation',
+    children: [
+      {
+        key: 'active',
+        name: 'Enabled',
+        type: 'boolean',
+        description: 'Controls whether this view is activated in this space.',
+        value: true,
+      },
+      {
+        key: 'list',
+        name: 'Process List',
+        children: [
+          {
+            key: 'active',
+            name: 'Enabled',
+            type: 'boolean',
+            description: 'Controls whether this view is activated in this space.',
+            value: true,
+          },
+        ],
+      },
+      {
+        key: 'editor',
+        name: 'Process Editor',
+        children: [
+          {
+            key: 'active',
+            name: 'Enabled',
+            type: 'boolean',
+            description: 'Controls whether this view is activated in this space.',
+            value: true,
+          },
+        ],
+      },
+      // {
+      //   key: 'templates',
+      //   name: 'Process Templates',
+      //   children: [
+      //     {
+      //       key: 'active',
+      //       name: 'Enabled',
+      //       type: 'boolean',
+      //       description: 'Controls whether this view is activated in this space.',
+      //       value: true,
+      //     },
+      //   ],
+      // },
+      ...(ganttViewSettings ? [ganttViewSettings] : []),
+    ],
+  };
+}

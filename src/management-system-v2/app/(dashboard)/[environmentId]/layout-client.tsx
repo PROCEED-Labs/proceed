6e271835b693ac8f1d49d8af5f92b6a219c01300
Janis Joderi Shoferi <janis.joderi@web.de>
2025-09-23 15:52:23 +0200

    Fixed styling

--- src/management-system-v2/app/(dashboard)/[environmentId]/layout-client.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/layout-client.tsx
@@ -77,261 +77,261 @@ const Layout: FC<
   bottomMenuItems,
   showTasklisSidebarEntry,
 }) => {
-    const session = useSession();
-    const userData = session?.data?.user;
-    const mobileDrawerOpen = useLayoutMobileDrawer((state) => state.open);
-    const setMobileDrawerOpen = useLayoutMobileDrawer((state) => state.set);
+  const session = useSession();
+  const userData = session?.data?.user;
+  const mobileDrawerOpen = useLayoutMobileDrawer((state) => state.open);
+  const setMobileDrawerOpen = useLayoutMobileDrawer((state) => state.set);
 
-    const modelerIsFullScreen = useModelerStateStore((state) => state.isFullScreen);
+  const modelerIsFullScreen = useModelerStateStore((state) => state.isFullScreen);
 
-    const [showLoginRequest, setShowLoginRequest] = useState(false);
-    const [collapsed, setCollapsed] = useState(false);
-    const breakpoint = Grid.useBreakpoint();
+  const [showLoginRequest, setShowLoginRequest] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+  const breakpoint = Grid.useBreakpoint();
 
-    const [userNeedsToChangePassword, setUserNeedsToChangePassword] = useState(
-      _userNeedsToChangePassword ?? false,
-    );
+  const [userNeedsToChangePassword, setUserNeedsToChangePassword] = useState(
+    _userNeedsToChangePassword ?? false,
+  );
 
-    let layoutMenuItems = _layoutMenuItems;
+  let layoutMenuItems = _layoutMenuItems;
 
-    const { data: automationSettings } = useQuery({
-      queryFn: async () => {
-        return wrapServerCall({
-          fn: async () => getSpaceSettingsValues(activeSpace.spaceId, 'process-automation'),
-          onSuccess: false,
-        });
+  const { data: automationSettings } = useQuery({
+    queryFn: async () => {
+      return wrapServerCall({
+        fn: async () => getSpaceSettingsValues(activeSpace.spaceId, 'process-automation'),
+        onSuccess: false,
+      });
+    },
+    queryKey: ['space-settings', activeSpace.spaceId, 'process-automation'],
+  });
+
+  const userTaskFilter = useMemo(
+    () => ({
+      allowedStates: ['READY', 'ACTIVE'],
+      hideUnassignedTasks: activeSpace.isOrganization,
+      hideNonOwnableTasks: true,
+    }),
+    [activeSpace.isOrganization],
+  );
+
+  const { userTasks } = useUserTasks(activeSpace, 2000, userTaskFilter);
+
+  if (showTasklisSidebarEntry) {
+    layoutMenuItems = [
+      {
+        key: 'tasklist',
+        label: (
+          <div style={{ display: 'flex', alignItems: 'baseline' }}>
+            <Link href={spaceURL(activeSpace, `/tasklist`)}>My Tasks</Link>
+            <Badge count={userTasks?.length} offset={[5, 0]}></Badge>
+          </div>
+        ),
+        icon: <CheckSquareOutlined />,
       },
-      queryKey: ['space-settings', activeSpace.spaceId, 'process-automation'],
-    });
+      ...layoutMenuItems,
+    ];
+  }
 
-    const userTaskFilter = useMemo(
-      () => ({
-        allowedStates: ['READY', 'ACTIVE'],
-        hideUnassignedTasks: activeSpace.isOrganization,
-        hideNonOwnableTasks: true,
-      }),
-      [activeSpace.isOrganization],
+  if (breakpoint.xs) {
+    layoutMenuItems = layoutMenuItems.filter(
+      (item) => !(item && 'type' in item && item.type === 'divider'),
     );
+  }
 
-    const { userTasks } = useUserTasks(activeSpace, 2000, userTaskFilter);
+  // The space id needs to be set here, because the hook is outside of the SpaceContext.Provider
+  const { imageSource } = useMSLogo(customLogo, { spaceId: activeSpace.spaceId });
 
-    if (showTasklisSidebarEntry) {
-      layoutMenuItems = [
-        {
-          key: 'tasklist',
-          label: (
-            <div style={{ display: 'flex', alignItems: 'baseline' }}>
-              <Link href={spaceURL(activeSpace, `/tasklist`)}>My Tasks</Link>
-              <Badge count={userTasks?.length} offset={[5, 0]}></Badge>
-            </div>
-          ),
-          icon: <CheckSquareOutlined />,
-        },
-        ...layoutMenuItems,
-      ];
-    }
+  const menu = (
+    <Menu
+      style={{ textAlign: collapsed && !breakpoint.xs ? 'center' : 'start' }}
+      mode="inline"
+      items={layoutMenuItems}
+      onClick={breakpoint.xs ? () => setMobileDrawerOpen(false) : undefined}
+    />
+  );
 
-    if (breakpoint.xs) {
-      layoutMenuItems = layoutMenuItems.filter(
-        (item) => !(item && 'type' in item && item.type === 'divider'),
-      );
-    }
-
-    // The space id needs to be set here, because the hook is outside of the SpaceContext.Provider
-    const { imageSource } = useMSLogo(customLogo, { spaceId: activeSpace.spaceId });
-
-    const menu = (
+  let bottomMenu;
+  if (bottomMenuItems && bottomMenuItems.length > 0) {
+    bottomMenu = (
       <Menu
         style={{ textAlign: collapsed && !breakpoint.xs ? 'center' : 'start' }}
         mode="inline"
-        items={layoutMenuItems}
+        items={bottomMenuItems}
         onClick={breakpoint.xs ? () => setMobileDrawerOpen(false) : undefined}
       />
     );
+  }
 
-    let bottomMenu;
-    if (bottomMenuItems && bottomMenuItems.length > 0) {
-      bottomMenu = (
-        <Menu
-          style={{ textAlign: collapsed && !breakpoint.xs ? 'center' : 'start' }}
-          mode="inline"
-          items={bottomMenuItems}
-          onClick={breakpoint.xs ? () => setMobileDrawerOpen(false) : undefined}
-        />
-      );
-    }
+  return (
+    <UserSpacesContext.Provider value={userEnvironments}>
+      <SpaceContext.Provider value={activeSpace}>
+        {!disableUserDataModal && userData && !userData.isGuest ? (
+          <AuthenticatedUserDataModal
+            modalOpen={!userData.username || !userData.lastName || !userData.firstName}
+            userData={userData}
+            close={() => {}}
+            structure={{
+              title: 'You need to complete your profile to continue',
+              password: false,
+              inputFields: [
+                {
+                  label: 'First Name',
+                  submitField: 'firstName',
+                  userDataField: 'firstName',
+                },
+                {
+                  label: 'Last Name',
+                  submitField: 'lastName',
+                  userDataField: 'lastName',
+                },
+                {
+                  label: 'Username',
+                  submitField: 'username',
+                  userDataField: 'username',
+                },
+              ],
+            }}
+            modalProps={{ closeIcon: null, destroyOnClose: true }}
+          />
+        ) : null}
 
-    return (
-      <UserSpacesContext.Provider value={userEnvironments}>
-        <SpaceContext.Provider value={activeSpace}>
-          {!disableUserDataModal && userData && !userData.isGuest ? (
-            <AuthenticatedUserDataModal
-              modalOpen={!userData.username || !userData.lastName || !userData.firstName}
-              userData={userData}
-              close={() => { }}
-              structure={{
-                title: 'You need to complete your profile to continue',
-                password: false,
-                inputFields: [
-                  {
-                    label: 'First Name',
-                    submitField: 'firstName',
-                    userDataField: 'firstName',
-                  },
-                  {
-                    label: 'Last Name',
-                    submitField: 'lastName',
-                    userDataField: 'lastName',
-                  },
-                  {
-                    label: 'Username',
-                    submitField: 'username',
-                    userDataField: 'username',
-                  },
-                ],
-              }}
-              modalProps={{ closeIcon: null, destroyOnClose: true }}
-            />
-          ) : null}
-
-          {userNeedsToChangePassword && (
-            <ChangeUserPasswordModal
-              open={true}
-              close={(passwordChanged) => {
-                if (passwordChanged) {
-                  setUserNeedsToChangePassword(false);
-                }
-              }}
-              title="You need to set your password"
-              hint={
-                <Alert message="Your account still has a temporary password, in order to use PROCEED you need to set a new password" />
+        {userNeedsToChangePassword && (
+          <ChangeUserPasswordModal
+            open={true}
+            close={(passwordChanged) => {
+              if (passwordChanged) {
+                setUserNeedsToChangePassword(false);
               }
-              modalProps={{ closable: false }}
-            />
-          )}
-
-          <AntLayout style={{ height: '100vh' }}>
-            <AntLayout hasSider>
-              {!hideSider && (
-                <AntLayout.Sider
-                  style={{
-                    backgroundColor: '#fff',
-                    borderRight: '1px solid #eee',
-                    display: modelerIsFullScreen ? 'none' : 'block',
-                    overflow: 'auto',
-                  }}
-                  className={cn(styles.Sider)}
-                  collapsible
-                  collapsed={collapsed}
-                  onCollapse={(collapsed) => setCollapsed(collapsed)}
-                  collapsedWidth={breakpoint.xs ? '0' : '75'}
-                  breakpoint="xl"
-                  theme="light"
-                >
-                  <div
-                    style={{
-                      height: '100%',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'space-between',
-                    }}
-                  >
-                    <div>
-                      <div
-                        style={{
-                          display: 'flex',
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                          padding: '8px',
-                          height: '64px',
-                        }}
-                      >
-                        <Link
-                          className={styles.LogoContainer}
-                          href={spaceURL(activeSpace, `/processes`)}
-                        >
-                          <Image
-                            src={imageSource}
-                            alt="PROCEED Logo"
-                            className={cn(styles.Logo, {
-                              [styles.collapsed]: collapsed,
-                            })}
-                            width={160}
-                            height={63}
-                            priority
-                          />
-                        </Link>
-                      </div>
-                      {loggedIn ? menu : null}
-                    </div>
-                    <div>
-                      {bottomMenu}
-                      <AntLayout.Footer
-                        style={{ display: modelerIsFullScreen ? 'none' : 'block' }}
-                        className={cn(styles.Footer)}
-                      >
-                        PROCEED Labs GmbH
-                      </AntLayout.Footer>
-                    </div>
-                  </div>
-                </AntLayout.Sider>
-              )}
-
-              <div className={cn(styles.Main, { [styles.collapsed]: false })}>{children}</div>
-            </AntLayout>
-          </AntLayout>
-
-          <Drawer
-            title={
-              loggedIn ? (
-                <Tooltip title="Account Settings">
-                  <UserAvatar user={userData} />
-                </Tooltip>
-              ) : (
-                <Button type="text" onClick={() => signIn()}>
-                  <u>Sign In</u>
-                </Button>
-              )
+            }}
+            title="You need to set your password"
+            hint={
+              <Alert message="Your account still has a temporary password, in order to use PROCEED you need to set a new password" />
             }
-            placement="right"
-            onClose={() => setMobileDrawerOpen(false)}
-            open={mobileDrawerOpen}
-          >
-            <div
-              style={{
-                height: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-              }}
-            >
-              {menu}
-              {bottomMenu}
-            </div>
-          </Drawer>
+            modalProps={{ closable: false }}
+          />
+        )}
 
-          <Modal
-            title={null}
-            footer={null}
-            closable={false}
-            open={showLoginRequest}
-            onCancel={() => setShowLoginRequest(false)}
-            styles={{ mask: { backdropFilter: 'blur(10px)' }, content: { padding: 0 } }}
+        <AntLayout style={{ height: '100vh' }}>
+          <AntLayout hasSider>
+            {!hideSider && (
+              <AntLayout.Sider
+                style={{
+                  backgroundColor: '#fff',
+                  borderRight: '1px solid #eee',
+                  display: modelerIsFullScreen ? 'none' : 'block',
+                  overflow: 'auto',
+                }}
+                className={cn(styles.Sider)}
+                collapsible
+                collapsed={collapsed}
+                onCollapse={(collapsed) => setCollapsed(collapsed)}
+                collapsedWidth={breakpoint.xs ? '0' : '75'}
+                breakpoint="xl"
+                theme="light"
+              >
+                <div
+                  style={{
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '8px',
+                        height: '64px',
+                      }}
+                    >
+                      <Link
+                        className={styles.LogoContainer}
+                        href={spaceURL(activeSpace, `/processes`)}
+                      >
+                        <Image
+                          src={imageSource}
+                          alt="PROCEED Logo"
+                          className={cn(styles.Logo, {
+                            [styles.collapsed]: collapsed,
+                          })}
+                          width={160}
+                          height={63}
+                          priority
+                        />
+                      </Link>
+                    </div>
+                    {loggedIn ? menu : null}
+                  </div>
+                  <div>
+                    {bottomMenu}
+                    <AntLayout.Footer
+                      style={{ display: modelerIsFullScreen ? 'none' : 'block' }}
+                      className={cn(styles.Footer)}
+                    >
+                      PROCEED Labs GmbH
+                    </AntLayout.Footer>
+                  </div>
+                </div>
+              </AntLayout.Sider>
+            )}
+
+            <div className={cn(styles.Main, { [styles.collapsed]: false })}>{children}</div>
+          </AntLayout>
+        </AntLayout>
+
+        <Drawer
+          title={
+            loggedIn ? (
+              <Tooltip title="Account Settings">
+                <UserAvatar user={userData} />
+              </Tooltip>
+            ) : (
+              <Button type="text" onClick={() => signIn()}>
+                <u>Sign In</u>
+              </Button>
+            )
+          }
+          placement="right"
+          onClose={() => setMobileDrawerOpen(false)}
+          open={mobileDrawerOpen}
+        >
+          <div
+            style={{
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+            }}
           >
-            <Alert
-              type="warning"
-              style={{ zIndex: '1000' }}
-              message={
-                <>
-                  To store and change settings,{' '}
-                  <SpaceLink href={'/signin'}>please log in as user.</SpaceLink>
-                </>
-              }
-            />
-          </Modal>
-        </SpaceContext.Provider>
-      </UserSpacesContext.Provider>
-    );
-  };
+            {menu}
+            {bottomMenu}
+          </div>
+        </Drawer>
+
+        <Modal
+          title={null}
+          footer={null}
+          closable={false}
+          open={showLoginRequest}
+          onCancel={() => setShowLoginRequest(false)}
+          styles={{ mask: { backdropFilter: 'blur(10px)' }, content: { padding: 0 } }}
+        >
+          <Alert
+            type="warning"
+            style={{ zIndex: '1000' }}
+            message={
+              <>
+                To store and change settings,{' '}
+                <SpaceLink href={'/signin'}>please log in as user.</SpaceLink>
+              </>
+            }
+          />
+        </Modal>
+      </SpaceContext.Provider>
+    </UserSpacesContext.Provider>
+  );
+};
 
 export default Layout;

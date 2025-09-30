a66ffdb26e73d252a26f5a7d0bc2fd3db0cf3a61
Felipe Trost H. <felipe.trost@gmail.com>
2025-09-30 13:40:04 +0200

    ms2/invite with username (#626)
    * feat(ms2/profile): change password
    
    * feat(ms2/db): flag for temporary passwords
    
    * feat(ms2): show hint when user has a temporary password
    
    * feat(ms2/org-user-dashboard): create users and add them to organizations
    
    * feat(ms2/create-user): select roles for added user
    
    * refactor(ms2/favorite-star): removed unnecessary server call
    
    * feat(ms2): invite-by-username
    
    * fix(ms2/invite-users): take PROCEED_PUBLIC_MAILSERVER_ACTIVE into consideration
    
    * format
    
    * removed test timeout
    
    ---------
    
    Co-authored-by: Kai Rohwer <rohwer.kai@icloud.com>

--- src/management-system-v2/lib/invitation-tokens.ts
+++ src/management-system-v2/lib/invitation-tokens.ts
@@ -1,6 +1,13 @@
+import 'server-only';
+
 import { z } from 'zod';
 import jwt from 'jsonwebtoken';
 import { env } from './ms-config/env-vars';
+import { getEnvironmentById } from './data/db/iam/environments';
+import { addMember, isMember } from './data/db/iam/memberships';
+import { getUserByEmail } from './data/db/iam/users';
+import { getRoleById } from './data/db/iam/roles';
+import { addRoleMappings } from './data/db/iam/role-mappings';
 
 const baseInvitationSchema = {
   spaceId: z.string(),
@@ -11,7 +18,7 @@ const invitationSchema = z.union([
   z.object({ email: z.string().email() }).extend(baseInvitationSchema),
 ]);
 
-type Invitation = z.infer<typeof invitationSchema>;
+export type Invitation = z.infer<typeof invitationSchema>;
 
 export function generateInvitationToken(invitation: Invitation, expiration: Date) {
   // in seconds
@@ -29,5 +36,38 @@ export function getInvitation(token: string) {
       return { error: 'TokenExpiredError' as const };
     }
     return { error: 'error' as const };
+  }
+}
+
+export async function acceptInvitation(invite: Invitation, userIdAcceptingInvite?: string) {
+  const organization = await getEnvironmentById(invite.spaceId);
+  if (!organization || !organization.isOrganization || !organization.isActive)
+    return { error: 'InvalidOrganization' as const };
+
+  const userId = 'userId' in invite ? invite.userId : (await getUserByEmail(invite.email))?.id;
+
+  if (!userId) return { error: 'UserNotFound' as const };
+
+  if (userIdAcceptingInvite && userIdAcceptingInvite !== userId)
+    return { error: 'WrongUser' as const };
+
+  if (!(await isMember(invite.spaceId, userId))) {
+    addMember(invite.spaceId, userId);
+
+    if (invite.roleIds) {
+      const validRoles = [];
+      for (const roleId of invite.roleIds) {
+        // skip roles that have been deleted
+        if (await getRoleById(roleId)) validRoles.push(roleId);
+      }
+
+      await addRoleMappings(
+        validRoles.map((roleId) => ({
+          environmentId: invite.spaceId,
+          roleId,
+          userId,
+        })),
+      );
+    }
   }
 }

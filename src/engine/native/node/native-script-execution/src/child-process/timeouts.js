81453ce45d15d228a32a76053479edfe01073cab
Felipe Trost <felipe.trost@gmail.com>
2025-10-02 17:52:50 +0200

    feat(node-native/script-execution): setTimeout & setInterval

--- src/engine/native/node/native-script-execution/src/child-process/timeouts.js
+++ src/engine/native/node/native-script-execution/src/child-process/timeouts.js
@@ -1,6 +1,10 @@
 // @ts-check
 
-const ivm = require('isolated-vm');
+// It could be possible to implement this a bit easier, by creating a reference of the callback
+// function (inside the isolate) and passing it to the main node program, however that would require
+// us to expose ivm in the isolate, and I'm not so sure how safe that is.
+
+// TODO: consider what to do with errors thrown inside the callback (cb) functions
 
 /**
  * @param {{
@@ -12,24 +16,74 @@ const ivm = require('isolated-vm');
  * }} data
  * */
 module.exports = function setupTimeouts({ context }) {
-  const sleep = new ivm.Reference((ms) => new Promise((res) => setTimeout(res, ms)));
+  // Timoeouts
+
+  context.evalSync(`
+    let _timeout_counter = 0;
+    const _active_timeouts = new Set();
+  `);
 
   context.evalClosureSync(
-    async function setTimeoutAsync(cb, ms) {
+    async function _timeoutExecutor(cb, ms, timeoutId) {
       // @ts-expect-error $0 will be defined when running in the isolate
-      await $0.apply(null, [ms], { result: { promise: true } });
-      cb();
-    }.toString() + `globalThis["setTimeoutAsync"] = setTimeoutAsync;`,
-    [sleep],
+      await waitAsync(ms);
+
+      if (!_active_timeouts.has(timeoutId)) return;
+
+      try {
+        cb();
+      } catch (e) {}
+
+      _active_timeouts.delete(timeoutId);
+    }.toString() + `globalThis["_timeoutExecutor"] = _timeoutExecutor;`,
   );
 
   context.evalClosureSync(
-    async function setIntervalAsync(cb, ms) {
+    function setTimeout(cb, ms) {
+      const timeoutId = _timeout_counter++;
+      _active_timeouts.add(timeoutId);
+      _timeoutExecutor(cb, ms, timeoutId);
+      return timeoutId;
+    }.toString() + `globalThis["setTimeout"] = setTimeout;`,
+  );
+
+  context.evalClosureSync(
+    function clearTimeout(timeoutId) {
+      return _active_timeouts.delete(timeoutId);
+    }.toString() + `globalThis["clearTimeout"] = clearTimeout;`,
+  );
+
+  // Intervals
+
+  context.evalSync(`
+    let _interval_counter = 0;
+    const _activeIntervals = new Set();
+  `);
+
+  context.evalClosureSync(
+    async function _intervalExecutor(cb, ms, intervalId) {
       do {
-        // @ts-expect-error $0 will be defined when running in the isolate
-        await $0.apply(null, [ms], { result: { promise: true } });
-      } while (!(await cb()));
-    }.toString() + 'globalThis["setIntervalAsync"]=setIntervalAsync;',
-    [sleep],
+        await waitAsync(ms);
+        // NOTE: maybe this shouldn't be awaited
+        try {
+          await cb();
+        } catch (e) {}
+      } while (_active_Intervals.has(intervalId));
+    }.toString() + 'globalThis["_intervalExecutor"]=_intervalExecutor;',
+  );
+
+  context.evalClosureSync(
+    function setInterval(cb, ms) {
+      const intervalId = _interval_counter++;
+      _activeIntervals.add(intervalId);
+      _intervalExecutor(cb, ms, intervalId);
+      return intervalId;
+    }.toString() + 'globalThis["setInterval"]=setInterval;',
+  );
+
+  context.evalClosureSync(
+    function clearInterval(intervalId) {
+      return _activeIntervals.delete(intervalId);
+    }.toString() + 'globalThis["clearInterval"]=clearInterval;',
   );
 };

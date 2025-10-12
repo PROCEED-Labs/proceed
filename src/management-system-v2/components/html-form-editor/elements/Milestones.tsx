90f93b999adb6c6695ac9f0a65e1a99c594ac49d
Janis Joderi Shoferi <janis.joderi@web.de>
2025-10-12 15:23:39 +0200

    Fixed: Milestones in html forms are using the outdated placeholder format

--- src/management-system-v2/components/html-form-editor/elements/Milestones.tsx
+++ src/management-system-v2/components/html-form-editor/elements/Milestones.tsx
@@ -5,23 +5,23 @@ export const ExportMilestones: UserComponent = () => {
   return (
     <div className="user-task-form-milestones">
       <p>Update your Milestones:</p>
-      {'{for milestone in milestones}'}
+      {'{{for milestone in milestones}}'}
       <div className="user-task-form-milestone">
         <label>
-          Milestone ID: {'{milestone.id}'} | Name: {'{milestone.name}'} | Description:{' '}
-          {'{milestone.description}'}
+          Milestone ID: {'{{milestone.id}}'} | Name: {'{{milestone.name}}'} | Description:{' '}
+          {'{{milestone.description}}'}
           <input
             type="range"
             min="0"
             max="100"
-            value={'{milestone.value}'}
-            className={`milestone-{milestone.id}`}
+            value={'{{milestone.value}}'}
+            className={`milestone-{{milestone.id}}`}
             onChange={() => {}}
           />
-          <output name={`fulfillment_{milestone.id}`}>{'{milestone.value}'}%</output>
+          <output name={`fulfillment_{{milestone.id}}`}>{'{{milestone.value}}'}%</output>
         </label>
       </div>
-      {'{/for}'}
+      {'{{/for}}'}
     </div>
   );
 };

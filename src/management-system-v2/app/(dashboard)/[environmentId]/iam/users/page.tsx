114b62732e9c62a0c073cae4c6f820e26781c55a
Felipe Trost <felipe.trost@gmail.com>
2025-12-09 02:16:05 +0100

    fix

--- src/management-system-v2/app/(dashboard)/[environmentId]/iam/users/page.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/iam/users/page.tsx
@@ -2,12 +2,8 @@ import { getCurrentEnvironment } from '@/components/auth';
 import UsersPage from './users-page';
 import Content from '@/components/content';
 import UnauthorizedFallback from '@/components/unauthorized-fallback';
-import { getMembers } from '@/lib/data/db/iam/memberships';
-import { getUserById } from '@/lib/data/db/iam/users';
-import { AuthenticatedUser } from '@/lib/data/user-schema';
-import { asyncMap } from '@/lib/helpers/javascriptHelpers';
+import { getFullMembersWithRoles } from '@/lib/data/db/iam/memberships';
 import { errorResponse } from '@/lib/server-error-handling/page-error-response';
-import { Result } from 'neverthrow';
 
 const Page = async ({ params }: { params: { environmentId: string } }) => {
   const currentSpace = await getCurrentEnvironment(params.environmentId);
@@ -17,21 +13,12 @@ const Page = async ({ params }: { params: { environmentId: string } }) => {
   const { ability, activeEnvironment } = currentSpace.value;
   if (!ability.can('manage', 'User')) return <UnauthorizedFallback />;
 
-  const memberships = await getMembers(activeEnvironment.spaceId, ability);
-  if (memberships.isErr()) {
-    return errorResponse(memberships);
-  }
-
-  const users = Result.combine(
-    await asyncMap(memberships.value, (user) => getUserById(user.userId)),
-  );
-  if (users.isErr()) {
-    return errorResponse(users);
-  }
+  const users = await getFullMembersWithRoles(activeEnvironment.spaceId, ability);
+  if (users.isErr()) return errorResponse(users);
 
   return (
     <Content title="Identity and Access Management">
-      <UsersPage users={users.value as AuthenticatedUser[]} />
+      <UsersPage users={users.value} />
     </Content>
   );
 };

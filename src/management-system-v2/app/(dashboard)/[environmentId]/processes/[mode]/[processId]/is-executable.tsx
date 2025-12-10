412e1314694b48a51bb22bcd67efbdd39ffd6d6d
Janis Joderi Shoferi <janis.joderi@web.de>
2025-12-10 11:46:46 +0100

    Added error handling and loading to the isExecutable toggle

--- src/management-system-v2/app/(dashboard)/[environmentId]/processes/[mode]/[processId]/is-executable.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/processes/[mode]/[processId]/is-executable.tsx
@@ -1,12 +1,11 @@
 import { BPMNCanvasRef } from '@/components/bpmn-canvas';
-import { Checkbox, Divider, Space } from 'antd';
-import { useEffect, useState } from 'react';
-import { is, isAny } from 'bpmn-js/lib/util/ModelUtil';
-import type { Element } from 'bpmn-js/lib/model/Types';
+import { Checkbox, Divider, Space, Spin } from 'antd';
 import { updateProcessMetaData } from '@/lib/data/processes';
-import { useParams, useRouter } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { useEnvironment } from '@/components/auth-can';
 import useModelerStateStore from './use-modeler-state-store';
+import { wrapServerCall } from '@/lib/wrap-server-call';
+import { useState } from 'react';
 
 type IsExecutableSectionProps = {
   modeler: BPMNCanvasRef | null;
@@ -15,16 +14,24 @@ type IsExecutableSectionProps = {
 
 const IsExecutableSection: React.FC<IsExecutableSectionProps> = ({ modeler, readOnly }) => {
   const isExecutable = useModelerStateStore((state) => state.isExecutable);
+  const setIsExecutable = useModelerStateStore((state) => state.setIsExecutable);
+
+  const [loading, setLoading] = useState(false);
 
   const { processId } = useParams();
   const { spaceId } = useEnvironment();
-  const router = useRouter();
 
   const changeIsExecutable = async (value: boolean) => {
     if (modeler) {
       // update the executable value in the database
-      await updateProcessMetaData(processId as string, spaceId, { executable: value });
-      router.refresh();
+      setLoading(true);
+      await wrapServerCall({
+        fn: async () => updateProcessMetaData(processId as string, spaceId, { executable: value }),
+        onSuccess: false,
+        onError: 'Failed to update the executable property.',
+      });
+      setLoading(false);
+      setIsExecutable(value);
     }
   };
 
@@ -33,14 +40,19 @@ const IsExecutableSection: React.FC<IsExecutableSectionProps> = ({ modeler, read
       <Divider style={{ display: 'flex', alignItems: 'center', fontSize: '0.85rem' }}>
         <span style={{ marginRight: '0.3em', marginBottom: '0.1rem' }}>Executable</span>
       </Divider>
-      <Checkbox
-        disabled={readOnly}
-        title="Toggles if this process is considered to be executable and therefore deployable to the engine."
-        checked={isExecutable}
-        onChange={(e) => changeIsExecutable(e.target.checked)}
-      >
+      <Space>
+        {loading ? (
+          <Spin style={{ width: '16px' }} size="small" />
+        ) : (
+          <Checkbox
+            disabled={readOnly}
+            title="Toggles if this process is considered to be executable and therefore deployable to the engine."
+            checked={isExecutable}
+            onChange={(e) => changeIsExecutable(e.target.checked)}
+          />
+        )}
         Activate Automation for the entire Process
-      </Checkbox>
+      </Space>
     </Space>
   );
 };

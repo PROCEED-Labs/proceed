9dedacfbb288dc9ddbc3b06b775f8fdd6083a272
Janis Joderi Shoferi <janis.joderi@web.de>
2026-01-30 19:19:06 +0100

    Catch an error with accesing the environment in the layout of the setting page instead of the specific setting to avoid rendering the sidebar overview

--- src/management-system-v2/app/(dashboard)/[environmentId]/settings/layout.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/settings/layout.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 import Content from '@/components/content';
 import SettingsPage from './settings-page';
+import { getCurrentEnvironment } from '@/components/auth';
+import { errorResponse } from '@/lib/server-error-handling/page-error-response';
 
-export default function Layout({
+export default async function Layout({
   params,
   ...children
 }: {
   params: Promise<{ environmentId: string }>;
 }) {
   // TODO: check if the user has the rights to change the settings
+  const currentSpace = await getCurrentEnvironment((await params).environmentId);
+  if (currentSpace.isErr()) {
+    return errorResponse(currentSpace);
+  }
 
   return (
     <Content title="Settings">

ab85c6ea6baa7d789f734d3a2cef2c1bb6448e7d
Janis Joderi Shoferi <janis.joderi@web.de>
2026-01-30 19:48:00 +0100

    Fixed: getting the html for a local task leads to an error

--- src/management-system-v2/lib/engines/server-actions.ts
+++ src/management-system-v2/lib/engines/server-actions.ts
@@ -274,7 +274,6 @@ export async function getTasklistEntryHTML(spaceId: string, userTaskId: string, 
       state: storedState,
     } = storedUserTask;
     const [taskId, instanceId, startTimeString] = userTaskId.split('|');
-    const [definitionId] = instanceId.split('-_');
 
     if (!html || !milestones || !initialVariables) {
       const startTime = parseInt(startTimeString);
@@ -361,7 +360,8 @@ export async function getTasklistEntryHTML(spaceId: string, userTaskId: string, 
 
     // maps relative urls used to get resources on the engine to the MS api to allow them to work here as well
     function mapResourceUrls(variables: Record<string, any>) {
-      if (!variables) return variables;
+      if (!instanceId || !variables) return variables;
+      const definitionId = instanceId.split('-_')[0];
 
       return Object.fromEntries(
         Object.entries(variables).map(([key, value]) => {

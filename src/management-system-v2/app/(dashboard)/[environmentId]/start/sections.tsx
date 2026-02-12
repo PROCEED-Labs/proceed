a8959ab96bd65c7dab6957ab0c0a2393de390af3
Janis Joderi Shoferi <janis.joderi@web.de>
2026-02-12 19:31:08 +0100

    Consolidated some more code

--- src/management-system-v2/app/(dashboard)/[environmentId]/start/sections.tsx
+++ src/management-system-v2/app/(dashboard)/[environmentId]/start/sections.tsx
@@ -17,6 +17,39 @@ import { UserOutlined } from '@ant-design/icons';
 import { HomeOutlined, SettingOutlined } from '@ant-design/icons';
 
 import styles from './sections.module.scss';
+import { ReactNode } from 'react';
+
+type SectionProps = {
+  icon: ReactNode;
+  title: string;
+  description: ReactNode;
+  tiles: { title: string; href: string; icon: ReactNode; condition?: boolean }[];
+};
+
+const Section: React.FC<SectionProps> = ({ icon, title, description, tiles }) => {
+  return (
+    <div key={title} className={styles.Section}>
+      <h2>
+        {icon}
+        {title}
+      </h2>
+      <p>{description}</p>
+      <div className={styles.TileGroup}>
+        {tiles.map(
+          (tile) =>
+            (tile.condition === undefined || tile.condition) && (
+              <Link key={tile.title} href={tile.href} className={styles.Tile}>
+                <Card hoverable className={styles.TileCard}>
+                  {tile.icon}
+                  <h3 className={styles.TileCardTitle}>{tile.title}</h3>
+                </Card>
+              </Link>
+            ),
+        )}
+      </div>
+    </div>
+  );
+};
 
 type MyTasksSectionProps = {
   showTaskEditor: boolean;
@@ -31,7 +64,6 @@ const MyTasksSection: React.FC<MyTasksSectionProps> = ({ showTaskEditor }) => {
       title: 'Task List',
       href: `/${environmentId}/tasklist`,
       icon: <CheckSquareOutlined className={styles.TileIcon} />,
-      condition: true,
     },
     {
       title: 'Task Editor',
@@ -42,29 +74,17 @@ const MyTasksSection: React.FC<MyTasksSectionProps> = ({ showTaskEditor }) => {
   ];
 
   return (
-    <div className={styles.Section}>
-      <h2>
-        <CheckSquareOutlined className={styles.SectionIcon} />
-        My Tasks
-      </h2>
-      <p>
-        Manage your running or planned tasks in the Task List.<br></br>
-        Edit your existing tasks using the Task Editor.
-      </p>
-      <div className={styles.TileGroup}>
-        {tiles.map(
-          (tile) =>
-            tile.condition && (
-              <Link key={tile.title} href={tile.href} className={styles.Tile}>
-                <Card hoverable className={styles.TileCard}>
-                  {tile.icon}
-                  <h3 className={styles.TileCardTitle}>{tile.title}</h3>
-                </Card>
-              </Link>
-            ),
-        )}
-      </div>
-    </div>
+    <Section
+      title="My Tasks"
+      description={
+        <p>
+          Manage your running or planned tasks in the Task List.<br></br>
+          Edit your existing tasks using the Task Editor.
+        </p>
+      }
+      icon={<CheckSquareOutlined className={styles.SectionIcon} />}
+      tiles={tiles}
+    />
   );
 };
 
@@ -93,29 +113,17 @@ const ProcessesSection: React.FC<ProcessSectionProps> = ({ showList, showEditor 
   ];
 
   return (
-    <div className={styles.Section}>
-      <h2>
-        <PartitionOutlined className={styles.SectionIcon} />
-        My Processes
-      </h2>
-      <p>
-        Create, edit and organize your BPMN processes with the Process Editor. <br></br>
-        Browse already versioned, released processes in the Process List
-      </p>
-      <div className={styles.TileGroup}>
-        {tiles.map(
-          (tile) =>
-            tile.condition && (
-              <Link key={tile.title} href={tile.href} className={styles.Tile}>
-                <Card hoverable className={styles.TileCard}>
-                  {tile.icon}
-                  <h3 className={styles.TileCardTitle}>{tile.title}</h3>
-                </Card>
-              </Link>
-            ),
-        )}
-      </div>
-    </div>
+    <Section
+      title="My Processes"
+      description={
+        <p>
+          Create, edit and organize your BPMN processes with the Process Editor. <br></br>
+          Browse already versioned, released processes in the Process List
+        </p>
+      }
+      icon={<PartitionOutlined className={styles.SectionIcon} />}
+      tiles={tiles}
+    />
   );
 };
 
@@ -155,29 +163,17 @@ const AutomationsSection: React.FC<AutomationSectionProps> = ({
   ];
 
   return (
-    <div className={styles.Section}>
-      <h2>
-        <PlaySquareOutlined className={styles.SectionIcon} />
-        Automations
-      </h2>
-      <p>
-        Observe your running automations on the Dashboard, deploy your automation processes
-        <br></br>in the Executions tab, and manage your connected engines in Process Engines.
-      </p>
-      <div className={styles.TileGroup}>
-        {tiles.map(
-          (tile) =>
-            tile.condition && (
-              <Link key={tile.title} href={tile.href} className={styles.Tile}>
-                <Card hoverable className={styles.TileCard}>
-                  {tile.icon}
-                  <h3 className={styles.TileCardTitle}>{tile.title}</h3>
-                </Card>
-              </Link>
-            ),
-        )}
-      </div>
-    </div>
+    <Section
+      title="Automations"
+      description={
+        <p>
+          Observe your running automations on the Dashboard, deploy your automation processes
+          <br></br>in the Executions tab, and manage your connected engines in Process Engines.
+        </p>
+      }
+      icon={<PlaySquareOutlined className={styles.SectionIcon} />}
+      tiles={tiles}
+    />
   );
 };
 
@@ -199,26 +195,17 @@ const PersonalSection = () => {
   ];
 
   return (
-    <div className={styles.Section}>
-      <h2>
-        <UserOutlined className={styles.SectionIcon} />
-        Personal
-      </h2>
-      <p>
-        Change your user profile information or<br></br>
-        manage the different personal spaces you created.
-      </p>
-      <div className={styles.TileGroup}>
-        {tiles.map((tile) => (
-          <Link key={tile.title} href={tile.href} className={styles.Tile}>
-            <Card hoverable className={styles.TileCard}>
-              {tile.icon}
-              <h3 className={styles.TileCardTitle}>{tile.title}</h3>
-            </Card>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <Section
+      title="Personal"
+      description={
+        <p>
+          Change your user profile information or<br></br>
+          manage the different personal spaces you created.
+        </p>
+      }
+      icon={<UserOutlined className={styles.SectionIcon} />}
+      tiles={tiles}
+    />
   );
 };
 
@@ -235,26 +222,17 @@ const HomeSection = () => {
   ];
 
   return (
-    <div className={styles.Section}>
-      <h2>
-        <HomeOutlined className={styles.SectionIcon} />
-        Home
-      </h2>
-      <p>
-        Adjust the visual settings of your space <br></br>
-        and create custom navigation links for easier access.
-      </p>
-      <div className={styles.TileGroup}>
-        {tiles.map((tile) => (
-          <Link key={tile.title} href={tile.href} className={styles.Tile}>
-            <Card hoverable className={styles.TileCard}>
-              {tile.icon}
-              <h3 className={styles.TileCardTitle}>{tile.title}</h3>
-            </Card>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <Section
+      title="Home"
+      description={
+        <p>
+          Adjust the visual settings of your space <br></br>
+          and create custom navigation links for easier access.
+        </p>
+      }
+      icon={<HomeOutlined className={styles.SectionIcon} />}
+      tiles={tiles}
+    />
   );
 };
 

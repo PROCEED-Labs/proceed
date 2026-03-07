9d301ad8e280de6567b3a744e55a2c09719a0f55
Can-Philipp Tura <c.tura@campus.tu-berlin.de>
2026-03-07 19:53:51 +0100

    PUT added, parsing detects non-string input, sets transformationType to manual on change

--- src/management-system-v2/app/api/spaces/[spaceId]/data/organization/[dataObjectId]/route.ts
+++ src/management-system-v2/app/api/spaces/[spaceId]/data/organization/[dataObjectId]/route.ts
@@ -7,28 +7,83 @@ import {
   nestedParametersFromStorage,
   getParameterParent,
   getDeepConfigurationById,
+  updateParameter,
 } from '@/lib/data/db/machine-config';
+import { Parameter } from '@/lib/data/machine-config-schema';
 import { env } from '@/lib/ms-config/env-vars';
 import { NextRequest, NextResponse } from 'next/server';
 import { validate as uuidValidate } from 'uuid';
+import { z } from 'zod';
 
 export async function GET(
   request: NextRequest,
   props: { params: Promise<{ spaceId: string; dataObjectId: string }> },
 ) {
-  const params = await props.params;
+  // Answer - Success: 200 OK - Returns the full, requested Data Object
+  // Answer - Error: 404 Not found - Specified Space or Data Object not found
 
-  const { spaceId, dataObjectId } = params;
-  const parentConfig = await getDeepConfigurationById(spaceId);
-  let parameter;
-  if (parentConfig.configType !== 'organization')
-    return NextResponse.json('This ID does not correspond to a space.', { status: 404 });
-  if (uuidValidate(dataObjectId)) {
-    parameter = findParameter(dataObjectId, parentConfig, 'config')?.selection;
-  } else {
-    const path = dataObjectId.toLocaleLowerCase().split('.');
-    parameter = extractParameter(parentConfig, path);
+  const { spaceId, dataObjectId } = await props.params;
+  const fetchedData = await getData(spaceId, dataObjectId);
+  if (fetchedData.isResponse) return fetchedData.data;
+  else if (fetchedData.data) return NextResponse.json(fetchedData.data);
+  else return NextResponse.json(`Parameter ${dataObjectId} not found.`, { status: 404 });
+}
+
+export async function PUT(
+  request: NextRequest,
+  props: { params: Promise<{ spaceId: string; dataObjectId: string }> },
+) {
+  // Answer - Success: 200 OK - Data Object updated
+  // Answer - Error: 404 Invalid input - Specified Space or Data Object not found
+  // Answer - Error: 409 Invalid input. Body contains the reason. For example, syntax invalid, etc.
+
+  const { spaceId, dataObjectId } = await props.params;
+  try {
+    const rawBody = await request.text();
+    const schema = z
+      .string()
+      .refine((s) => !/[^\x20-\x7E\t\n\r]/.test(s), { message: 'Binary data detected' });
+    const body = schema.parse(rawBody);
+    const fetchedData = await getData(spaceId, dataObjectId);
+    if (fetchedData.isResponse) return fetchedData.data;
+    else if (fetchedData.data) {
+      const parameter = fetchedData.data as Parameter;
+      const parameterChanges = {
+        value: body,
+        ...(parameter.transformation?.transformationType === 'linked' && {
+          transformation: { ...parameter.transformation, transformationType: 'manual' as const },
+        }),
+      };
+      await updateParameter(parameter.id, parameterChanges, spaceId);
+      return NextResponse.json('Success.');
+    } else {
+      return NextResponse.json(`Parameter ${dataObjectId} not found.`, { status: 404 });
+    }
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 409 });
   }
-  if (parameter) return NextResponse.json(parameter);
-  else return NextResponse.json('This ID does not correspond to a space.', { status: 404 });
+}
+
+async function getData(spaceId: string, dataObjectId: string) {
+  try {
+    const parentConfig = await getDeepConfigurationById(spaceId);
+    let parameter;
+    if (parentConfig.configType !== 'organization')
+      return {
+        isResponse: true,
+        data: NextResponse.json('This ID does not correspond to a space.', { status: 404 }),
+      };
+    if (uuidValidate(dataObjectId)) {
+      parameter = findParameter(dataObjectId, parentConfig, 'config')?.selection;
+    } else {
+      const path = dataObjectId.toLocaleLowerCase().split('.');
+      parameter = extractParameter(parentConfig, path);
+    }
+    return { isResponse: false, data: parameter };
+  } catch {
+    return {
+      isResponse: true,
+      data: NextResponse.json(`No config found for spaceId ${spaceId}.`, { status: 404 }),
+    };
+  }
 }

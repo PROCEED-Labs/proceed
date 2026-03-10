41668f4e778d72ceb5e1a5e218ea168203032e4d
Can-Philipp Tura <c.tura@campus.tu-berlin.de>
2026-03-10 16:24:33 +0100

    helper functions updated, joined, and move to helper-file

--- src/management-system-v2/app/api/spaces/[spaceId]/data/organization/[dataObjectId]/route.ts
+++ src/management-system-v2/app/api/spaces/[spaceId]/data/organization/[dataObjectId]/route.ts
@@ -1,12 +1,10 @@
-import {
-  extractParameter,
-  findParameter,
-} from '@/app/(dashboard)/[environmentId]/machine-config/configuration-helper';
-import { getDeepConfigurationById, updateParameter } from '@/lib/data/db/machine-config';
-import { Parameter } from '@/lib/data/machine-config-schema';
+import { updateParameter } from '@/lib/data/db/machine-config';
 import { NextRequest, NextResponse } from 'next/server';
-import { validate as uuidValidate } from 'uuid';
-import { z } from 'zod';
+import {
+  getDataObject,
+  isErrorResponse,
+  parseAndGetChanges,
+} from '@/app/api/spaces/[spaceId]/data/helper';
 
 export async function GET(
   request: NextRequest,
@@ -17,9 +15,8 @@ export async function GET(
 
   const { spaceId, dataObjectId } = await props.params;
   const fetchedData = await getDataObject(spaceId, dataObjectId);
-  if (fetchedData.isResponse) return fetchedData.data;
-  else if (fetchedData.data) return NextResponse.json(fetchedData.data);
-  else return NextResponse.json(`Parameter ${dataObjectId} not found.`, { status: 404 });
+  if (isErrorResponse(fetchedData)) return fetchedData.data;
+  else return NextResponse.json(fetchedData.data);
 }
 
 export async function PUT(
@@ -32,70 +29,12 @@ export async function PUT(
 
   const { spaceId, dataObjectId } = await props.params;
   try {
-    const rawBody = await request.text();
-    const schema = z
-      .string()
-      .refine((s) => !/[^\x20-\x7E\t\n\r]/.test(s), { message: 'Binary data detected' });
-    const body = schema.parse(rawBody);
     const fetchedData = await getDataObject(spaceId, dataObjectId);
-    if (fetchedData.isResponse) return fetchedData.data;
-    else if (fetchedData.data) {
-      const parameter = fetchedData.data as Parameter;
-      const parameterChanges = {
-        value: body,
-        ...((parameter.transformation?.transformationType === 'linked' ||
-          parameter.transformation?.transformationType === 'algorithm') && {
-          transformation: {
-            ...parameter.transformation,
-            transformationType: 'manual' as const,
-            action: '',
-          },
-        }),
-      };
-      await updateParameter(parameter.id, parameterChanges, spaceId);
-      return NextResponse.json('Success.');
-    } else {
-      return NextResponse.json(`Parameter ${dataObjectId} not found.`, { status: 404 });
-    }
+    if (isErrorResponse(fetchedData)) return fetchedData.data;
+    const parameterChanges = await parseAndGetChanges(await request.text(), fetchedData.data);
+    await updateParameter(parameterChanges.id, parameterChanges.changes, spaceId);
+    return NextResponse.json('Success.');
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 409 });
-  }
-}
-
-async function getDataObject(spaceId: string, dataObjectId: string) {
-  try {
-    const parentConfig = await getDeepConfigurationById(spaceId);
-    if (parentConfig.configType !== 'organization') {
-      return {
-        isResponse: true,
-        data: NextResponse.json('This ID does not correspond to a space.', { status: 404 }),
-      };
-    } else {
-      const parentParameter = extractParameter(parentConfig, ['organization']);
-      if (!parentParameter) {
-        return {
-          isResponse: true,
-          data: NextResponse.json(
-            `Organization parameter not found in config of space ${spaceId}.`,
-            {
-              status: 404,
-            },
-          ),
-        };
-      }
-      let parameter: Parameter | undefined;
-      if (uuidValidate(dataObjectId)) {
-        parameter = findParameter(dataObjectId, parentParameter, 'parameter')?.selection;
-      } else {
-        const path = dataObjectId.toLocaleLowerCase().split('.');
-        parameter = extractParameter(parentParameter, path);
-      }
-      return { isResponse: false, data: parameter };
-    }
-  } catch {
-    return {
-      isResponse: true,
-      data: NextResponse.json(`No config found for spaceId ${spaceId}.`, { status: 404 }),
-    };
+    return NextResponse.json(error, { status: 409 });
   }
 }
--- src/management-system-v2/app/api/spaces/[spaceId]/data/user/[userId]/[dataObjectId]/route.ts
+++ src/management-system-v2/app/api/spaces/[spaceId]/data/user/[userId]/[dataObjectId]/route.ts
@@ -1,27 +1,19 @@
-import {
-  extractParameter,
-  findParameter,
-} from '@/app/(dashboard)/[environmentId]/machine-config/configuration-helper';
-import { getMembers } from '@/lib/data/db/iam/memberships';
-import { getDeepConfigurationById, updateParameter } from '@/lib/data/db/machine-config';
-import { Parameter } from '@/lib/data/machine-config-schema';
+import { updateParameter } from '@/lib/data/db/machine-config';
 import { NextRequest, NextResponse } from 'next/server';
-import { validate as uuidValidate } from 'uuid';
-import { z } from 'zod';
+import {
+  getDataObject,
+  isErrorResponse,
+  parseAndGetChanges,
+} from '@/app/api/spaces/[spaceId]/data/helper';
 
 export async function GET(
   request: NextRequest,
   props: { params: Promise<{ spaceId: string; userId: string; dataObjectId: string }> },
 ) {
   const { spaceId, userId, dataObjectId } = await props.params;
-  const fetchedData = await getDataObject(spaceId, userId, dataObjectId);
-  if (fetchedData.isResponse) return fetchedData.data;
-  else if (fetchedData.data) return NextResponse.json(fetchedData.data);
-  else
-    return NextResponse.json(
-      `Parameter ${dataObjectId} not found in user parameter for user ${userId} in space config for space ${spaceId}.`,
-      { status: 404 },
-    );
+  const fetchedData = await getDataObject(spaceId, dataObjectId, userId);
+  if (isErrorResponse(fetchedData)) return fetchedData.data;
+  else return NextResponse.json(fetchedData.data);
 }
 
 export async function PUT(
@@ -30,77 +22,12 @@ export async function PUT(
 ) {
   const { spaceId, userId, dataObjectId } = await props.params;
   try {
-    const rawBody = await request.text();
-    const schema = z
-      .string()
-      .refine((s) => !/[^\x20-\x7E\t\n\r]/.test(s), { message: 'Binary data detected' });
-    const body = schema.parse(rawBody);
-    const fetchedData = await getDataObject(spaceId, userId, dataObjectId);
-    if (fetchedData.isResponse) return fetchedData.data;
-    else if (fetchedData.data) {
-      const parameter = fetchedData.data as Parameter;
-      const parameterChanges = {
-        value: body,
-        ...((parameter.transformation?.transformationType === 'linked' ||
-          parameter.transformation?.transformationType === 'algorithm') && {
-          transformation: {
-            ...parameter.transformation,
-            transformationType: 'manual' as const,
-            action: '',
-          },
-        }),
-      };
-      await updateParameter(parameter.id, parameterChanges, spaceId);
-      return NextResponse.json('Success.');
-    } else {
-      return NextResponse.json(
-        `Parameter ${dataObjectId} not found in user parameter for user ${userId} in space config for space ${spaceId}.`,
-        { status: 404 },
-      );
-    }
+    const fetchedData = await getDataObject(spaceId, dataObjectId, userId);
+    if (isErrorResponse(fetchedData)) return fetchedData.data;
+    const parameterChanges = await parseAndGetChanges(await request.text(), fetchedData.data);
+    await updateParameter(parameterChanges.id, parameterChanges.changes, spaceId);
+    return NextResponse.json('Success.');
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 409 });
-  }
-}
-
-async function getDataObject(spaceId: string, userId: string, dataObjectId: string) {
-  try {
-    const parentConfig = await getDeepConfigurationById(spaceId);
-    const memberId = (await getMembers(spaceId)).find((member) => member.userId == userId)?.id;
-    if (!memberId) {
-      return {
-        isResponse: true,
-        data: NextResponse.json(`User ${userId} is not part of space ${spaceId}.`, { status: 404 }),
-      };
-    }
-    const userParameter = findParameter(memberId, parentConfig, 'config')?.selection;
-    let parameter: Parameter | undefined;
-    if (parentConfig.configType !== 'organization') {
-      return {
-        isResponse: true,
-        data: NextResponse.json(`${spaceId} does not correspond to a space.`, { status: 404 }),
-      };
-    } else if (!userParameter) {
-      return {
-        isResponse: true,
-        data: NextResponse.json(
-          `User Parameter for ${userId} (memberId: ${memberId}) not found in config for space ${spaceId}.`,
-          { status: 404 },
-        ),
-      };
-    } else {
-      if (uuidValidate(dataObjectId)) {
-        parameter = findParameter(dataObjectId, userParameter, 'config')?.selection;
-      } else {
-        const path = dataObjectId.toLocaleLowerCase().split('.');
-        parameter = extractParameter(userParameter, path);
-      }
-      return { isResponse: false, data: parameter };
-    }
-  } catch {
-    return {
-      isResponse: true,
-      data: NextResponse.json(`No config found for spaceId ${spaceId}.`, { status: 404 }),
-    };
+    return NextResponse.json(error, { status: 409 });
   }
 }

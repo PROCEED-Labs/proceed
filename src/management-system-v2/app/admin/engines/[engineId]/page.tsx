6336e90837792f6571763c4553ffbd423403153e
Janis Joderi Shoferi <janis.joderi@web.de>
2026-06-09 16:25:48 +0200

    Removed invalid import

--- src/management-system-v2/app/admin/engines/[engineId]/page.tsx
+++ src/management-system-v2/app/admin/engines/[engineId]/page.tsx
@@ -5,7 +5,6 @@ import { notFound, redirect } from 'next/navigation';
 import { Suspense } from 'react';
 import Link from 'next/link';
 import { LeftOutlined } from '@ant-design/icons';
-import { type EngineWithConnections } from '@/lib/engines/types';
 import { getEngineById } from '@/lib/data/engines';
 import { getMSConfig } from '@/lib/ms-config/ms-config';
 import EngineDashboard from '@/components/engine-dashboard/server-component';
